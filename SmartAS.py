36e890f6816a3d5c105b232aa655c532998acdb0
Héctor Climente <hclimente@gmail.com>
2014-01-20 11:44:08 +0100

    Deleted commented shell calls.

--- SmartAS.py
+++ SmartAS.py
@@ -56,21 +56,17 @@ def exploreData():
 	
 	print "* Reading and summarizing input files: computing PSI values and plotting correlations between replicates."
 	sh.cmd("Pipeline/PSICalculation.r")
-	#subprocess.call("Pipeline/PSICalculation.r", shell=True)
 	copy("SmartAS.RData", "Results/RWorkspaces/1_ExploreData.RData")
 
 def getCandidates(minExpression, minCandidateExpression, minPSI):
 
 	print "* Extracting transcripts with high variance and high expression."
 	sh.cmd("Pipeline/GetCandidates.r", minExpression, minCandidateExpression, minPSI)
-	#subprocess.call("Pipeline/GetCandidates.r " + str(minExpression) + " " + str(minCandidateExpression) + " " + str(minPSI), shell=True)
 	copy("SmartAS.RData", "Results/RWorkspaces/2_GetCandidates.RData")
 
 	sh.cmd("sort Results/expressedGenes.lst >Results/expressedGenes.tmp.lst")
-	#subprocess.call("sort Results/expressedGenes.lst >Results/expressedGenes.tmp.lst", shell=True)
 	sh.cmd("mv Results/expressedGenes.tmp.lst Results/expressedGenes.lst")
-	#subprocess.call("mv Results/expressedGenes.tmp.lst Results/expressedGenes.lst", shell=True)
-
+	
 def prepareILoopsInput():
 
 	getExpressedGenes = 1
@@ -82,16 +78,13 @@ def prepareILoopsInput():
 	if not diff.stdout.read().strip() and os.path.exists("old/iLoops/input/ExpressedTranscripts.fasta"):
 		getExpressedGenes = 0
 
-	#subprocess.call("Pipeline/getiLoopsInput.pl Results/expressedGenes.lst Results/candidateList.lst " + getExpressedGenes, shell=True)
 	sh.cmd("Pipeline/getiLoopsInput.pl Results/expressedGenes.lst Results/candidateList.lst ", getExpressedGenes)
 
 def launchILoops():
 
 	print "* Launching iLoops jobs."
 	
-	#subprocess.call("scp -r Results/iLoops hectorc@gaudi.imim.es:~/SmartAS/Results", shell=True)
 	sh.cmd("scp -r Results/iLoops hectorc@gaudi.imim.es:~/SmartAS/Results")
-	#subprocess.call("ssh hectorc@gaudi.imim.es '~/SmartAS/Pipeline/launchILoops.sh /sbi/users/hectorc/SmartAS/Results/iLoops", shell=True)
 	sh.cmd("ssh hectorc@gaudi.imim.es '~/SmartAS/Pipeline/launchILoops.sh /sbi/users/hectorc/SmartAS/Results/iLoops")
 
 	print "\t* Waiting..."
@@ -99,7 +92,6 @@ def launchILoops():
 def exloreILoopsResults():
 
 	print "* Examining iLoops results."
-	#subprocess.call("Pipeline/exploreiLoopsOutput.py", shell=True)
 	sh.cmd("Pipeline/exploreiLoopsOutput.py")
 
 main(sys.argv[1:])
