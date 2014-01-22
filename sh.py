d72501931ca43b1d8879d50a7950ab832e5b8d9e
Héctor Climente <hclimente@gmail.com>
2014-01-22 17:50:07 +0100

    setWorkspace.R translated into Python.

--- sh.py
+++ sh.py
@@ -1,6 +1,7 @@
 #!/soft/devel/python-2.7/bin/python
 
 from subprocess import call
+from rpy2.robjects import r
 
 def cmd(base, *args):
 	command = base
@@ -8,7 +9,40 @@ def cmd(base, *args):
 		command += " " + str(arg)
 	call(command, shell=True)
 
-def setEnvironment(wd, currentStep):
+def setRWorkspace(wd, Conditions, Compartments, Replicates, Kmer):
+
+	r("wd <- \"" + wd + "\"")
+	r("inputData <- list()")
+	
+	condStat = 'inputData[["Conditions"]] <- c('
+	for condition in Conditions:
+		condStat = condStat + "\"" + condition + "\","
+	condStat = condStat[:-1] + ")"
+	
+	compStat = 'inputData[["Compartments"]] <- c('
+	for compartment in Compartments:
+		compStat = compStat + "\"" + compartment + "\","
+	compStat = compStat[:-1] + ")"
+	
+	replStat = 'inputData[["Replicates"]] <- c('
+	for replicate in Replicates:
+		replStat = replStat + "\"" + replicate + "\","
+	replStat = replStat[:-1] + ")"
+	
+	kmerStat = 'inputData[["K-mer"]] <- c('
+	for kmer in Kmer:
+		kmerStat = kmerStat + "\"" + kmer + "\","
+	kmerStat = kmerStat[:-1] + ")"
+	
+	r(condStat)
+	r(compStat)
+	r(replStat)
+	r(kmerStat)
+	
+	r('save.image("SmartAS.RData")')
+
+def setEnvironment(wd, currentStep, Conditions, Compartments, Replicates, Kmer):
+
 	print "* Preparing the environment"
 	cmd("cd " + wd)
 
@@ -38,4 +72,5 @@ def setEnvironment(wd, currentStep):
 		cmd("mv SmartAS.RData old/SmartAS.old.RData")
 		cmd("mkdir -p Results/DataExploration")
 		cmd("mkdir -p Results/RWorkspaces")
-		cmd("Pipeline/SetWorkspace.r " + wd)
+		setRWorkspace(wd, Conditions, Compartments, Replicates, Kmer)
+		#cmd("Pipeline/SetWorkspace.r " + wd)
