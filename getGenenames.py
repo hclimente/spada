7521c22858fa3217ea8485f5bda763129be87d43
Héctor Climente <hclimente@gmail.com>
2014-01-30 12:33:52 +0100

    Print the candidate list with genenames and in tsv format.

---
+++ getGenenames.py
@@ -0,0 +1,19 @@
+#!/soft/devel/python-2.7/bin/python
+
+from sh import *
+import sys
+
+candidates = sys.argv[1];
+
+with open(candidates, "r") as CANDIDATES:
+	with open ("Results/candidateList_withGenenames.tsv", "w") as GENENAMES:
+		GENENAMES.write("Genename\tEnsembl Id\tNormal isoform\tTumor isoform\n")
+		for line in CANDIDATES:
+			ids = line.split("\t")
+			ensGene = ids[2].strip()
+
+			geneInfo = getFeature("feature", ensGene, "feature=gene")
+
+			for line in geneInfo:
+				if line["ID"] == ensGene:
+					GENENAMES.write(line["external_name"] + "\t" + ensGene + "\t" + ids[0] + "\t" + ids[1] + "\n")
