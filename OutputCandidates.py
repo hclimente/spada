ee647e91fbd8ea413572b1db12908d44373bf9c9
Héctor Climente <hclimente@gmail.com>
2014-02-03 19:35:39 +0100

    Not querying anymore Ensembl for GFF tracks (testing)

--- OutputCandidates.py
+++ OutputCandidates.py
@@ -3,6 +3,24 @@
 from sh import *
 import sys
 
+candidates = sys.argv[1];
+
+with open(candidates, "r") as CANDIDATES, open('Results/candidates_normal.v2.gff', 'w') as GFF2n_TRACK, open('Results/candidates_tumor.v2.gff', 'w') as GFF2t_TRACK:
+	with open("Data/annotation.gtf", "r") as ALLTRANSCRIPTS:
+		candTnt = []
+		for line in CANDIDATES:
+			ids = line.split("\t")
+			pair = [ids[0], ids[1]]
+			candTnt.append(pair)
+		
+		for line in ALLTRANSCRIPTS:
+			for pair in candTnt:
+				if line.find(pair[0]) != -1:
+					GFF2n_TRACK.write(line)
+				elif line.find(pair[1]) != -1:
+					GFF2t_TRACK.write(line)
+
+
 exit()
 
 candidates = sys.argv[1];
