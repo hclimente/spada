c4013f507ac0b767813c61f10b4ef6c3d175e86c
Héctor Climente <hector.climente@upf.edu>
2014-02-07 18:00:46 +0100

    GetCandidates adapted to TCGA (still testing). Also, modified to accept the plot of Venn diagrams.

--- GetCandidates.r
+++ GetCandidates.r
@@ -29,60 +29,56 @@ minExpression <- as.numeric(args[1])
 minCandidateExpression <- as.numeric(args[2])
 minPSI <- as.numeric(args[3])
 
-for (kmer in inputData[["K-mer"]]){
-	for (replicate in inputData[["Replicates"]]){
+for (replicate in inputData[["Replicates"]]){
 
-		tag <- paste0(inputData[["Compartments"]][1], replicate, "_", kmer)
-		#candidates[[tag]] <- data.frame(Gene=as.character(), Entropy_Ref=as.numeric(), Entropy_Alt=as.numeric(), Switch=as.numeric(), maxdPSI=as.character(), 
-		#								GENCODEmaxdPSI=as.character(), mindPSI=as.character(), GENCODEminPSI=as.character())
-		candidates[[tag]] <- data.frame(Gene=as.character(), Genename=as.character(), Entropy_Ref=as.numeric(), Entropy_Alt=as.numeric(), 
-										Switch=as.numeric(), maxdPSI=as.character(), mindPSI=as.character())
+	candidates[[replicate]] <- data.frame(Gene=as.character(), Genename=as.character(), Entropy_Ref=as.numeric(), Entropy_Alt=as.numeric(), 
+										  Switch=as.numeric(), maxdPSI=as.character(), mindPSI=as.character())
 
-		#Filter by deltaPSI and expression, based on the FPR
-		psiThreshold <- abs(intraReplicate[[tag]]$deltaPSI) > minPSI
-		expressionThreshold <- intraReplicate[[tag]]$la_tTPM > minCandidateExpression
+	#Filter by deltaPSI and expression, based on the FPR
+	psiThreshold <- abs(intraReplicate[[replicate]]$deltaPSI) > minPSI
+	expressionThreshold <- intraReplicate[[replicate]]$la_tTPM > minCandidateExpression
 			
-		for (aCandidate in unique(intraReplicate[[tag]]$Gene[psiThreshold & expressionThreshold])){
+	for (aCandidate in unique(intraReplicate[[replicate]]$Gene[psiThreshold & expressionThreshold])){
 
-			thisGeneData <- intraReplicate[[tag]]$Gene == aCandidate
-			posPSI <- intraReplicate[[tag]]$deltaPSI > 0
-			negPSI <- intraReplicate[[tag]]$deltaPSI < 0
+		thisGeneData <- intraReplicate[[replicate]]$Gene == aCandidate
+		posPSI <- intraReplicate[[replicate]]$deltaPSI > 0
+		negPSI <- intraReplicate[[replicate]]$deltaPSI < 0
 
-			#Calculate max difference between the transcripts
-			maxDeltaPsi <- max(intraReplicate[[tag]]$deltaPSI[thisGeneData & posPSI], na.rm=T)
-			minDeltaPsi <- min(intraReplicate[[tag]]$deltaPSI[thisGeneData & negPSI], na.rm=T)
-			maxSwitch <- maxDeltaPsi - minDeltaPsi
+		#Calculate max difference between the transcripts
+		maxDeltaPsi <- max(intraReplicate[[replicate]]$deltaPSI[thisGeneData & posPSI], na.rm=T)
+		minDeltaPsi <- min(intraReplicate[[replicate]]$deltaPSI[thisGeneData & negPSI], na.rm=T)
+		maxSwitch <- maxDeltaPsi - minDeltaPsi
 
-			#deltaPSI = PSI_ref - PSI_alt
-			#	MaxDeltaPsiCond: predominant transcript in the normal
-			#	MinDeltaPsiCond: predominant transcript in the tumor
-			maxDeltaPsiCond <- intraReplicate[[tag]]$deltaPSI == maxDeltaPsi
-			minDeltaPsiCond <- intraReplicate[[tag]]$deltaPSI == minDeltaPsi
+		#deltaPSI = PSI_ref - PSI_alt
+		#	MaxDeltaPsiCond: predominant transcript in the normal
+		#	MinDeltaPsiCond: predominant transcript in the tumor
+		maxDeltaPsiCond <- intraReplicate[[replicate]]$deltaPSI == maxDeltaPsi
+		minDeltaPsiCond <- intraReplicate[[replicate]]$deltaPSI == minDeltaPsi
 
-			candidates[[tag]] <- rbind( candidates[[tag]], data.frame(Gene=aCandidate, Entropy_Ref=calculateEntropy(intraReplicate[[tag]]$PSI_ref[thisGeneData]), 
-										Entropy_Alt=calculateEntropy(intraReplicate[[tag]]$PSI_alt[thisGeneData]), Switch=maxSwitch, 
-										maxdPSI=intraReplicate[[tag]]$Transcript[thisGeneData & maxDeltaPsiCond], 
-										Genename=intraReplicate[[tag]]$Genename[thisGeneData & maxDeltaPsiCond], 
-										mindPSI=intraReplicate[[tag]]$Transcript[thisGeneData & minDeltaPsiCond]))
-		}
-
-		#Expressed genes: transcript whose expression is above the threshold
-		expressedGenes <-(log(intraReplicate[[tag]]$TPM_ref) + log(intraReplicate[[tag]]$TPM_alt)) / 2 > minExpression
-		allGenes <- unique(c(allGenes, intraReplicate[[tag]]$Transcript[expressedGenes]))
-
+		candidates[[replicate]] <- rbind( candidates[[replicate]], data.frame(Gene=aCandidate, Entropy_Ref=calculateEntropy(intraReplicate[[replicate]]$PSI_ref[thisGeneData]), 
+										  Entropy_Alt=calculateEntropy(intraReplicate[[replicate]]$PSI_alt[thisGeneData]), Switch=maxSwitch, 
+										  maxdPSI=intraReplicate[[replicate]]$Transcript[thisGeneData & maxDeltaPsiCond], 
+										  Genename=intraReplicate[[replicate]]$Genename[thisGeneData & maxDeltaPsiCond], 
+										  mindPSI=intraReplicate[[replicate]]$Transcript[thisGeneData & minDeltaPsiCond])
+										)
 	}
+
+	#Expressed genes: transcript whose expression is above the threshold
+	expressedGenes <-(log(intraReplicate[[replicate]]$TPM_ref) + log(intraReplicate[[replicate]]$TPM_alt)) / 2 > minExpression
+	allGenes <- unique(c(allGenes, intraReplicate[[replicate]]$Transcript[expressedGenes]))
+
+	entropyCutRef <- candidates[[replicate]]$Entropy_Ref < median(candidates[[replicate]]$Entropy_Ref)  
+ 	entropyCutAlt <- candidates[[replicate]]$Entropy_Alt < median(candidates[[replicate]]$Entropy_Alt)
+	switchCut <- candidates[[replicate]]$Switch > median(candidates[[replicate]]$Switch)
+
+	candidates[[replicate]] <- candidates[[replicate]] [entropyCutRef & entropyCutAlt & switchCut, c("Genename", "Gene", "maxdPSI","mindPSI")])
+
 }
 
 candidateList <- data.frame(maxdPSI=as.character(), mindPSI=as.character())
 
 for (aCondition in candidates){
-
-  entropyCutRef <- aCondition$Entropy_Ref < median(aCondition$Entropy_Ref)  
-  entropyCutAlt <- aCondition$Entropy_Alt < median(aCondition$Entropy_Alt)
-  switchCut <- aCondition$Switch > median(aCondition$Switch)
-  
-  candidateList <- rbind(candidateList, aCondition[entropyCutRef & entropyCutAlt & switchCut, c("Genename", "Gene", "maxdPSI","mindPSI")])
-  
+	candidateList <- rbind(candidateList, aCondition[entropyCutRef & entropyCutAlt & switchCut, c("Genename", "Gene", "maxdPSI","mindPSI")])
 }
 
 candidateList <- unique(candidateList)
