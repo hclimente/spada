31910eeac71e05b854315061fe6a8be081b3492e
Héctor Climente <hector.climente@upf.edu>
2014-02-07 13:57:39 +0100

    ExploreData adapted to work with multiple replicates. Still testing.

--- ExploreData.r
+++ ExploreData.r
@@ -56,91 +56,95 @@ createRow <- function(rawValue){
   return(newRow)
 }
 
-for (kmer in inputData[["K-mer"]]){
-  for (replicate in inputData[["Replicates"]]){
-    tag <- paste0(compartment, replicate, "_", kmer)
-    for (sample in inputData[["Conditions"]]){
+for (replicate in inputData[["Replicates"]]){
+  for (sample in c("N", "T")){
 
-      thisTag <- paste0(sample, tag)
-      cat("\t* Exploring file",thisTag, "\n")
-      inputFile=paste0(wd, "/Data/GENCODE/", thisTag, ".filtered.sf")
-      outputFile=paste0(wd, "/Results/", thisTag, ".tsv")
+    thisTag <- paste0(replicate, "_", sample)
+    cat("\t* Exploring file",thisTag, "\n")
+    inputFile=paste0(wd, "/Data/Input/", thisTag, ".tsv")
+    outputFile=paste0(wd, "/Results/", thisTag, ".tsv")
       
-      #Read Sailfish table
-      isoformExpression[[thisTag]] <- read.table(inputFile, header=F, sep="\t", stringsAsFactors=F)
-      colnames(isoformExpression[[thisTag]]) <- c("Gene", "Transcript","Genename","TPM")
+    #Read Sailfish table
+    isoformExpression[[thisTag]] <- read.table(inputFile, header=F, sep="\t", stringsAsFactors=F)
+    colnames(isoformExpression[[thisTag]]) <- c("Gene", "Transcript","Genename","TPM")
       
-      #Calculate the PSI for each transcript and the total expression of the gene
-      vPSI <- as.numeric()
-      vtTPM <- as.numeric()
+    #Calculate the PSI for each transcript and the total expression of the gene
+    vPSI <- as.numeric()
+    vtTPM <- as.numeric()
       
-      for (thisTranscript in 1:nrow(isoformExpression[[thisTag]])){
-        mask <- isoformExpression[[thisTag]]$Gene==isoformExpression[[thisTag]]$Gene[thisTranscript]
-        total <- sum(isoformExpression[[thisTag]]$TPM[mask])
-        vtTPM <- c(vtTPM, total)
-        if(total!=0){
-          vPSI <- c(vPSI, isoformExpression[[thisTag]]$TPM[thisTranscript]/total)
-        } else {
-          vPSI <- c(vPSI, NA)
-        }
+    for (thisTranscript in 1:nrow(isoformExpression[[thisTag]])){
+      mask <- isoformExpression[[thisTag]]$Gene==isoformExpression[[thisTag]]$Gene[thisTranscript]
+      total <- sum(isoformExpression[[thisTag]]$TPM[mask])
+      vtTPM <- c(vtTPM, total)
+      if(total!=0){
+        vPSI <- c(vPSI, isoformExpression[[thisTag]]$TPM[thisTranscript]/total)
+      } else {
+        vPSI <- c(vPSI, NA)
       }
-      
-      isoformExpression[[thisTag]]$tTPM <- vtTPM
-      isoformExpression[[thisTag]]$PSI <- vPSI
-      
-      write.table(isoformExpression[[thisTag]], file=outputFile, sep="\t", row.names=F)
     }
-   
-    refTag <- paste0(reference, tag)
-    altTag <- paste0(alterated, tag)
-    
-    intraReplicate[[tag]] <- merge(isoformExpression[[refTag]], isoformExpression[[altTag]], by=c("Gene", "Transcript", "Genename"), suffixes=c("_ref","_alt"), all=T)
-    #intraReplicate[[tag]] <- merge(isoformExpression[[refTag]], isoformExpression[[altTag]], by=c("Gene","Transcript","GENCODE"), suffixes=c("_ref","_alt"), all=T)
-    intraReplicate[[tag]]$deltaPSI <- intraReplicate[[tag]]$PSI_ref - intraReplicate[[tag]]$PSI_alt
-    intraReplicate[[tag]]$la_tTPM <- 0.5 * (log(intraReplicate[[tag]]$tTPM_ref) + log(intraReplicate[[tag]]$tTPM_alt))
-    
-    #Plot stuff
-    printLogFreqHist(intraReplicate[[tag]]$deltaPSI, "deltaPSI", paste0("deltaPSI_", tag))
-    printTPMHist(intraReplicate[[tag]]$TPM_ref, "log10(TPM10+0.0001)", paste0("TPM10_",tag))
-    printLogFreqHist(intraReplicate[[tag]]$PSI_ref, "PSI10", paste0("PSI10_",tag))
-    printTPMHist(intraReplicate[[tag]]$TPM_alt, "log10(TPM7+0.0001)", paste0("TPM7_",tag))
-    printLogFreqHist(intraReplicate[[tag]]$PSI_ref, "PSI7", paste0("PSI7_",tag))
-    
-    write.table(intraReplicate[[tag]], file=paste0(wd,"/Results/IntraReplicate",tag,".tsv"), sep="\t", row.names=F)
-
+      
+    isoformExpression[[thisTag]]$tTPM <- vtTPM
+    isoformExpression[[thisTag]]$PSI <- vPSI
+      
+    write.table(isoformExpression[[thisTag]], file=outputFile, sep="\t", row.names=F)
   }
+   
+  refTag <- paste0(replicate, "N")
+  altTag <- paste0(replicate, "T")
+    
+  intraReplicate[[replicate]] <- merge(isoformExpression[[refTag]], isoformExpression[[altTag]], by=c("Gene", "Transcript", "Genename"), suffixes=c("_N","_T"), all=T)
+  intraReplicate[[replicate]]$deltaPSI <- intraReplicate[[replicate]]$PSI_N - intraReplicate[[replicate]]$PSI_T
+  intraReplicate[[replicate]]$la_tTPM <- 0.5 * (log(intraReplicate[[replicate]]$tTPM_N) + log(intraReplicate[[replicate]]$tTPM_T))
+    
+  #Plot stuff
+  printLogFreqHist(intraReplicate[[replicate]]$deltaPSI, "deltaPSI", paste0("deltaPSI_", replicate))
+  printTPMHist(intraReplicate[[replicate]]$TPM_N, "log10(TPM_N+0.0001)", paste0("TPM_N_",replicate))
+  printLogFreqHist(intraReplicate[[replicate]]$PSI_N, "PSI_N", paste0("PSI_N_",replicate))
+  printTPMHist(intraReplicate[[replicate]]$TPM_T, "log10(TPM_T+0.0001)", paste0("TPM_T_",replicate))
+  printLogFreqHist(intraReplicate[[replicate]]$PSI_N, "PSI_T", paste0("PSI_T_",replicate))
+    
+  write.table(intraReplicate[[replicate]], file=paste0(wd,"/Results/IntraReplicate",replicate,".tsv"), sep="\t", row.names=F)
 
-  #Plot more stuff
-  tag <- paste0(compartment, kmer)
-  tag1 <- paste0(compartment, inputData[["Replicates"]][1], "_", kmer)
-  tag2 <- paste0(compartment, inputData[["Replicates"]][2], "_", kmer)
-  
-  plotCorrelations(intraReplicate[[tag1]]$TPM_ref, intraReplicate[[tag2]]$TPM_ref, "TPM_10", paste0("TPM10_cor_", tag))
-  plotCorrelations(intraReplicate[[tag1]]$TPM_alt, intraReplicate[[tag2]]$TPM_alt, "TPM_7", paste0("TPM7_cor_", tag))
-  plotCorrelations(intraReplicate[[tag1]]$deltaPSI, intraReplicate[[tag2]]$deltaPSI, "deltaPSI", paste0("deltaPSI_cor_", tag))
-  plotCorrelations(intraReplicate[[tag1]]$PSI_ref, intraReplicate[[tag2]]$PSI_ref, "PSI_10", paste0("PSI10_cor_", tag))
-  plotCorrelations(intraReplicate[[tag1]]$PSI_alt, intraReplicate[[tag2]]$PSI_alt, "PSI_10", paste0("PSI7_cor_", tag))
-  
-  interReplicate[["Ref"]] <- merge(intraReplicate[[tag1]], intraReplicate[[tag2]], by=c("Gene", "Transcript", "Genename"), suffixes=c("_1","_2"), all=T)
-  #interReplicate[["Ref"]] <- merge(intraReplicate[[tag1]], intraReplicate[[tag2]], by=c("Gene","Transcript","GENCODE"), suffixes=c("_1","_2"), all=T)
-  interReplicate[["Ref"]] <- subset(interReplicate[["Ref"]], select=-c(TPM_alt_1, tTPM_alt_1, PSI_alt_1, TPM_alt_2, tTPM_alt_2, PSI_alt_2))
-  interReplicate[["Ref"]]$deltaPSI <- interReplicate[["Ref"]]$PSI_ref_1 - interReplicate[["Ref"]]$PSI_ref_2
-  interReplicate[["Ref"]]$la_tTPM <- 0.5 * (log(interReplicate[["Ref"]]$tTPM_ref_1) + log(interReplicate[["Ref"]]$tTPM_ref_2))
-  
-  interReplicate[["Alt"]] <- merge(intraReplicate[[tag1]], intraReplicate[[tag2]], by=c("Gene", "Transcript", "Genename"), suffixes=c("_1","_2"), all=T)
-  #interReplicate[["Alt"]] <- merge(intraReplicate[[tag1]], intraReplicate[[tag2]], by=c("Gene","Transcript","GENCODE"), suffixes=c("_1","_2"), all=T)
-  interReplicate[["Alt"]] <- subset(interReplicate[["Alt"]], select=-c(TPM_ref_1, tTPM_ref_1, PSI_ref_1, TPM_ref_2, tTPM_ref_2, PSI_ref_2))
-  interReplicate[["Alt"]]$deltaPSI <- interReplicate[["Alt"]]$PSI_alt_1 - interReplicate[["Alt"]]$PSI_alt_2
-  interReplicate[["Alt"]]$la_tTPM <- 0.5 * (log(interReplicate[["Alt"]]$tTPM_alt_1) + log(interReplicate[["Alt"]]$tTPM_alt_2))
-  
-  simplePlot(intraReplicate[[tag1]]$la_tTPM, intraReplicate[[tag1]]$deltaPSI, tag1, "0.5·(log(sum tTPM10) + log(sum tTPM7) )", 
-             "deltaPSI", paste0(wd,"/Results/DataExploration/latTPM_PSI_intrarreplicate",tag1,".png"))
-  simplePlot(intraReplicate[[tag2]]$la_tTPM, intraReplicate[[tag2]]$deltaPSI, tag2, "0.5·(log(sum tTPM10) + log(sum tTPM7) )", 
-             "deltaPSI", paste0(wd,"/Results/DataExploration/latTPM_PSI_intrarreplicate",tag2,".png"))
-  simplePlot(interReplicate[["Ref"]]$la_tTPM, interReplicate[["Ref"]]$deltaPSI, paste0(tag, "_10"), "0.5·(log(sum tTPM_1) + log(sum tTPM_2) )", 
-             "deltaPSI", paste0(wd,"/Results/DataExploration/latTPM_PSI_interreplicate10_",tag,".png"))
-  simplePlot(interReplicate[["Alt"]]$la_tTPM, interReplicate[["Alt"]]$deltaPSI, paste0(tag, "_7"), "0.5·(log(sum tTPM_1) + log(sum tTPM_2) )", 
-             "deltaPSI", paste0(wd,"/Results/DataExploration/latTPM_PSI_interreplicate7_",tag,".png"))
+}
+
+for (r1 in inputData[["Replicates"]]){
+  for (r2 in inputData[["Replicates"]]){
+    #Plot more stuff
+    if (r1 == r2){
+      break
+    }
+
+    thisComp <- paste0(r1, "_", r2)
+
+    plotCorrelations(intraReplicate[[r1]]$TPM_N, intraReplicate[[r2]]$TPM_N, "TPM_N", paste0("TPM_N_cor_", thisComp))
+    plotCorrelations(intraReplicate[[r1]]$TPM_T, intraReplicate[[r2]]$TPM_T, "TPM_T", paste0("TPM_T_cor_", thisComp))
+    plotCorrelations(intraReplicate[[r1]]$deltaPSI, intraReplicate[[r2]]$deltaPSI, "deltaPSI", paste0("deltaPSI_cor_", thisComp))
+    plotCorrelations(intraReplicate[[r1]]$PSI_N, intraReplicate[[r2]]$PSI_N, "PSI_N", paste0("PSI_N_cor_", thisComp))
+    plotCorrelations(intraReplicate[[r1]]$PSI_T, intraReplicate[[r2]]$PSI_T, "PSI_T", paste0("PSI_T_cor_", thisComp))
+      
+    interReplicate[["Normal"]] <- merge(intraReplicate[[r1]], intraReplicate[[r2]], by=c("Gene", "Transcript", "Genename"),
+                                        suffixes=c(paste0("_", r1),paste0("_", r2)), all=T)
+    interReplicate[["Normal"]] <- subset(interReplicate[["Normal"]], select=-c(paste0("TPM_T_", r1), (paste0"tTPM_T_", r1), paste0("PSI_T_", r1), 
+                                                                               paste0("TPM_T_", r2), paste0("tTPM_T_", r2), paste0("PSI_T_", r2)))
+    interReplicate[["Normal"]]$deltaPSI <- interReplicate[["Normal"]]$PSI_N_1 - interReplicate[["Normal"]]$PSI_N_2
+    interReplicate[["Normal"]]$la_tTPM <- 0.5 * (log(interReplicate[["Normal"]]$tTPM_N_1) + log(interReplicate[["Normal"]]$tTPM_N_2))
+      
+    interReplicate[["Tumor"]] <- merge(intraReplicate[[r1]], intraReplicate[[r2]], by=c("Gene", "Transcript", "Genename"), 
+                                       suffixes=c(paste0("_", r1),paste0("_", r2)), all=T)
+    interReplicate[["Tumor"]] <- subset(interReplicate[["Tumor"]], select=-c(TPM_N_1, tTPM_N_1, PSI_N_1, TPM_N_2, tTPM_N_2, PSI_N_2))
+    interReplicate[["Tumor"]] <- subset(interReplicate[["Tumor"]], select=-c(paste0("TPM_N_", r1), (paste0"tTPM_N_", r1), paste0("PSI_N_", r1), 
+                                                                             paste0("TPM_N_", r2), paste0("tTPM_N_", r2), paste0("PSI_N_", r2)))
+    interReplicate[["Tumor"]]$deltaPSI <- interReplicate[["Tumor"]][,paste0("PSI_T_", r1)] - interReplicate[["Tumor"]][,paste0("PSI_T_", r2)]
+    interReplicate[["Tumor"]]$la_tTPM <- 0.5 * (log(interReplicate[["Tumor"]][,paste0("tTPM_T_", r2)]) + log(interReplicate[["Tumor"]][,paste0("tTPM_T_", r2)]))
+  }
+  simplePlot(intraReplicate[[r1]]$la_tTPM, intraReplicate[[r1]]$deltaPSI, r1, "0.5·(log(sum tTPM_N) + log(sum tTPM_T) )", 
+             "deltaPSI", paste0(wd,"/Results/DataExploration/latTPM_PSI_intrarreplicate",r1,".png"))
+  #simplePlot(intraReplicate[[r2]]$la_tTPM, intraReplicate[[r2]]$deltaPSI, r2, "0.5·(log(sum tTPM_N) + log(sum tTPM_T) )", 
+  #           "deltaPSI", paste0(wd,"/Results/DataExploration/latTPM_PSI_intrarreplicate",r2,".png"))
+  simplePlot(interReplicate[["Normal"]]$la_tTPM, interReplicate[["Normal"]]$deltaPSI, paste0(tag, "__N"), "0.5·(log(sum tTPM_1) + log(sum tTPM_2) )", 
+            "deltaPSI", paste0(wd,"/Results/DataExploration/latTPM_PSI_interreplicate_N_",tag,".png"))
+  #simplePlot(interReplicate[["Tumor"]]$la_tTPM, interReplicate[["Tumor"]]$deltaPSI, paste0(tag, "__T"), "0.5·(log(sum tTPM_1) + log(sum tTPM_2) )", 
+  #         "deltaPSI", paste0(wd,"/Results/DataExploration/latTPM_PSI_interreplicate_T_",tag,".png"))
 }
 
 #Estimate the False Positive Rate
@@ -149,7 +153,7 @@ psiRange <- as.numeric()
 for (psiValue in seq(0, 1, by=0.05)){
   expressionRange <- as.numeric()
   for (expValue in seq(-5, 8, by=0.25)){
-    FPR <- c(FPR, sum((abs(interReplicate[["Ref"]]$deltaPSI)>=psiValue & interReplicate[["Ref"]]$la_tTPM>=expValue), na.rm=TRUE)/nrow(interReplicate[["Ref"]]))
+    FPR <- c(FPR, sum((abs(interReplicate[["Normal"]]$deltaPSI)>=psiValue & interReplicate[["Normal"]]$la_tTPM>=expValue), na.rm=TRUE)/nrow(interReplicate[["Normal"]]))
     expressionRange <- c(expressionRange,expValue)
   }
   psiRange <- c(psiRange,psiValue)
