568d953ccd80ddbccf58d62e6260297c3aaf532e
Héctor Climente <hclimente@gmail.com>
2014-02-26 18:09:33 +0100

    library name changed

--- launchILoops.py
+++ launchILoops.py
@@ -3,7 +3,7 @@
 import sys, os
 from libsmartas import *
 from fnmatch import filter
-import iLoopsXMLParser as parser
+import iLoops_xml_parser as parser
 
 class iLoopsParser(parser.ILXMLParser):
 	def custom_protein_output(self, protein_object, **kwds): 
