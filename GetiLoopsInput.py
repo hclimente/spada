e60415f50fb9cc3177b1405aba01addad19e4647
Héctor Climente <hclimente@gmail.com>
2014-02-27 17:32:45 +0100

    Pruning code.

--- GetiLoopsInput.py
+++ GetiLoopsInput.py
@@ -37,9 +37,12 @@ def writeFasta(basename, inputType, expressedTranscripts):
 	transcriptCounter = 0
 
 	expressedTranscriptsSet = set()
-	with open(expressedTranscripts, "r") as EXPRESSED:
-		for line in EXPRESSED:
-			expressedTranscriptsSet.add(line.strip())
+	if isinstance(expressedTranscript, basestring):
+		with open(expressedTranscripts, "r") as EXPRESSED:
+			for line in EXPRESSED:
+				expressedTranscriptsSet.add(line.strip())
+	elif isinstance(expressedTranscript, set):
+		expressedTranscriptsSet = expressedTranscript
 
 	MULTIFASTA = open(basename + "_" + str(fileCounter) + ".fasta", "w")
 
@@ -63,29 +66,68 @@ def writeFasta(basename, inputType, expressedTranscripts):
 			elif wannaWrite:
 				MULTIFASTA.write(line)
 
-out = sys.argv[1]
-expressedTranscripts = out + sys.argv[2]
-candidateTranscripts = out + sys.argv[3]
-inputType = sys.argv[4]
+def parseMapping(iLoopsFolder):
+	loopFamilies = {}
+
+	for mappingBatch in filter(os.listdir(iLoopsFolder + "Output/"), "Mapping_*"):
+		xmlFile = iLoopsFolder + "Output/" + mappingBatch + "/" + mappingBatch + ".xml"
+		errFile = iLoopsFolder + "Output/" + mappingBatch + "/" + mappingBatch + ".err.xml"
+
+		newLoops = myParser.parseLoops(xmlOutput					   = xmlFile,
+									   output_proteins               = True, 
+									   output_alignments             = False,
+									   output_domain_mappings        = False,
+									   output_protein_features       = True,
+									   output_domain_assignations    = False,
+									   output_interactions           = False,
+									   output_interaction_signatures = False,
+									   output_RF_results             = False,
+									   output_RF_precisions          = False
+									  )
+		
+		for aTranscript, loops in sorted(newLoops.iteritems()):
+			if loops not in loopFamilies.keys():
+				loopFamilies[loops] = []
+
+			loopFamilies[loops].append(aTranscript)
+
+		myParser.parseNoLoops(xmlOutput                     = errFile,
+							  iLoopsPath 					= iLoopsFolder,
+							  output_proteins               = True, 
+							  output_alignments             = False,
+							  output_domain_mappings        = False,
+							  output_protein_features       = False,
+							  output_domain_assignations    = False,
+							  output_interactions           = False,
+							  output_interaction_signatures = False,
+							  output_RF_results             = False,
+							  output_RF_precisions          = False
+							 )
+
+	loopPatternModel = set()
+	with open(iLoopsFolder + "loopFamilies.txt", "w") as loopFamiliesList:
+		 for loopPattern in loopFamilies.keys():
+		 	loopPatternModel.add(loopFamilies[loopPattern][0])
+
+		 	loopFamiliesList.write(">" + loopPattern + "\t" + loopFamilies[loopPattern][0] + "\n")
+		 	for transcript in loopFamilies[loopPattern][1:]:
+		 		loopFamiliesList.write(transcript + "\n")
+
+	return loopPatternModel
+
+os.chdir(sys.argv[1])
+out = "Results/" + sys.argv[2]
+expressedTranscripts = out + sys.argv[3]
+candidateTranscripts = out + sys.argv[4]
+inputType = sys.argv[5]
 iLoopsFolder = out + "/iLoops/"
 myParser = iLoopsParser()
-os.chdir("/sbi/users/hectorc/SmartAS")
 
-print("\t* Preparing original FASTA file.")
+print("\t* Preparing FASTA files.")
 
 writeFasta(iLoopsFolder + "ExpressedTranscripts", inputType, expressedTranscripts)
 
-isoformSeq = {}
 for expressedFasta in filter(os.listdir(iLoopsFolder), "ExpressedTranscripts_*.fasta"):
-	with open(iLoopsFolder + "/" + expressedFasta, "r") as expFasta:
-		currentTranscript = ""
-		for line in expFasta:
-			trueLine = line.strip()
-			if trueLine.find(">") != -1:
-				currentTranscript = trueLine[1:]
-				isoformSeq[currentTranscript] = ""
-			else:
-				isoformSeq[currentTranscript] += trueLine
 	
 	assignationBatch = expressedFasta.split("_")[1].split(".")[0]
 	cmd("/soft/devel/python-2.7/bin/python /sbi/programs/iLoops_devel/iLoops.py",
@@ -97,53 +139,8 @@ for expressedFasta in filter(os.listdir(iLoopsFolder), "ExpressedTranscripts_*.f
 		"-n 25"
 	   )
 
-loopFamilies = {}
-
-for mappingBatch in filter(os.listdir(iLoopsFolder + "Output/"), "Mapping_*"):
-	xmlFile = iLoopsFolder + "Output/" + mappingBatch + "/" + mappingBatch + ".xml"
-	errFile = iLoopsFolder + "Output/" + mappingBatch + "/" + mappingBatch + ".err.xml"
-
-	newLoops = myParser.parseLoops(xmlOutput					   = xmlFile,
-								   output_proteins               = True, 
-								   output_alignments             = False,
-								   output_domain_mappings        = False,
-								   output_protein_features       = True,
-								   output_domain_assignations    = False,
-								   output_interactions           = False,
-								   output_interaction_signatures = False,
-								   output_RF_results             = False,
-								   output_RF_precisions          = False
-								  )
-	
-	for aTranscript, loops in sorted(newLoops.iteritems()):
-		if loops not in loopFamilies.keys():
-			loopFamilies[loops] = []
-
-		loopFamilies[loops].append(aTranscript)
-
-	myParser.parseNoLoops(xmlOutput                     = errFile,
-						  iLoopsPath 					= iLoopsFolder,
-						  output_proteins               = True, 
-						  output_alignments             = False,
-						  output_domain_mappings        = False,
-						  output_protein_features       = False,
-						  output_domain_assignations    = False,
-						  output_interactions           = False,
-						  output_interaction_signatures = False,
-						  output_RF_results             = False,
-						  output_RF_precisions          = False
-						 )
-
-with open(iLoopsFolder + "expressedTranscripts.uniqLoops.li", "w") as uniqLoops, \
-	 open(iLoopsFolder + "loopFamilies.txt", "w") as loopFamiliesList:
-	 for loopPattern in loopFamilies.keys():
-	 	uniqLoops.write(loopFamilies[loopPattern][0] + "\n")
-
-	 	loopFamiliesList.write(">" + loopPattern + "\n")
-	 	for transcript in loopFamilies[loopPattern]:
-	 		loopFamiliesList.write(transcript + "\n")
-
-writeFasta(iLoopsFolder + "ExpressedTranscripts.uniqLoops", inputType, iLoopsFolder + "expressedTranscripts.uniqLoops.li")
+loopPatternReps = parseMapping(iLoopsFolder)
+writeFasta(iLoopsFolder + "ExpressedTranscripts.uniqLoops", inputType, loopPatternReps)
 
 print("\t* Writing the pairs files.")
 top = 0
@@ -155,16 +152,15 @@ with open(candidateTranscripts, "r") as CANDIDATES:
 		
 		for aCandidate in [elements[2], elements[3]]:
 	
-			fileNumber = 1
-			numberOfCandidates = 0
 			cmd("mkdir " + out + "/iLoops/Input/" + aCandidate)
-
-			print aCandidate
 
 			for expressedFasta in filter(os.listdir(iLoopsFolder), "ExpressedTranscripts.uniqLoops_*.fasta"):
 				cmd("cp", iLoopsFolder + expressedFasta, iLoopsFolder + "Input/" + aCandidate)
 				with open(iLoopsFolder + "Input/" + aCandidate + "/" + expressedFasta, "a") as exprFast, \
 					 open("Data/" + inputType + "/proteins.fa", "r") as motherFast:
+
+					 fileNumber = (expressedFasta.split(".")[1]).split("_")[1]
+
 					 writeSeq = False
 					 for line in motherFast:
 					 	if line.find(">") != -1:
@@ -175,21 +171,14 @@ with open(candidateTranscripts, "r") as CANDIDATES:
 						elif writeSeq:
 					 		exprFast.write(line)
 
-				PAIRS = open(iLoopsFolder + "Input/" + aCandidate + '/' + aCandidate + '_' + str(fileNumber) + '.net', "w")
-				with open(iLoopsFolder + "Input/" + aCandidate + "/" + expressedFasta, "r") as exprFast:
+				with open(iLoopsFolder + "Input/" + aCandidate + '/' + aCandidate + '_' + fileNumber + '.net', "w") as PAIRS, \
+					 open(iLoopsFolder + "Input/" + aCandidate + "/" + expressedFasta, "r") as exprFast:
 					for rawExpressed in exprFast:
 						if rawExpressed.find(">") == -1:
 							continue
 						expressedTranscript = rawExpressed.strip()[1:]
 						PAIRS.write(aCandidate + "\t" + expressedTranscript + "\n")
-						numberOfCandidates += 1
-						if(numberOfCandidates >= 10000):
-							fileNumber += 1
-							numberOfCandidates = 0
-							PAIRS.close()
-							PAIRS = open(iLoopsFolder + "/Input/" + aCandidate + '/' + aCandidate + '_' + str(fileNumber) + '.net', "w")
-					PAIRS.close()
-
+						
 		top +=1
 		if top >=30:
 			break
