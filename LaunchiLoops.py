557e75635703d0ab4454a4d2a4bbe6b51128d2b9
Héctor Climente <hclimente@gmail.com>
2014-03-04 16:52:58 +0100

    Check that candidates have loops and they are not the same. Code cleaned.

--- LaunchiLoops.py
+++ LaunchiLoops.py
@@ -1,35 +1,9 @@
 #!/soft/devel/python-2.7/bin/python
 
 import sys, os
-from shutil import copy
-from libsmartas import *
+from include.libsmartas import *
 from fnmatch import filter
-import include.iLoops_xml_parser as parser
-
-class iLoopsParser(parser.ILXMLParser):
-	def custom_protein_output(self, protein_object, **kwds): 
-		return protein_object
-	
-	def parseLoops(self, xmlOutput, **kwds):
-		parsedLoops = {}
-
-		for resultItem in self.results_parser(xml_file=xmlOutput, report_level=0, **kwds): 
-			if isinstance(resultItem, parser.ILXMLProtein):
-				if resultItem.get_loops():
-					loopList = []
-					for aLoop in resultItem.get_loops():
-						loopList.append(aLoop.get_code())
-					loopList.sort()
-					parsedLoops[resultItem.get_name()] = ";".join(loopList)
-
-		return parsedLoops
-
-	def parseNoLoops(self, iLoopsPath, xmlOutput, **kwds):
-		with open(iLoopsPath + "noLoops.li", "a") as noLoops:
-			for resultItem in self.results_parser(xml_file=xmlOutput, report_level=0, **kwds): 
-				if isinstance(resultItem, parser.ILXMLProtein):
-					noLoops.write(resultItem.get_name() + "\n")
-
+import include.custom_iLoops_xml_parser as parser
 
 def writeFasta(basename, inputType, expressedTranscripts):
 	wannaWrite = False
@@ -66,14 +40,17 @@ def writeFasta(basename, inputType, expressedTranscripts):
 			elif wannaWrite:
 				MULTIFASTA.write(line)
 
-def parseMapping(iLoopsFolder):
-	loopFamilies = {}
 
-	for mappingBatch in filter(os.listdir(iLoopsFolder + "Output/"), "Mapping_*"):
+def parseMapping(iLoopsFolder, tag):
+	loopFamilies = {}
+	noLoops = set()
+	loopPatternModel = set()
+
+	for mappingBatch in filter(os.listdir(iLoopsFolder + "Output/"), tag + "_*"):
 		xmlFile = iLoopsFolder + "Output/" + mappingBatch + "/" + mappingBatch + ".xml"
 		errFile = iLoopsFolder + "Output/" + mappingBatch + "/" + mappingBatch + ".err.xml"
 
-		newLoops = myParser.parseLoops(xmlOutput					   = xmlFile,
+		newLoops = myParser.parseLoops(xmlOutput					 = xmlFile,
 									   output_proteins               = True, 
 									   output_alignments             = False,
 									   output_domain_mappings        = False,
@@ -91,21 +68,22 @@ def parseMapping(iLoopsFolder):
 
 			loopFamilies[loops].append(aTranscript)
 
-		myParser.parseNoLoops(xmlOutput                     = errFile,
-							  iLoopsPath 					= iLoopsFolder,
-							  output_proteins               = True, 
-							  output_alignments             = False,
-							  output_domain_mappings        = False,
-							  output_protein_features       = False,
-							  output_domain_assignations    = False,
-							  output_interactions           = False,
-							  output_interaction_signatures = False,
-							  output_RF_results             = False,
-							  output_RF_precisions          = False
-							 )
+		newNoLoops = myParser.parseNoLoops(xmlOutput                     = errFile,
+									    iLoopsPath                    = iLoopsFolder,
+									    output_proteins               = True, 
+									    output_alignments             = False,
+									    output_domain_mappings        = False,
+									    output_protein_features       = False,
+									    output_domain_assignations    = False,
+									    output_interactions           = False,
+									    output_interaction_signatures = False,
+									    output_RF_results             = False,
+									    output_RF_precisions          = False
+									   )
+		for aTranscript in newNoLoops:
+			noLoops.add(aTranscript)		
 
-	loopPatternModel = set()
-	with open(iLoopsFolder + "loopFamilies.txt", "w") as loopFamiliesList:
+	with open(iLoopsFolder + tag + "_loopFamilies.txt", "w") as loopFamiliesList:
 		 for loopPattern in loopFamilies.keys():
 		 	loopPatternModel.add(loopFamilies[loopPattern][0])
 
@@ -113,7 +91,7 @@ def parseMapping(iLoopsFolder):
 		 	for transcript in loopFamilies[loopPattern][1:]:
 		 		loopFamiliesList.write(transcript + "\n")
 
-	return loopPatternModel
+	return loopPatternModel, noLoops
 
 os.chdir(sys.argv[1])
 out = "Results/" + sys.argv[2]
@@ -123,80 +101,122 @@ inputType = sys.argv[5]
 iLoopsFolder = out + "/iLoops/"
 myParser = iLoopsParser()
 
-print("\t* Preparing FASTA files.")
+def getFASTAInput(iLoopsFolder, tag, inputType, transcripts):
 
-writeFasta(iLoopsFolder + "ExpressedTranscripts", inputType, expressedTranscripts)
+	writeFasta(iLoopsFolder + tag, inputType, transcripts)
 
-for expressedFasta in filter(os.listdir(iLoopsFolder), "ExpressedTranscripts_*.fasta"):
-	
-	assignationBatch = expressedFasta.split("_")[1].split(".")[0]
-	cmd("/soft/devel/python-2.7/bin/python /sbi/programs/iLoops_devel/iLoops.py",
-		"-f " + iLoopsFolder + expressedFasta,
-		"-j " + iLoopsFolder + "Output/Mapping_" + assignationBatch,
-		"-x Mapping_" + assignationBatch + ".xml",
-		"-v",
-		"-m",
-		"-n 25"
-	   )
-
-loopPatternReps = parseMapping(iLoopsFolder)
-writeFasta(iLoopsFolder + "ExpressedTranscripts.uniqLoops", inputType, loopPatternReps)
-
-print("\t* Writing the pairs files.")
-top = 0
-
-with open(candidateTranscripts, "r") as CANDIDATES:
-	CANDIDATES.readline()
-	for line in CANDIDATES:
-		elements = line.split("\t")
+	for expressedFasta in filter(os.listdir(iLoopsFolder), tag + "_*.fasta"):
 		
-		for aCandidate in [elements[2], elements[3]]:
-	
-			cmd("mkdir " + out + "/iLoops/Input/" + aCandidate)
+		assignationBatch = expressedFasta.split("_")[1].split(".")[0]
+		cmd("/soft/devel/python-2.7/bin/python /sbi/programs/iLoops_devel/iLoops.py",
+			"-f " + iLoopsFolder + expressedFasta,
+			"-j " + iLoopsFolder + "Output/Mapping_" + tag + "_" + assignationBatch,
+			"-x Mapping_" + tag + "_" + assignationBatch + ".xml",
+			"-v",
+			"-m",
+			"-n 25"
+		   )
 
-			for expressedFasta in filter(os.listdir(iLoopsFolder), "ExpressedTranscripts.uniqLoops_*.fasta"):
-				cmd("cp", iLoopsFolder + expressedFasta, iLoopsFolder + "Input/" + aCandidate)
-				with open(iLoopsFolder + "Input/" + aCandidate + "/" + expressedFasta, "a") as exprFast, \
-					 open("Data/" + inputType + "/proteins.fa", "r") as motherFast:
+	loopPatternReps, noLoops = parseMapping(iLoopsFolder, "Mapping_" + tag)
 
-					 fileNumber = (expressedFasta.split(".")[1]).split("_")[1]
+	writeFasta(iLoopsFolder + tag + ".uniqLoops", inputType, loopPatternReps)
 
-					 writeSeq = False
-					 for line in motherFast:
-					 	if line.find(">") != -1:
-					 		writeSeq = False
-					 		if line.find(aCandidate) != -1:
-					 			writeSeq = True
-								exprFast.write(">" + aCandidate + "\n")
-						elif writeSeq:
-					 		exprFast.write(line)
+	with open(iLoopsPath + tag + "_noLoops.li", "a") as NOLOOPS:
+		for aTranscript in noLoops:
+			NOLOOPS.write(aTranscript + "\n")
 
-				with open(iLoopsFolder + "Input/" + aCandidate + '/' + aCandidate + '_' + fileNumber + '.net', "w") as PAIRS, \
-					 open(iLoopsFolder + "Input/" + aCandidate + "/" + expressedFasta, "r") as exprFast:
-					for rawExpressed in exprFast:
-						if rawExpressed.find(">") == -1:
-							continue
-						expressedTranscript = rawExpressed.strip()[1:]
-						PAIRS.write(aCandidate + "\t" + expressedTranscript + "\n")
-						
-		top +=1
-		if top >=30:
-			break
+	return loopPatternReps, noLoops
+
+def getPairsInput(iLoopsFolder, goodCandidates):
+	for aCandidate in goodCandidates:
+
+		cmd("mkdir " + iLoopsFolder + aCandidate)
+		for expressedFasta in filter(os.listdir(iLoopsFolder), "ExpressedTranscripts.uniqLoops_*.fasta"):
+			cmd("cp", iLoopsFolder + expressedFasta, iLoopsFolder + "Input/" + aCandidate)
+
+			with open(iLoopsFolder + "Input/" + aCandidate + "/" + expressedFasta, "a") as exprFast, \
+				 open("Data/" + inputType + "/proteins.fa", "r") as motherFast:
+				fileNumber = (expressedFasta.split(".")[1]).split("_")[1]
+				writeSeq = False
+				
+				for line in motherFast:
+					if line.find(">") != -1:
+						writeSeq = False
+						if line.find(aCandidate) != -1:
+							writeSeq = True
+						exprFast.write(">" + aCandidate + "\n")
+					elif writeSeq:
+						exprFast.write(line)
+			with open(iLoopsFolder + "Input/" + aCandidate + '/' + aCandidate + '_' + fileNumber + '.net', "w") as PAIRS, \
+				 open(iLoopsFolder + "Input/" + aCandidate + "/" + expressedFasta, "r") as exprFast:
+				for rawExpressed in exprFast:
+					if rawExpressed.find(">") == -1:
+						continue
+					expressedTranscript = rawExpressed.strip()[1:]
+					PAIRS.write(aCandidate + "\t" + expressedTranscript + "\n")
+		
+def getFASTAandPairs(iLoopsFolder, inputType, candidates):
+	candidates = set()
+	candidatePairs = []
+
+	with open(transcripts, "r") as CANDIDATES:
+
+		CANDIDATES.readline()
+		top = 0
+
+		for line in CANDIDATES:
+			elements = line.split("\t")
+			candidates.add(elements[2])
+			candidates.add(elements[3])
+			candidatePairs.append[elements[2], elements[3]]
+			
+			top += 1
+			if top >= 10:
+				break
+
+	loopPatternReps, noLoops = getFASTAInput(iLoopsFolder, "Candidate", inputType, candidateTranscripts)
+
+	toDelete = []
+
+	for candidatePair in candidatePairs:
+		for candidate in candidatePair:
+			if candidate in noLoops:
+				toDelete.append(candidatePair)
+
+		for loop in loopPatternReps.keys():
+			if candidatePair[0] in loopPatternReps[loop] and candidatePair[1] in loopPatternReps[loop]:
+				toDelete.append(candidatePair)
+
+	for dele in toDelete:
+		candidatePairs.remove(dele)
+
+	getPairsInput(iLoopsFolder, candidatePairs)
+
+	return candidatePairs
+
+
+print("\t* Preparing FASTA files for all transcripts.")
+getFASTAInput(iLoopsFolder, "Expressed", inputType, expressedTranscripts)
+
+print("\t* Checking loop mapping for top candidates and preparing input.")
+goodCandidates = getFASTAandPairs(iLoopsFolder, inputType, candidateTranscripts)
 
 print("\t* Launching iLoops.")
-for transcript in filter(os.listdir(iLoopsFolder + "/Input"), "ENST*"):
-	for configFile in filter(os.listdir(iLoopsFolder + "/Input/" + transcript), "*net"):
-		batch = (configFile.split(".")[0]).split("_")[1]
-		cmd("/soft/devel/python-2.7/bin/python /sbi/programs/iLoops_devel/iLoops.py",
-			"-f " + iLoopsFolder + "/Input/" + transcript + "/" + "ExpressedTranscripts.uniqLoops_" + batch + ".fasta",
-			"-q " + iLoopsFolder + "/Input/" + transcript + "/" + configFile,
-			"-j " + iLoopsFolder + "/Output/" + configFile,
-			"-x " + configFile + ".xml",
-			"-g all",
-			"-n 25",
-			"-Q sbi",
-			"-c 1,5,6,7,8,9,10,11,12,13,14,15,20,30,40,50",
-			"-v"
-			)
 
-cmd("scp -r " + iLoopsFolder + " hector@feynman.imim.es:~/SmartAS/Results/")
+for transcriptPair in goodCandidates:
+	for transcript in transcriptPair:
+		for configFile in filter(os.listdir(iLoopsFolder + "/Input/" + transcript), "*net"):
+			batch = (configFile.split(".")[0]).split("_")[1]
+			cmd("/soft/devel/python-2.7/bin/python /sbi/programs/iLoops_devel/iLoops.py",
+				"-f " + iLoopsFolder + "/Input/" + transcript + "/" + "ExpressedTranscripts.uniqLoops_" + batch + ".fasta",
+				"-q " + iLoopsFolder + "/Input/" + transcript + "/" + configFile,
+				"-j " + iLoopsFolder + "/Output/" + configFile,
+				"-x " + configFile + ".xml",
+				"-g all",
+				"-n 25",
+				"-Q sbi",
+				"-c 1,5,6,7,8,9,10,11,12,13,14,15,20,30,40,50",
+				"-v"
+			   )
+
+cmd("scp -r " + iLoopsFolder + " hector@feynman.imim.es:~/SmartAS/" + out)
