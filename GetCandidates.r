7a64971e37d3e2fdc8a2dfcf96621058e0d10bb3
Héctor Climente <hclimente@gmail.com>
2014-03-12 18:40:58 +0100

    Transcript is considered expressed in the threshold is surpassed in any of the conditions. The threshold for considering a candidate expressed is the same as for any other effect. The genename of the expressed transcript is written too.

--- GetCandidates.r
+++ GetCandidates.r
@@ -4,11 +4,10 @@ library(plyr)
 
 args <- commandArgs(trailingOnly = TRUE)
 minExpression <- as.numeric(args[1])
-minCandidateExpression <- as.numeric(args[2])
-load(paste0("Results/", args[3], "/RWorkspaces/1_ExploreData.RData"))
+load(paste0("Results/", args[2], "/RWorkspaces/1_ExploreData.RData"))
 
 candidates <- list()
-allGenes <- as.character()
+replicateExpressed  <- list()
 
 for (replicate in seq(1,inputData[["Replicates"]])){
   
@@ -17,75 +16,80 @@ for (replicate in seq(1,inputData[["Replicates"]])){
   candidates[[replicate]] <- data.frame(Gene=as.character(), Genename=as.character(), Switch=as.numeric(), maxdPSI=as.character(), mindPSI=as.character())
   
   #Filter by deltaPSI and expression, based on the FPR
-  #psiThreshold <- abs(intraReplicate[[replicate]]$deltaPSI) > 0.15
   psiThreshold <- abs(intraReplicate[[replicate]]$deltaPSI) > 4 * interReplicate$MAD
   psiThreshold[is.na(psiThreshold)] <- FALSE
-  posPSI <- intraReplicate[[replicate]]$deltaPSI > 0 & psiThreshold
-  negPSI <- intraReplicate[[replicate]]$deltaPSI < 0 & psiThreshold
-  expressionThreshold <- intraReplicate[[replicate]]$la_tTPM > minCandidateExpression
   
-  replicateCandidates <- vector('list', length(unique(intraReplicate[[replicate]]$Gene[psiThreshold & expressionThreshold])))
+  norExpression <- log(intraReplicate[[replicate]]$TPM_N) > minExpression
+  tumExpression <- log(intraReplicate[[replicate]]$TPM_T) > minExpression
+  
+  replicateCandidates <- vector('list', length(unique(intraReplicate[[replicate]]$Gene[psiThreshold]))) # & expressionThreshold])))
 
-  for (aCandidate in unique(intraReplicate[[replicate]]$Gene[psiThreshold & expressionThreshold])){
+  for (aCandidate in unique(intraReplicate[[replicate]]$Gene[psiThreshold])){ # & expressionThreshold])){
     
     thisGeneData <- intraReplicate[[replicate]]$Gene == aCandidate
 
-    if(length(intraReplicate[[replicate]]$deltaPSI[thisGeneData & posPSI]) == 0 || length(intraReplicate[[replicate]]$deltaPSI[thisGeneData & negPSI]) == 0){
+    norTranscripts <- intraReplicate[[replicate]]$deltaPSI > 0 & psiThreshold & norExpression & thisGeneData
+    tumTranscripts <- intraReplicate[[replicate]]$deltaPSI < 0 & psiThreshold & tumExpression & thisGeneData
+
+    if(length(intraReplicate[[replicate]]$deltaPSI[norTranscripts]) == 0 || length(intraReplicate[[replicate]]$deltaPSI[tumTranscripts]) == 0){
       next
     }
     
-    #Calculate max difference between the transcripts
-    maxDeltaPsi <- max(intraReplicate[[replicate]]$deltaPSI[thisGeneData & posPSI], na.rm=T)
-    minDeltaPsi <- min(intraReplicate[[replicate]]$deltaPSI[thisGeneData & negPSI], na.rm=T)
-    maxSwitch <- maxDeltaPsi - minDeltaPsi
-    
-    #deltaPSI = PSI_ref - PSI_alt
-    #	MaxDeltaPsiCond: predominant transcript in the normal sample
-    #	MinDeltaPsiCond: predominant transcript in the tumor sample
-    maxDeltaPsiCond <- intraReplicate[[replicate]]$deltaPSI == maxDeltaPsi
-    minDeltaPsiCond <- intraReplicate[[replicate]]$deltaPSI == minDeltaPsi
+    #Calculate max difference between the transcripts and choose the candidates
+    #     deltaPSI = PSI_ref - PSI_alt
+    # MaxDeltaPsiCond: predominant transcript in the normal sample
+    # MinDeltaPsiCond: predominant transcript in the tumor sample
+    maxDeltaPsi <- max(intraReplicate[[replicate]]$deltaPSI[thisGeneData & norTranscripts], na.rm=T)
+    norCandidate <- intraReplicate[[replicate]]$deltaPSI == maxDeltaPsi
 
-    replicateCandidates[[aCandidate]] <- data.frame(Genename=intraReplicate[[replicate]]$Genename[thisGeneData & maxDeltaPsiCond], 
+    minDeltaPsi <- min(intraReplicate[[replicate]]$deltaPSI[thisGeneData & tumTranscripts], na.rm=T)
+    tumCandidate <- intraReplicate[[replicate]]$deltaPSI == minDeltaPsi
+
+    Switch <- maxDeltaPsi - minDeltaPsi
+  
+    if (Switch < 0.2){
+      next
+    }
+
+    replicateCandidates[[aCandidate]] <- data.frame(Genename=intraReplicate[[replicate]]$Genename[thisGeneData & norCandidate], 
                                                     Gene=aCandidate,
-                                                    maxdPSI=intraReplicate[[replicate]]$Transcript[thisGeneData & maxDeltaPsiCond], 
-                                                    mindPSI=intraReplicate[[replicate]]$Transcript[thisGeneData & minDeltaPsiCond],
-                                                    Switch=maxSwitch
-                                                   )
+                                                    maxdPSI=intraReplicate[[replicate]]$Transcript[thisGeneData & norCandidate], 
+                                                    mindPSI=intraReplicate[[replicate]]$Transcript[thisGeneData & tumCandidate],
+                                                    Switch=Switch)
   }
 
   candidates[[replicate]] <- do.call('rbind', replicateCandidates)
-  
-  #Expressed genes: transcript whose expression is above the threshold
-  expressedGenes <-(log(as.numeric(intraReplicate[[replicate]]$TPM_N)) + log(as.numeric(intraReplicate[[replicate]]$TPM_T))) / 2 > minExpression
-  allGenes <- unique(c(allGenes, intraReplicate[[replicate]]$Transcript[expressedGenes]))
-  
-  switchCut <- candidates[[replicate]]$Switch > 0.2
-  
-  candidates[[replicate]] <- candidates[[replicate]] [switchCut, c("Genename", "Gene", "maxdPSI","mindPSI", "Switch")]
 
-  cat(":", nrow(candidates[[replicate]]), "candidates found\n")
+  #Expressed genes: transcripts whose expression are above the expression threshold in any of the conditions
+  replicateExpressed[[replicate]] <- data.frame(Transcript=intraReplicate[[replicate]]$Transcript[norExpression | tumExpression],
+                                                Genename=intraReplicate[[replicate]]$Genename[norExpression | tumExpression])
+
+  cat(":", nrow(candidates[[replicate]]), "candidates found.\n")
   
 }
+
+allExpressedTranscripts <- unique(do.call('rbind', replicateExpressed))
 
 candidateList <- do.call("rbind", candidates)
 candidateList <- ddply(candidateList,.(Genename,Gene,maxdPSI,mindPSI), summarise, Replicated=length(Genename))
 candidateList <- candidateList[with(candidateList, order(-Replicated)), ]
 
 write.table(candidateList, file=paste0(out, "candidateList.tsv"), sep="\t", row.names=F, col.names=F, quote=F)
-write(allGenes, paste0(out, "expressedGenes.lst"), sep="\n")
+write.table(allExpressedTranscripts, paste0(out, "expressedGenes.lst"), sep="\t", row.names=F, col.names=F, quote=F)
 
 save(isoformExpression, intraReplicate, interReplicate, candidates, candidateList, inputData, wd, out, file=paste0(out, "RWorkspaces/2_GetCandidates.RData"))
 
+#Plot heatmap
 library(gplots)
 library(RColorBrewer)
 
-top <- 30
+top <- length(candidateList$Genename[candidateList$Replicated >= inputData[["Replicates"]] * 0.2])
 
 topCandidates <- ddply(candidateList,.(Genename), summarise, Replicated=sum(Replicated))
 topCandidates <- topCandidates[with(topCandidates, order(-Replicated)), ]
 topCandidates <- head(topCandidates, n=top)
 
-fig <- data.frame(matrix(nrow=top, ncol=inputData[["Replicates"]]))
+fig <- data.frame(matrix(nrow=length(topCandidates$Genename), ncol=inputData[["Replicates"]]))
 rownames(fig) <- topCandidates$Genename
 colnames(fig) <- seq(1,inputData[["Replicates"]])
 
@@ -94,7 +98,7 @@ for (replicate in seq(1,inputData[["Replicates"]])){
     if (gene %in% candidates[[replicate]]$Genename) {
       fig[gene, replicate] <- head(candidates[[replicate]]$Switch[candidates[[replicate]]$Genename == gene],1)
     } else {
-      fig[gene, replicate] <- NA
+      fig[gene, replicate] <- 0
     }
   }
 }
