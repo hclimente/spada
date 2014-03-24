bfdf11aeeb2b9f46fad8de8038472c400e20cfb0
Héctor Climente <hclimente@gmail.com>
2014-03-24 18:02:26 +0100

    Added support for external file launchers.

--- scripts/tcgaLauncher.sh
+++ scripts/tcgaLauncher.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-while read tag
-do
-	echo === $tag ===
-	Pipeline/scripts/standarizeInput.py TCGA $tag
-	echo Input standarized
-	Pipeline/SmartAS.py -f Data/Input/TCGA/$tag/config.cfg 2>&1
+fileList=$1
+origin=$2
 
-done < $1
+while read line
+do
+	echo === $line ===
+	if [[ $origin -eq "ext" ]]; then
+		if [[ ! -f Data/TCGA/External/"$line"_expressedGenes.lst ]]; then
+			echo Data/TCGA/External/"$line"_expressedGenes.lst
+			continue
+		fi
+		cat michal.cfg >$line.cfg
+   		echo tag1=$line >>$line.cfg
+		echo external=Data/TCGA/External/$line >>$line.cfg
+   		Pipeline/SmartAS.py -f $line.cfg 2>&1
+
+   		rm $line.cfg
+    else
+		Pipeline/scripts/standarizeInput.py TCGA $line
+		echo Input standarized
+		Pipeline/SmartAS.py -f Data/Input/TCGA/$line/config.cfg 2>&1
+	fi
+
+done < "$fileList"
 
 #Data/Input/TCGA_tags.txt
