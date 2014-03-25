b3cee74c89eb250da06772fe190c76ad57143629
Héctor Climente <hclimente@gmail.com>
2014-03-25 17:15:59 +0100

    Almost functional.

--- scripts/InputUnpaired.r
+++ scripts/InputUnpaired.r
@@ -5,34 +5,27 @@ load(paste0("Results/", args[1], "/RWorkspaces/1_ExploreData.RData"))
 unpairedReplicates <- as.numeric(args[2])
 inputPath <- args[3]
 
+intraReplicate <- list()
+
 for (replicate in seq(1, unpairedReplicates)){
-  cat("\t* Exploring tumor replicate",replicate, "/", inputData[["Replicates"]], "\t")
+  cat("\t* Exploring tumor replicate",replicate, "/", unpairedReplicates, "\n")
 
   inputFile=paste0(inputPath, replicate, "_T.tsv")
   outputFile=paste0(out, replicate, "_T.tsv")
-    
-  isoformExpression <- read.table(inputFile, header=F, sep="\t", stringsAsFactors=F)
-  colnames(isoformExpression) <- c("Gene", "Transcript","Genename","TPM")
-  vtTPM <- aggregate(TPM ~ Gene, data = isoformExpression, FUN = "sum")
-  colnames(vtTPM) <- c("Gene", "tTPM")
-  isoformExpression <- merge(isoformExpression, vtTPM)
-  isoformExpression <- transform(isoformExpression, PSI = TPM / tTPM)
-    
-  if(!exists("interReplicate_T")){
-    interReplicate_T <- isoformExpression
-    interReplicate_T <- interReplicate_T[,!(colnames(interReplicate_T) %in% c("TPM", "tTPM")), drop=FALSE]
-    
-    columns <- c("Gene", "Transcript", "Genename", paste0("PSI_", replicate_c))
-    
+
+  intraReplicate[[replicate]] <- read.table(inputFile, header=F, sep="\t", stringsAsFactors=F)
+  colnames(intraReplicate[[replicate]]) <- c("Gene", "Transcript","Genename","TPM_T")
+  vtTPM <- aggregate(TPM_T ~ Gene, data = intraReplicate[[replicate]], FUN = "sum")
+  colnames(vtTPM) <- c("Gene", "tTPM_T")
+  intraReplicate[[replicate]] <- merge(intraReplicate[[replicate]], vtTPM)
+  intraReplicate[[replicate]] <- transform(intraReplicate[[replicate]], PSI_T = TPM_T / tTPM)
+  intraReplicate[[replicate]]$TPM_N = 9999
+  if all(intraReplicate[[replicate]]$Transcript == interReplicate_N$Transcript){
+    intraReplicate[[replicate]]$deltaPSI <- 0.6745 * (intraReplicate[[replicate]]$PSI - interReplicate_N$MedianPSI) / interReplicate_N$MAD
   } else {
-    interReplicate_T <- merge(interReplicate_T, intraReplicate[[replicate]], by=c("Gene", "Transcript", "Genename"), suffixes=c("",paste0("_", replicate_c)), all=T)
-    interReplicate_T <- interReplicate_T[,!(colnames(interReplicate_T) %in% delete), drop=FALSE]
-    
-    columns <- c(columns, paste0("PSI_", replicate_c))
+    cat("Error\n")
   }
+
 }
 
-interReplicate_T$MedianPSI <- apply(interReplicate_T[,psiCols], 1, median)
-interReplicate_T$MAD <- apply(interReplicate_T[,psiCols], 1, mad)
-
-save(interReplicate_T, file=paste0(out, "RWorkspaces/1_Tumor_Intereplicate.RData"))
+save(intraReplicate, file=paste0(out, "RWorkspaces/1_Tumor_Intereplicate.RData"))
