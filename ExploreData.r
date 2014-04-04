c0642a5739b8a860e54ed0f79fac56cdefaa4d87
Héctor Climente <hclimente@gmail.com>
2014-04-04 18:46:39 +0200

    Create interReplicate comparison for each condition, and with data about TPM and tTPM too.

--- ExploreData.r
+++ ExploreData.r
@@ -5,6 +5,7 @@ load(paste0("Results/", args[1], "/RWorkspaces/0_InitialEnvironment.RData"))
 inputPath <- args[2]
 
 intraReplicate <- list()
+interReplicate <- list(N=NULL, T=NULL)
 isoformExpression <- list()
 
 printTPMHist <- function(x, xLab, pngName){
@@ -36,6 +37,20 @@ simplePlot <- function(x, y, title, xLab, yLab, pngName){
   png(pngName, width=960, height=960)
   plot(x, y, main=title, xlab=xLab, ylab=yLab)
   graphics.off()
+}
+
+calculateMAD <- function(x){
+
+  result <- apply(x, 1, mad, na.rm=T)
+  
+  mad0_mask <- result == 0
+  mad0_mask[is.na(mad0_mask)] <- TRUE
+  mad0 <- x[mad0_mask, ]
+  
+  result[mad0_mask] <- apply(mad0, 1, function(x) mad(x,center = mean(as.numeric(x),na.rm=T), constant = 1.253314, na.rm = TRUE))
+  
+  return(result)
+
 }
 
 for (replicate in seq(1, inputData[["Replicates"]])){
@@ -77,66 +92,71 @@ for (replicate in seq(1, inputData[["Replicates"]])){
 
 }
 
-psiCols <- as.character()
-delete <- c("TPM_T","tTPM_T", "PSI_T", "deltaPSI", "la_tTPM", "TPM_N", "tTPM_N")
+tpmCols <- paste0("TPM_", seq(1,inputData[["Replicates"]]) )
+ttpmCols <- paste0("tTPM_", seq(1,inputData[["Replicates"]]) )
+psiCols <- paste0("PSI_", seq(1,inputData[["Replicates"]]) )
 
-for (replicate in seq(1,inputData[["Replicates"]])){
+all <- c(rbind(tpmCols, ttpmCols, psiCols))
 
-  replicate_c <- as.character(replicate)
-      
-  if(!exists("interReplicate_N")){
-    interReplicate_N <- intraReplicate[[replicate]]
-    interReplicate_N <- interReplicate_N[,!(colnames(interReplicate_N) %in% delete), drop=FALSE]
-    
-    columns <- c("Gene", "Transcript", "Genename", paste0("PSI_", replicate_c))
-    
-  } else {
-    interReplicate_N <- merge(interReplicate_N, intraReplicate[[replicate]], by=c("Gene", "Transcript", "Genename"), suffixes=c("",paste0("_", replicate_c)), all=T)
-    interReplicate_N <- interReplicate_N[,!(colnames(interReplicate_N) %in% delete), drop=FALSE]
-    
-    columns <- c(columns, paste0("PSI_", replicate_c))
+delete <- list()
+delete[["N"]] <- c("TPM_T","tTPM_T", "PSI_T", "deltaPSI", "la_tTPM")
+delete[["T"]] <- c("TPM_N","tTPM_N", "PSI_N", "deltaPSI", "la_tTPM")
+
+for (sample in inputData[["Conditions"]]){
+
+  for (replicate in seq(1,inputData[["Replicates"]])){
+
+    replicate_c <- as.character(replicate)
+        
+    if(is.null(interReplicate[[sample]])){
+      interReplicate[[sample]] <- intraReplicate[[replicate]]    
+    } else {
+      interReplicate[[sample]] <- merge(interReplicate[[sample]], intraReplicate[[replicate]], by=c("Gene", "Transcript", "Genename"), suffixes=c("",paste0("_", replicate_c)), all=T)
+    }
+
+    interReplicate[[sample]] <- interReplicate[[sample]][,!(colnames(interReplicate[[sample]]) %in% delete[[sample]]), drop=FALSE]
+
+    simplePlot(intraReplicate[[replicate]]$la_tTPM, intraReplicate[[replicate]]$deltaPSI, replicate, "0.5·(log(sum tTPM_N) + log(sum tTPM_T) )", 
+               "deltaPSI", paste0(out, "DataExploration/latTPM_PSI_intrarreplicate",replicate,"_", sample,".png"))
+
   }
-  colnames(interReplicate_N) <- columns
-  psiCols <- c(psiCols, paste0("PSI_", replicate))
 
-  simplePlot(intraReplicate[[replicate]]$la_tTPM, intraReplicate[[replicate]]$deltaPSI, replicate, "0.5·(log(sum tTPM_N) + log(sum tTPM_T) )", 
-             "deltaPSI", paste0(out, "DataExploration/latTPM_PSI_intrarreplicate",replicate,".png"))
+  colnames(interReplicate[[sample]]) <- c("Gene", "Transcript", "Genename", all)
+
+  interReplicate[[sample]]$Median_PSI <- apply(interReplicate[[sample]][,psiCols], 1, median, na.rm=T)
+  interReplicate[[sample]]$MAD_PSI <- calculateMAD(interReplicate[[sample]][,psiCols])
+  interReplicate[[sample]]$Median_TPM <- apply(interReplicate[[sample]][,tpmCols], 1, median, na.rm=T)
+  interReplicate[[sample]]$MAD_TPM <- calculateMAD(interReplicate[[sample]][,tpmCols])
+  interReplicate[[sample]]$Median_tTPM <- apply(interReplicate[[sample]][,ttpmCols], 1, median, na.rm=T)
+  interReplicate[[sample]]$MAD_tTPM <- calculateMAD(interReplicate[[sample]][,ttpmCols])
 
 }
-
-interReplicate_N$MedianPSI <- apply(interReplicate_N[,psiCols], 1, median, na.rm=T)
-interReplicate_N$MAD <- apply(interReplicate_N[,psiCols], 1, mad, na.rm=T)
-
-mad0_mask <- interReplicate_N$MAD == 0
-mad0_mask[is.na(mad0_mask)] <- TRUE
-mad0 <- interReplicate_N[mad0_mask, psiCols]
-
-interReplicate_N$MAD[mad0_mask] <- apply(mad0, 1, function(x) mad(x,center = mean(as.numeric(x),na.rm=T), constant = 1.253314, na.rm = TRUE))
 
 fpr1 <- as.numeric()
 fpr5 <- as.numeric()
 fprMAD <- as.numeric()
 
-for (tx in interReplicate_N$Transcript){
-  thisTranscript <- interReplicate_N[interReplicate_N$Transcript==tx,]
+for (tx in interReplicate[["N"]]$Transcript){
+  thisTranscript <- interReplicate[["N"]][interReplicate[["N"]]$Transcript==tx,psiCols]
+  this4MAD <- 4 * interReplicate[["N"]]$MAD_PSI[interReplicate[["N"]]$Transcript==tx]
   
-  diffMatrix <- abs(outer(as.numeric(thisTranscript[,psiCols]),as.numeric(thisTranscript[,psiCols]),"-"))
+  diffMatrix <- abs(outer(as.numeric(thisTranscript),as.numeric(thisTranscript),"-"))
   subtraction <- diffMatrix[lower.tri(diffMatrix, diag = FALSE)]
   fpr1 <- c(fpr1, as.numeric( quantile(subtraction, 0.99, na.rm=T) ) )
   fpr5 <- c(fpr5, as.numeric( quantile(subtraction, 0.95, na.rm=T) ) )
-  if ( !any(is.na(subtraction) ) ) {
-    fprMAD <- c(fprMAD, 1 - ecdf(subtraction)(4 * thisTranscript$MAD) )
+  if ( any( !is.na(subtraction) ) ) {
+    fprMAD <- c(fprMAD, 1 - ecdf(subtraction)(this4MAD) )
   } else {
     fprMAD <- c(fprMAD, NA)
   }
 }
 
-interReplicate_N$FPR_1 <- fpr1
-interReplicate_N$FPR_5 <- fpr5
-interReplicate_N$FPR_4MAD <- fprMAD
+interReplicate[["N"]]$FPR_1 <- fpr1
+interReplicate[["N"]]$FPR_5 <- fpr5
+interReplicate[["N"]]$FPR_4MAD <- fprMAD
 
-simplePlot(interReplicate_N$MedianPSI, interReplicate_N$MAD, "InterReplicate_N", "Median PSI", "MAD", paste0(out, "DataExploration/Interreplicate_mean_MAD_N.png"))
-simplePlot(interReplicate_N$MedianPSI, interReplicate_N$FPR_1, "InterReplicate_N", "Median PSI", "FPR 1%", paste0(out, "DataExploration/Interreplicate_medianPSI_FRP1_N.png"))
-simplePlot(interReplicate_N$MedianPSI, interReplicate_N$FPR_5, "InterReplicate_N", "Median PSI", "FPR 5%", paste0(out, "DataExploration/Interreplicate_medianPSI_FRP5_N.png"))
+simplePlot(interReplicate[["N"]]$Median_PSI, interReplicate[["N"]]$MAD, "InterReplicate_N", "Median PSI", "MAD", paste0(out, "DataExploration/Interreplicate_mean_MAD_N.png"))
+simplePlot(interReplicate[["N"]]$Median_PSI, interReplicate[["N"]]$FPR_1, "InterReplicate_N", "Median PSI", "FPR 1%", paste0(out, "DataExploration/Interreplicate_medianPSI_FRP1_N.png"))
+simplePlot(interReplicate[["N"]]$Median_PSI, interReplicate[["N"]]$FPR_5, "InterReplicate_N", "Median PSI", "FPR 5%", paste0(out, "DataExploration/Interreplicate_medianPSI_FRP5_N.png"))
 
-save(isoformExpression, intraReplicate, interReplicate_N, inputData, wd, out, file=paste0(out, "RWorkspaces/1_ExploreData.RData"))
+save(isoformExpression, intraReplicate, interReplicate, inputData, wd, out, file=paste0(out, "RWorkspaces/1_ExploreData.RData"))
