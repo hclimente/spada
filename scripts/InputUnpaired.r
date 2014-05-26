5f3d5597886bbf7200a35f23c573c8944d24dc4e
Héctor Climente <hclimente@gmail.com>
2014-05-26 17:03:34 +0200

    Changes in calculation of robust Z-score.

--- scripts/InputUnpaired.r
+++ scripts/InputUnpaired.r
@@ -1,7 +1,9 @@
 #!/soft/R/R-3.0.0/bin/Rscript
 
 args <- commandArgs(trailingOnly = TRUE)
+
 load(paste0("Results/", args[1], "/RWorkspaces/1_ExploreData.RData"))
+out <- paste0("Results/", args[1], "_unpaired/")
 unpairedReplicates <- as.numeric(args[2])
 inputPath <- args[3]
 
@@ -18,14 +20,15 @@ for (replicate in seq(1, unpairedReplicates)){
   vtTPM <- aggregate(TPM_T ~ Gene, data = intraReplicate[[replicate]], FUN = "sum")
   colnames(vtTPM) <- c("Gene", "tTPM_T")
   intraReplicate[[replicate]] <- merge(intraReplicate[[replicate]], vtTPM)
-  intraReplicate[[replicate]] <- transform(intraReplicate[[replicate]], PSI_T = TPM_T / tTPM)
+  intraReplicate[[replicate]] <- transform(intraReplicate[[replicate]], PSI_T = TPM_T / tTPM_T)
   intraReplicate[[replicate]]$TPM_N = 9999
-  if all(intraReplicate[[replicate]]$Transcript == interReplicate_N$Transcript){
-    intraReplicate[[replicate]]$deltaPSI <- 0.6745 * (intraReplicate[[replicate]]$PSI - interReplicate_N$MedianPSI) / interReplicate_N$MAD
-  } else {
-    cat("Error\n")
-  }
 
+  tmp <- merge(intraReplicate[[replicate]], interReplicate[["T"]], by=c("Gene", "Genename", "Transcript") )
+  tmp <- tmp[,! names(tmp) %in% c(paste0("PSI_", seq(1,inputData[["Replicates"]]) ), paste0("TPM_", seq(1,inputData[["Replicates"]]) ), paste0("tTPM_", seq(1,inputData[["Replicates"]]) ) ) ]
+  #rzs <- sweep(tmp$PSI_T, 1, tmp$Median_PSI, "-")
+  #rzs <- sweep(rzs, 1, tmp$MAD_PSI, "/")
+  #tmp$deltaPSI <- rzs
+  intraReplicate[[replicate]]$deltaPSI <- (tmp$PSI_T - tmp$Median_PSI ) / tmp$MAD_PSI
 }
 
-save(intraReplicate, file=paste0(out, "RWorkspaces/1_Tumor_Intereplicate.RData"))
+save(intraReplicate, out, file=paste0(out, "RWorkspaces/1_Tumor_Intereplicate.RData"))
