6a1aaa559cf85acc29e3140c318b02096d72ec09
Héctor Climente <hclimente@gmail.com>
2014-07-23 12:55:46 +0200

    Statistics counters about location of isoform specific residues.
    Trivial changes.

--- methods/structural_analysis.py
+++ methods/structural_analysis.py
@@ -1,5 +1,6 @@
 #!/soft/devel/python-2.7/bin/python
 
+from libs import utils
 from network import biological_entities
 from network import gene_network, isoform_network
 from methods import method
@@ -7,11 +8,21 @@ from methods import method
 from collections import Counter
 from Bio import pairwise2
 
-import pdb
-
 class StructuralAnalysis(method.Method):
 	def __init__(self, gn_network, tx_network):
 		method.Method.__init__(self, __name__, gn_network, tx_network)
+		self._statistics = {}
+		self._statistics["isoformSpecific"] = {}
+		self._statistics["isoformSpecific"]["buried"] 					= 0
+		self._statistics["isoformSpecific"]["interactingSurface"] 		= 0
+		self._statistics["isoformSpecific"]["nonInteractingSurface"]	= 0
+		self._statistics["isoformSpecific"]["unknown"]					= 0
+
+		self._statistics["nonIsoformSpecific"] = {}
+		self._statistics["nonIsoformSpecific"]["buried"] 				= 0
+		self._statistics["nonIsoformSpecific"]["interactingSurface"] 	= 0
+		self._statistics["nonIsoformSpecific"]["nonInteractingSurface"] = 0
+		self._statistics["nonIsoformSpecific"]["unknown"]				= 0
 
 	def run(self):
 		self.logger.info("Structural analysis.")
@@ -20,7 +31,7 @@ class StructuralAnalysis(method.Method):
 		for gene, properties in sortedNodes:
 			if not self._gene_network._net.node[gene]["isoformSwitches"]: continue
 			self.logger.debug("Searching structural information for gene {0}.".format(gene))
-			switch = self._gene_network._net.node[gene]["isoformSwitches"][0]
+			switch = self._gene_network._net.node[gene]["isoformSwitches"][0][0]
 			nIso = switch[0]
 			tIso = switch[1]
 
@@ -28,13 +39,17 @@ class StructuralAnalysis(method.Method):
 											nIso, 
 											self._transcript_network._net.node[nIso]["Uniprot"], 
 											self._transcript_network._net.node[nIso]["proteinSequence"], 
-											self._transcript_network._net.node[nIso]["exonStructure"]
+											self._transcript_network._net.node[nIso]["exonStructure"],
+											self._transcript_network._net.node[nIso]["cdsCoords"],
+											self._transcript_network._net.node[nIso]["strand"]
 														)
 			tumorProtein = biological_entities.Protein( 
 											tIso, 
 											self._transcript_network._net.node[tIso]["Uniprot"], 
 											self._transcript_network._net.node[tIso]["proteinSequence"], 
-											self._transcript_network._net.node[tIso]["exonStructure"]
+											self._transcript_network._net.node[tIso]["exonStructure"],
+											self._transcript_network._net.node[tIso]["cdsCoords"],
+											self._transcript_network._net.node[tIso]["strand"]
 													  )
 
 			if self._transcript_network._net.node[nIso]["Uniprot"] is not None:
@@ -47,13 +62,14 @@ class StructuralAnalysis(method.Method):
 				continue
 			
 			self.logger.info("Structural information found for gene {0}.".format(gene))
-			pdb.set_trace()
 
 			if normalProtein.hasPdbs:
 				normalProtein.getAlteredRegions(tumorProtein)
+				self.getStatistics(normalProtein)
 				normalProtein.report()
 			if tumorProtein.hasPdbs:
 				tumorProtein.getAlteredRegions(normalProtein)
+				self.getStatistics(tumorProtein)
 				tumorProtein.report()
 
 	def analyzeStructuralImpact(self, protein):
@@ -71,14 +87,37 @@ class StructuralAnalysis(method.Method):
 			elif protein.uniprot == line[1]:
 				self.logger.debug("Relevant interaction for {0} at {1}.".format(
 															protein.tx, interactionPdb))
-				protein.calculateVolumes(interactionPdb, "B")
+				protein.calculateVolumes(interactionPdb, "B", self._transcript_network._net.node[protein.tx]["exonStructure"])
 				noInteractions = False
 
 		if noInteractions:
-			self.logger.debug("No relevant structures found for {0}.".format(protein.tx))
+			self.logger.debug("No relevant structures found for {0}, {1}.".format(protein.tx,protein.uniprot))
 			return False
 
 		return True
+
+	def getStatistics(self, protein):
+		for residue in protein._structure: 
+			if residue.isoformSpecific:
+				if not residue.tag:
+				 	self._statistics["isoformSpecific"]["unknown"] 				+= 1
+				elif residue.tag == "IS":
+					self._statistics["isoformSpecific"]["interactingSurface"] 	+= 1
+				elif residue.tag == "NIS":
+				 	self._statistics["isoformSpecific"]["nonInteractingSurface"]+= 1
+				elif residue.tag == "B":
+				 	self._statistics["isoformSpecific"]["buried"]				+= 1
+
+			else:
+				if not residue.tag:
+				 	self._statistics["nonIsoformSpecific"]["unknown"] 				+= 1
+				elif residue.tag == "IS":
+					self._statistics["nonIsoformSpecific"]["interactingSurface"] 	+= 1
+				elif residue.tag == "NIS":
+				 	self._statistics["nonIsoformSpecific"]["nonInteractingSurface"]	+= 1
+				elif residue.tag == "B":
+				 	self._statistics["nonIsoformSpecific"]["buried"]				+= 1
+
 
 if __name__ == '__main__':
 	pass
