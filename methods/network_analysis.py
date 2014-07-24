bfa3cae1f72aef6e006431734f3f9b27b51db3cb
Héctor Climente <hclimente@gmail.com>
2014-07-24 16:10:47 +0200

    Added use of GUILD subfolder.
    Corrected reading from GUILD output.
    
    Function to select top x GUILD nodes added.

--- methods/network_analysis.py
+++ methods/network_analysis.py
@@ -10,33 +10,43 @@ from libs.guild.src import combine_scores
 from libs.guild.src import create_random_networks_for_netzcore
 
 import pandas as pd
+import networkx
 
 class NetworkAnalysis(method.Method):
 	def __init__(self, gn_network, tx_network):
 		method.Method.__init__(self, __name__, gn_network, tx_network)
 
-	def run(self):
-		self.logger.info("GUILD analysis.")
-		out_network.Output().getGUILDInput(self._gene_network)
+	def run(self, onlyExperimental):
+		self.logger.info("Network analysis.")
+		out_network.getGUILDInput(self._gene_network, onlyExperimental=onlyExperimental)
 
 		self.runGUILD( method="NetScore", reps=3, iters=2 )
 		self.runGUILD( method="NetZcore", samples=100 )
- 		self.runGUILD( method="NetShort" )
- 		self.runGUILD( method="fFlow", iters=5, min_seed_score=1.0 )
- 		self.runGUILD( method="NetRank" )
+		self.runGUILD( method="NetShort" )
+		self.runGUILD( method="fFlow", iters=5, min_seed_score=1.0 )
+		self.runGUILD( method="NetRank" )
 
-		netComboInput = [ "{0}guild_{1}.out".format(options.Options().qout,x) for x in ["NetScore", "NetZcore", "NetShort"] ]
-		combine_scores.score_combined(netComboInput, options.Options().qout + "guild_netCombo.out")
+		netComboInput = [ "{0}GUILD/guild_{1}.out".format(options.Options().qout,x) for x in ["NetScore", "NetZcore", "NetShort"] ]
+		combine_scores.score_combined(netComboInput, options.Options().qout + "GUILD/guild_netCombo.out")
 
-		fullComboInput = [ "{0}guild_{1}.out".format(options.Options().qout,x) for x in ["NetScore", "NetZcore", "NetShort", "fFlow", "NetRank"] ]
-		combine_scores.score_combined(fullComboInput, options.Options().qout + "guild_fullCombo.out")
+		fullComboInput = [ "{0}GUILD/guild_{1}.out".format(options.Options().qout,x) for x in ["NetScore", "NetZcore", "NetShort", "fFlow", "NetRank"] ]
+		combine_scores.score_combined(fullComboInput, options.Options().qout + "GUILD/guild_fullCombo.out")
+
+		self.readGUILDOutput("GUILD/guild_fullCombo.out")
+		self.extractTopSubnetwork(1)
+		self.extractTopSubnetwork(5)
 
 	def runGUILD(self, method, reps=None, iters=None, samples=None, min_seed_score=None):
+
+		self.logger.info("Running GUILD, method {0}.".format(method))
+
+		oneLetter = {"NetScore": "s", "NetZcore": "z", "NetShort": "d", "fFlow": "f", "NetRank": "r"}
+
 		guild = ["{0}Pipeline/libs/guild/guild".format(options.Options().wd)]
-		guild.append( "-n {0}guild_nodes.tsv".format(options.Options().qout) )
-		guild.append( "-e {0}guild_edges.tsv".format(options.Options().qout) )
-		guild.append( "-s {0}".format(method) )
-		guild.append( "-o {0}guild_{1}.out".format(options.Options().qout,method) )
+		guild.append( "-n {0}GUILD/guild_nodes.tsv".format(options.Options().qout) )
+		guild.append( "-e {0}GUILD/guild_edges.tsv".format(options.Options().qout) )
+		guild.append( "-s {0}".format(oneLetter[method]) )
+		guild.append( "-o {0}GUILD/guild_{1}.out".format(options.Options().qout,method) )
 
 		if reps: 			guild.append( "-r {0}".format(str(reps)) )
 		if iters: 			guild.append( "-i {0}".format(str(iters)) )
@@ -44,33 +54,45 @@ class NetworkAnalysis(method.Method):
 		if min_seed_score:	guild.append( "-t {0}".format(str(min_seed_score)) )
 
 		if method == "NetZcore":
+			self.logger.debug("Creating 100 random networks for NetZcore.")
 			create_random_networks_for_netzcore.sample_network_preserving_topology(
-															options.Options().qout+"guild_edges.tsv", 
+															options.Options().qout + "GUILD/guild_edges.tsv", 
 															samples, 
-															options.Options().qout+"guild_edges.tsv."
+															options.Options().qout + "GUILD/guild_edges.tsv."
 														)
+			guild.append("-d {0}GUILD/guild_edges.tsv.".format(options.Options().qout))
 
 		utils.cmd(*guild)
 
-	def readGUILDOutput(self, file):
-		##CHECK SPECIALLY THIS
-		guildOut = pd.DataFrame.from_csv(options.Options().qout + file, sep="\t", header=None, index_col=None)
-		guildOut.columns = ['Gene', 'Score']
-		guildOut.Score = guildOut.Score.astype(float)
+	def readGUILDOutput(self, guildResults):
+		guildOut = pd.DataFrame.from_csv(options.Options().qout + guildResults, sep="\t", header=None)
+		guildOut.columns = ['Score']
 
 		for Gene,row in guildOut.iterrows():
-			self._gene_network._net.node[Gene]["scoreG"] = row["Score"]
+			self._gene_network.update_node("scoreG",row["Score"],gene_id=str(Gene))
 
-		#guildOut.sort(column="Score", ascending=False, inplace=True)
+	def extractTopSubnetwork(self, x):
+		"""Creates a dot file on GUILD subfolder with the subnetwork top x genes, 
+		ranked by GUILD score."""
 
-		#top1 = round(len(guildOut.index) * 0.01)
-		#top5 = round(len(guildOut.index) * 0.05)
+		topX = 0.01 * x
 
-		# utils.cmd("sort","-g","-r","-k","2","{0}guild_{1}.out".format(options.Options().qout,method), 
-		# 		  "|","head","-114",">{0}{1}_top1.txt".format(options.Options().qout,method) )
-		# utils.cmd("sort","-g","-r","-k","2","{0}guild_{1}.out".format(options.Options().qout,method), 
-		# 		  "|","head","-570",">{0}{1}_top5.txt".format(options.Options().qout,method) )
+		topThreshold = round(len(self._gene_network.nodes()) * topX)
+		topGenes = []
+
+		sortedNodes = sorted(self._gene_network.nodes(data=True), key=lambda (a, dct): dct['scoreG'], reverse=True)
+		counter = 1
+		for gene in [ y[0] for y in sortedNodes ]:
+			if counter <= topThreshold: 
+				topGenes.append(gene)
+			else:
+				break
+			counter +=1
+
+		topNetwork = networkx.subgraph(self._gene_network._net, topGenes)
+
+		out_network.outputDot(topNetwork, "GUILD/guildTop{0}.dot".format(x) )
 
 if __name__ == '__main__':
-	#CHECK EVERYTHING
+
 	pass
