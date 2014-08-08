8a0762a9951199f6eddc9a35754ca3ec00b75fcd
Héctor Climente <hclimente@gmail.com>
2014-08-08 11:29:37 +0200

    Added control to avoid calculating log of None values. Added None instead of "" when a value cannot be calculated.

--- interface/out_network.py
+++ interface/out_network.py
@@ -9,22 +9,31 @@ def interactorsInfo(gn_net, tx_net, gene, nIso, tIso):
 	geneSym = gn_net._net.node[gene]["symbol"]
 	logging.info("Writing InteraX file for gene {0}({1}), isoforms {2} and {3}.".format(
 						gene, geneSym, nIso, tIso ))
-	with open("{0}/{1}/InteraXv2_{2}_{3}_{4}.tsv".format(options.Options().qout, options.Options().iLoopsVersion, geneSym, nIso, tIso), "w" ) as INTERAX:
-		INTERAX.write("#")
-		INTERAX.write("\t{0}\t{1}".format( nIso, tx_net._net.node[nIso]["median_PSI_N"] ))
-		INTERAX.write("\t{0}\t{1}".format( tIso, tx_net._net.node[tIso]["median_PSI_T"] ))
-		INTERAX.write("\t{0}".format( gene ))
-		INTERAX.write("\t{0}".format( geneSym ))
-		INTERAX.write("\t{0}".format( math.log10 ( tx_net._net.node[nIso]["median_TPM_N"] + 0.0001 ) ))
-		INTERAX.write("\t{0}".format( math.log10 ( tx_net._net.node[tIso]["median_TPM_N"] + 0.0001 ) ))
-		INTERAX.write("\n")
+	with open("{0}iLoops/{1}/InteraXv2_{2}_{3}_{4}.tsv".format(options.Options().qout, options.Options().iLoopsVersion, geneSym, nIso, tIso), "w" ) as INTERAX:
+		
+		nTPM_N = None
+		if tx_net._net.node[nIso]["median_TPM_N"] is not None:
+			nTPM_N = math.log10 ( tx_net._net.node[nIso]["median_TPM_N"] + 0.0001 )
+
+		tTPM_T = None
+		if tx_net._net.node[nIso]["median_TPM_N"] is not None:
+			tTPM_T = math.log10 ( tx_net._net.node[tIso]["median_TPM_T"] + 0.0001 )
+
+		INTERAX.write("#\t")
+		INTERAX.write("{0}\t{1}\t".format( nIso, tx_net._net.node[nIso]["median_PSI_N"] ))
+		INTERAX.write("{0}\t{1}\t".format( tIso, tx_net._net.node[tIso]["median_PSI_T"] ))
+		INTERAX.write("{0}\t{1}\t".format( gene, geneSym ))
+		INTERAX.write("{0}\t{1}\n".format( nTPM_N, tTPM_T ))
+
+		INTERAX.write("Partner\tGene\tSymbol\tRC_n\tRC_t\tdeltaRC\t")
+		INTERAX.write("Annotation\tTPM_n\tPSI_n\tTPM_t\tPSI_t\n")
 
 		for partnerIso in set( tx_net._net.neighbors(nIso) ) | set( tx_net._net.neighbors(tIso) ):
 			partnerGene = tx_net._net.node[partnerIso]["gene_id"]
 			annotation = "Driver" if gn_net._net.node[partnerGene]["Driver"] else ""
-			RCN = "" if partnerIso not in tx_net._net.neighbors(nIso) else tx_net._net.edge[nIso][partnerIso]["RC"]
-			RCT = "" if partnerIso not in tx_net._net.neighbors(tIso) else tx_net._net.edge[tIso][partnerIso]["RC"]
-			dRC = int(RCN - RCT) if RCN and RCT else ""
+			RCN = None if partnerIso not in tx_net._net.neighbors(nIso) else tx_net._net.edge[nIso][partnerIso]["RC"]
+			RCT = None if partnerIso not in tx_net._net.neighbors(tIso) else tx_net._net.edge[tIso][partnerIso]["RC"]
+			dRC = int(RCN - RCT) if RCN and RCT else None
 
 			nTPM = None
 			if tx_net._net.node[partnerIso]["median_TPM_N"] is not None:
@@ -48,11 +57,14 @@ def interactorsInfo(gn_net, tx_net, gene, nIso, tIso):
 
 def getGUILDInput(gn_net, onlyExperimental=False):
 	logging.info("Writing GUILD input files.")
-	with open(options.Options().qout + "GUILD/guild_nodes.tsv", "w" ) as GUILD_NODES:
+	outputFolder = "{0}GUILD_enriched/".format(options.Options().qout)
+	if onlyExperimental: outputFolder = "{0}GUILD_experimental/".format(options.Options().qout)
+
+	with open("{0}guild_nodes.tsv".format(outputFolder), "w" ) as GUILD_NODES:
 		for node,info in gn_net.nodes(data=True):
 			GUILD_NODES.write("{0} {1}\n".format(node, info["score"]))
 
-	with open(options.Options().qout + "GUILD/guild_edges.tsv", "w" ) as GUILD_EDGES:
+	with open("{0}guild_edges.tsv".format(outputFolder), "w" ) as GUILD_EDGES:
 		for node1,node2,info in gn_net.edges(data=True):
 			if onlyExperimental:
 				if info["experimental"]:
@@ -60,8 +72,8 @@ def getGUILDInput(gn_net, onlyExperimental=False):
 			else:
 				GUILD_EDGES.write("{0} {1} {2}\n".format(node1, info["score"], node2))
 
-def outputDot(network, name):
-	networkx.write_dot(network, options.Options().qout + name)
+def outputDot(network, path):
+	networkx.write_dot(network, path)
 
 def outputGTF(nodesOfInterest, tx_network):
 	logging.info("Writing GTF files.")
@@ -114,9 +126,39 @@ def outCandidateList(gn_network, tx_network):
 				if nIso.get_utrDiff(tIso): 	utrChange 	= True
 
 				cList.write("{0}\t{1}\t".format( gene, geneProperties["symbol"] ))
-				cList.write("{0}\t{1}\t".format( nIso, tIso ))
+				cList.write("{0}\t{1}\t".format( nIso.name, tIso.name ))
 				cList.write("{0}\t{1}\t".format( nUniprot, tUniprot ))
 				cList.write("{0}\t{1}\t".format( score, geneProperties["Driver"] ))
 				cList.write("{0}\t{1}\t".format( geneProperties["EpiFactor"], geneProperties["RBP"] ))
 				cList.write("{0}\t{1}\t".format( cds, cdsChange ))
 				cList.write("{0}\t{1}\n".format( utrChange, ",".join(patients) ))
+
+def outTSV(network,path):
+	with open(path + "_nodes.tsv", "w") as NODES:
+		columns = network.nodes(data=True)[0][1].keys()
+		NODES.write("Node")
+		for col in columns:
+			NODES.write("\t"+col)
+		NODES.write("\n")
+
+		import pdb
+		pdb.set_trace()
+
+		for node,properties in network.nodes(data=True):
+			NODES.write(node)
+			for key in properties:
+				NODES.write("\t"+str(properties[key]))
+			NODES.write("\n")
+
+	with open(path + "_edges.tsv", "w") as EDGES:
+		columns = network.edges(data=True)[0][2].keys()
+		EDGES.write("Node_1\tNode_2")
+		for col in columns:
+			EDGES.write("\t"+col)
+		EDGES.write("\n")
+		
+		for node1,node2,properties in network.edges(data=True):
+			EDGES.write(node1 + "\t" + node2)
+			for key in properties:
+				EDGES.write("\t"+str(properties[key]))
+			EDGES.write("\n")
