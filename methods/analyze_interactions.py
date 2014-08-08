479f5a5223d4f06c14204edac2ed821dbec8bfee
Héctor Climente <hclimente@gmail.com>
2014-08-08 11:36:30 +0200

    Adapted to store and work with gene_subnetwork reference. txNetwork: when adding edges of a candidate, add to all the edges with the same loop pattern. Added detectChanges to find genes in the subnetwork with both experimental and iLoops predicted interaction.

--- methods/analyze_interactions.py
+++ methods/analyze_interactions.py
@@ -14,8 +14,8 @@ import time
 import pdb
 
 class AnalyzeInteractions(method.Method):
-	def __init__(self, gn_network, tx_network):
-		method.Method.__init__(self, __name__, gn_network, tx_network)
+	def __init__(self, gn_network, tx_network, gn_subnetwork):
+		method.Method.__init__(self, __name__, gn_network, tx_network, gn_subnetwork)
 		self._expressed_transcripts = set()
 
 		for txSet in [ self._gene_network._net.node[x]["ExpressedTranscripts"] for x in self._gene_network.nodes() ]:
@@ -27,19 +27,24 @@ class AnalyzeInteractions(method.Method):
 		self.logger.info("Examining iLoops results.")
 		
 		#Sort by score: drivers will be first to be analyzed. Then, genes with an isoform switch with 
-		#decreasing patients
+		#decreasing number of patients
 
-		sortedNodes = sorted(self._gene_network.nodes(data=True), key=lambda (a, dct): dct['score'], reverse=True)
+		sortedNodes = sorted(self._gene_subnetwork.nodes(data=True), key=lambda (a, dct): dct['score'], reverse=True)
 		for gene,properties in sortedNodes:
-			if not self._gene_network._net.node[gene]["isoformSwitches"]: continue
-			switch = self._gene_network._net.node[gene]["isoformSwitches"][0][0]
-			nIso = switch[0]
-			tIso = switch[1]
+			if not properties["isoformSwitches"]: continue
+			for switch,score,patients in properties["isoformSwitches"]:
+				nIso = switch[0]
+				tIso = switch[1]
 
-			if self.getPredictedInteractions(gene, nIso, tIso):
-				self.calculateGeneInteractions(gene, nIso, tIso)
-				out_network.interactorsInfo(self._gene_network, self._transcript_network, 
-											gene, nIso, tIso )
+				if self.getPredictedInteractions(gene, nIso, tIso):
+					self.calculateGeneInteractions(gene, nIso, tIso)
+					out_network.interactorsInfo(self._gene_network, self._transcript_network, 
+												gene, nIso, tIso )
+					self.detectChanges(gene,nIso,tIso)
+
+					self._gene_subnetwork.saveNetwork("geneSubnetwork.pkl")
+					self._transcript_network.saveNetwork("txNetwork.pkl")
+					break
 
 	def getAnalyzableCandidates(self):
 		candidatesGaudi = {}
@@ -55,7 +60,7 @@ class AnalyzeInteractions(method.Method):
 
 	def getPredictedInteractions(self, gene, nIso, tIso):
 		parser = iLoops_parser.iLoopsParser()
-		symbol = self._gene_network._net.node[gene]["symbol"]
+		symbol = self._gene_subnetwork._net.node[gene]["symbol"]
 		
 		if not nIso in self._analyzable_candidates and not tIso in self._analyzable_candidates:
  			self.logger.debug("Gene {0} ({1}): lacking {2} and {3} predictions.".format(gene, symbol, nIso, tIso) )
@@ -114,47 +119,58 @@ class AnalyzeInteractions(method.Method):
 			os.remove(xmlFile)
 
 			for partner in interactions:
-				self._transcript_network.add_edge( iso, partner )
-				self._transcript_network.update_edge( iso, partner, "iLoops_prediction", True )
-				self._transcript_network.update_edge( iso, partner, "RC", interactions[partner] )
-			
-			self.logger.info("Predictions for {0} analyzed.".format(iso) )
-			self._gene_network.saveNetwork("geneNetwork_2.pkl")
-			self._transcript_network.saveNetwork("txNetwork_2.pkl")
+				partnerSameLoops = [ x[0] for x in self._transcript_network.nodes(data=True) if x[1]["iLoopsFamily"] == self._transcript_network._net.node[partner]["iLoopsFamily"] ]
+				for iso2 in partnerSameLoops:
+					self._transcript_network.add_edge( iso, iso2 )
+					self._transcript_network.update_edge( iso, iso2, "iLoops_prediction", True )
+					self._transcript_network.update_edge( iso, iso2, "RC", interactions[partner] )
 
 		return True
 
 	def calculateGeneInteractions(self, gene, nIso, tIso):
 
+		if [ x for x in self._gene_subnetwork._net.edges(gene,data=True) if x[2]["iLoops_prediction"] ]:
+ 			self.logger.debug("Gene {0}: {1} and {2} predictions already summarized.".format(gene, nIso, tIso) )
+ 			return
+
+		self.logger.info("Extrapolating predictions for {0} and {1} to gene level.".format(nIso,tIso))
+		nInfo = self._transcript_network._net.node[nIso]
+		tInfo = self._transcript_network._net.node[tIso]
+
 		for partner in self._transcript_network._net.neighbors(nIso):
-			gene 	= self._transcript_network._net.node[nIso]["gene_id"]
-			nRC		= self._transcript_network._net.edge[nIso][partner]["RC"]
+			gene 	= nInfo["gene_id"]
+			gene2 	= self._transcript_network._net.node[partner]["gene_id"]
 			deltaRC = 100
-			tRC 	= ""
-			if partner not in self._transcript_network._net.neighbors(tIso):
-				tRC = self._transcript_network._net.edge[tIso][partner]["RC"]
+
+			if partner in self._transcript_network._net.neighbors(tIso):
+				nRC 	= self._transcript_network._net.edge[nIso][partner]["RC"]
+				tRC 	= self._transcript_network._net.edge[tIso][partner]["RC"]
 				deltaRC = nRC - tRC
 
-			for isoSameLoops in [ x for x in self._transcript_network.nodes() if self._transcript_network._net.node[x]["iLoopsFamily"] == self._transcript_network._net.node[partner]["iLoopsFamily"] ]:
-				self._gene_network.add_edge(
-											 gene_id1=gene, 
-											 gene_id2=self._transcript_network._net.node[isoSameLoops]["gene_id"]
-										   )
-				self._gene_network.update_edge( "score", deltaRC, gene_id1=gene, gene_id2=self._transcript_network._net.node[isoSameLoops]["gene_id"])
+			self._gene_subnetwork.add_edge(gene_id1=gene, gene_id2=gene2)
+			self._gene_subnetwork.update_edge("deltaRC",deltaRC,gene_id1=gene,gene_id2=gene2)
+			self._gene_subnetwork.update_edge("iLoops_prediction",True,gene_id1=gene,gene_id2=gene2)
 
 		for partner in [ x for x in self._transcript_network._net.neighbors(tIso) if x not in self._transcript_network._net.neighbors(nIso) ]:
-			gene 	= self._transcript_network._net.node[tIso]["gene_id"]
-			tRC		= self._transcript_network._net.edge[tIso][partner]["RC"]
+			gene 	= tInfo["gene_id"]
+			gene2 	= self._transcript_network._net.node[partner]["gene_id"]
 			
-			for isoSameLoops in [ x for x in self._transcript_network.nodes() if self._transcript_network._net.node[x]["iLoopsFamily"] == self._transcript_network._net.node[partner]["iLoopsFamily"] ]:
-				self._gene_network.add_edge(
-											 gene_id1=gene, 
-											 gene_id2=self._transcript_network._net.node[isoSameLoops]["gene_id"]
-										   )
-				self._gene_network.update_edge( "score", -100, gene_id1=gene, gene_id2=self._transcript_network._net.node[isoSameLoops]["gene_id"])
+			self._gene_subnetwork.add_edge(gene_id1=gene, gene_id2=gene2)
+			self._gene_subnetwork.update_edge("deltaRC",-100,gene_id1=gene,gene_id2=gene2)
+			self._gene_subnetwork.update_edge("iLoops_prediction",True,gene_id1=gene,gene_id2=gene2)
 
-		self._gene_network.saveNetwork("geneNetwork_3.pkl")
-		self._transcript_network.saveNetwork("txNetwork_3.pkl")
+	def detectChanges(self,gene,nIso,tIso):
+		nInfo = self._transcript_network._net.node[nIso]
+		tInfo = self._transcript_network._net.node[tIso]
+
+		for gene1,gene2 in self._gene_subnetwork._net.edges(gene):
+			edgeInfo = self._gene_subnetwork._net.edge[gene1][gene2]
+			if edgeInfo["iLoops_prediction"] and edgeInfo["experimental"]:
+				if edgeInfo["deltaRC"] <= -20 and edgeInfo["score"] >= 0.6:
+					self.logger.info("{0} - {1} interaction lost.".format(gene1, gene2))
+				elif edgeInfo["deltaRC"] >= 20 and edgeInfo["score"] < 0.2:
+					if nInfo["RC"] == 50 or tInfo["RC"] == 500:
+						self.logger.info("{0} - {1} interaction gained.".format(gene1, gene2))
 
 if __name__ == '__main__':
 	pass
