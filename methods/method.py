5204d0a13182a0311838846d3cf29d6a5d3cdde7
Héctor Climente <hclimente@gmail.com>
2014-08-08 11:41:01 +0200

    Adapted to store the gene subnetwork too.

--- methods/method.py
+++ methods/method.py
@@ -3,7 +3,8 @@
 import logging
 
 class Method:
-	def __init__(self, name, gn_network, tx_network):
+	def __init__(self, name, gn_network, tx_network, gn_subnetwork):
 		self.logger = logging.getLogger(name)
 		self._gene_network			= gn_network
 		self._transcript_network	= tx_network
+		self._gene_subnetwork 		= gn_subnetwork
