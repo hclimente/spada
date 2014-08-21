abdbeb2dae93e53f9a1f2d148b84a0d860cbb7d3
Héctor Climente <hclimente@gmail.com>
2014-08-21 16:22:35 +0200

    Added method to find its own files from I3D containing its structure. Added method to read and store InterPro results.

--- biological_entities/protein.py
+++ biological_entities/protein.py
@@ -1,4 +1,5 @@
 from biological_entities import aminoacid
+from libs import utils
 from libs.SBI.structure import PDB
 from libs.SBI.structure.contacts import Complex
 
@@ -8,13 +9,14 @@ import logging
 class Protein:
 	def __init__(self, tx, txInfo):
 
-		self.logger 			= logging.getLogger(__name__)
 		self._tx 				= tx
+		self._gene 				= txInfo["gene_id"]
 		self._uniprot 			= txInfo["Uniprot"]
 		self._sequence 			= txInfo["proteinSequence"]
 		self._structure			= []
 		self._residueCorresp	= {}
 		self._has_pdbs			= False
+		self._features 			= []
 
 		if self._sequence is not None:
 			for res in range(0, len(self._sequence) ):
@@ -27,7 +29,9 @@ class Protein:
 	@property
 	def uniprot(self): return self._uniprot
 	@property
-	def hasPdbs(self): return self._has_pdbs	
+	def hasPdbs(self): return self._has_pdbs
+	@property
+	def seq(self): return self._sequence
 
 	def mapSubsequence(self, querySequence, strict=True):
 
@@ -51,7 +55,7 @@ class Protein:
 				elif self._sequence[posOri] == thisRes: 
 					correspondence.append(posOri)
 				else:
-					self.logger.debug("Unmatched residue {0} != {1}.".format(self._sequence[posOri], thisRes))
+					logging.debug("Unmatched residue {0} != {1}.".format(self._sequence[posOri], thisRes))
 
 				posSub += 1
 
@@ -60,7 +64,7 @@ class Protein:
 
 		return correspondence
 
-	def calculateVolumes(self, interaction, chain, exons):
+	def calculateVolumes(self, interaction, chain):
 
 		self._has_pdbs = True
 
@@ -99,17 +103,17 @@ class Protein:
  			if thisRes.identifier in buried:
 				self._structure[a[x]].setTag("B")
 				self._structure[a[x]]._pdbMapping[interaction] = ( chainObj.chain, thisRes.identifier, "B" )
-				self.logger.debug("{0}: residue {1}-{2} ({3} in sequence) detected as buried.".format(
+				logging.debug("{0}: residue {1}-{2} ({3} in sequence) detected as buried.".format(
 											interaction, chainObj.chain, thisRes.identifier, x))
 			elif thisRes.identifier in non_interacting_surface:
 				self._structure[a[x]].setTag("NIS")
 				self._structure[a[x]]._pdbMapping[interaction] = ( chainObj.chain, thisRes.identifier, "NIS" )
-				self.logger.debug("{0}: residue {1}-{2} ({3} in sequence) detected as non-interacting surface.".format(
+				logging.debug("{0}: residue {1}-{2} ({3} in sequence) detected as non-interacting surface.".format(
 											interaction, chainObj.chain, thisRes.identifier, x))
 			elif thisRes.identifier in interacting_surface:
 				self._structure[a[x]].setTag("IS")
 				self._structure[a[x]]._pdbMapping[interaction] = ( chainObj.chain, thisRes.identifier, "IS" )
-				self.logger.debug("{0}: residue {1}-{2} ({3} in sequence) detected as interacting surface.".format(
+				logging.debug("{0}: residue {1}-{2} ({3} in sequence) detected as interacting surface.".format(
 											interaction, chainObj.chain, thisRes.identifier, x))
 
 		return True
@@ -156,10 +160,27 @@ class Protein:
 		for aminoAcid, gPos in zip(self._structure, genomicPositions[:-1]): #Remove the stop codon
 			aminoAcid.setGenomicPosition(gPos)
 
-	def getAlteredRegions(self, otherIsoform):
-		for thisResidue in self._structure:
-			if thisResidue.genomicPosition not in [ y.genomicPosition for y in otherIsoform._structure]:
-				thisResidue.setIsoformSpecific(True)
+	def checkInteractome3DStructures(self):
+
+		noInteractions = True
+
+		for line in utils.readTable("Data/Databases/Interactome3D/2014_01/interactions.dat"):
+			pdbFile = "Data/Databases/Interactome3D/2014_01/interactions/" + line[21]
+
+			if self.uniprot == line[0]:
+				logging.debug("Relevant interaction for {0} at {1}.".format(self.tx, pdbFile))
+				if self.calculateVolumes(pdbFile, "A"):
+					noInteractions = False
+			elif self.uniprot == line[1]:
+				logging.debug("Relevant interaction for {0} at {1}.".format(self.tx, pdbFile))
+				if self.calculateVolumes(pdbFile, "B"):
+					noInteractions = False
+
+		if noInteractions:
+			logging.debug("No relevant structures found for {0}, {1}.".format(self.tx,self.uniprot))
+			return False
+
+		return True
 
 	def report(self):
 		seq 	= ""
@@ -173,8 +194,7 @@ class Protein:
 			if aResidue.isoformSpecific: 	isoSp += "X"
 			else: 							isoSp += "-"
 
-		self.logger.debug("{0},{1}".format(self._tx, seq))
-		self.logger.debug("{0},{1}".format(self._tx, isoSp))
+		return (seq,isoSp)
 
 	def printPDBInfo(self):
 		for pdb in set([ y for x in self._structure for y in x._pdbMapping ]):
@@ -182,20 +202,62 @@ class Protein:
 			chainsSet = set([ x._pdbMapping[pdb][0] for x in self._structure if pdb in x._pdbMapping ])
 			chain = set(chainsSet).pop()
 			if len(chainsSet) > 1:
-				self.logger.error("More than one chain for a protein in the PDB {0}, chains {1}.".format(pdb, chainsSet))
+				logging.error("More than one chain for a protein in the PDB {0}, chains {1}.".format(pdb, chainsSet))
 			isoSpec = [ x._pdbMapping[pdb][1][:-1] for x in self._structure if pdb in x._pdbMapping and x.isoformSpecific ]
 			interact = [ x._pdbMapping[pdb][1][:-1] for x in self._structure if pdb in x._pdbMapping and x._pdbMapping[pdb][2]=="IS" ]
 
 			if not isoSpec or not interact:
 				return
 
-			self.logger.debug("{0}, load {1}".format(self._tx,pdb))
-			self.logger.debug("{0}, set bg_rgb=[1,1,1]".format(self._tx))
-			self.logger.debug("{0}, hide all".format(self._tx))
-			self.logger.debug("{0}, show cartoon, all".format(self._tx))
-			self.logger.debug("{0}, color black, all".format(self._tx))
-			self.logger.debug("{0}, color white, chain {1}".format(self._tx,chain))
-			self.logger.debug("{0}, select interact, chain {1} & resi {2}".format(self._tx,chain, "+".join(interact) ))
-			self.logger.debug("{0}, select isoSpecific, chain {1} & resi {2}".format(self._tx,chain, "+".join(isoSpec) ))
-			self.logger.debug("{0}, color orange, isoSpecific".format(self._tx))
-			self.logger.debug("{0}, color red, interact & isoSpecific".format(self._tx))
+			logging.debug("{0}, load {1}".format(self._tx,pdb))
+			logging.debug("{0}, set bg_rgb=[1,1,1]".format(self._tx))
+			logging.debug("{0}, hide all".format(self._tx))
+			logging.debug("{0}, show cartoon, all".format(self._tx))
+			logging.debug("{0}, color black, all".format(self._tx))
+			logging.debug("{0}, color white, chain {1}".format(self._tx,chain))
+			logging.debug("{0}, select interact, chain {1} & resi {2}".format(self._tx,chain, "+".join(interact) ))
+			logging.debug("{0}, select isoSpecific, chain {1} & resi {2}".format(self._tx,chain, "+".join(isoSpec) ))
+			logging.debug("{0}, color orange, isoSpecific".format(self._tx))
+			logging.debug("{0}, color red, interact & isoSpecific".format(self._tx))
+
+	def readInterpro(self,interproOut):
+
+		for cols in utils.readTable(interproOut, header=False):
+			# https://code.google.com/p/interproscan/wiki/OutputFormats#Tab-separated_values_format_%28TSV%29
+		    protein_accession 	= cols[0] #Protein Accession
+		    md5_digest 			= cols[1] #Sequence MD5 digest
+		    seq_length 			= cols[2] #Sequence Length
+		    analysis 			= cols[3] #Analysis
+		    signature_accession = cols[4] #Signature Accession
+		    signature_descript 	= cols[5] #Signature Description
+		    start 				= int(cols[6]) #Start location
+		    stop 				= int(cols[7]) #Stop location
+		    try:
+		    	score 			= float(cols[8]) #Score - is the e-value of the match reported by member database method
+		    except ValueError:
+		    	score 			= None
+		    status 				= True if cols[9] == "T" else False #Status - is the status of the match (T: true)
+		    date 				= cols[10] #Date - is the date of the run
+		    if len(cols) > 11:
+		    	interpro_annotation	= cols[11] #(InterPro annotations - accession)
+		    if len(cols) > 12:
+		    	interpro_descript	= cols[12] #(InterPro annotations - description)
+		    if len(cols) > 13:
+		    	go_annotation		= cols[13] #(GO annotations)
+		    if len(cols) > 14:
+		    	pathway_annotation 	= cols[14] #(Pathways annotations)
+
+			if score and score > 0.01: continue
+
+			isoSpecificRes = set([ x._num for x in self._structure if x.isoformSpecific ])
+			featureRes = set(range(start,stop+1))
+			overlapRes = isoSpecificRes | featureRes
+
+			featInfo = {}
+			featInfo["region"] 			= [start,stop]
+			featInfo["accession"] 		= signature_accession
+			featInfo["description"] 	= signature_descript
+			featInfo["analysis"]		= analysis
+			featInfo["percentAffected"]	= len(overlapRes)/len(featureRes) * 100
+
+			self._features.append(featInfo)
