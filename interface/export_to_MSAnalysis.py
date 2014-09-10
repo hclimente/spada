2f295df39b3789003000c6a3d660e6e001922907
Héctor Climente <hclimente@gmail.com>
2014-09-10 18:16:33 +0200

    Added interface to Emma's script.

---
+++ interface/export_to_MSAnalysis.py
@@ -0,0 +1,17 @@
+from libs import options
+from libs import utils
+
+class Export2MSAnalysis:
+	def __init__(self):
+		pass
+
+	def generateFile(self,gn_network):
+		with open(options.Options().qout+"msInput.txt","w") as MS_INPUT:
+			for gene,info,switch in utils.iterate_switches_ScoreWise(gn_network):
+				if switch.nIsoform and switch.tIsoform:
+					MS_INPUT.write("{0}\t{1}\t{2}\t".format(gene,switch.nTx,switch.tTx))
+					MS_INPUT.write("{0}\t{1}\t".format(switch.nIsoform.seq,switch.tIsoform.seq))
+					MS_INPUT.write("{0}\n".format(",".join(switch.patients)))
+
+if __name__ == '__main__':
+	pass
