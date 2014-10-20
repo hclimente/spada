72bedc5eb5fe9c6ba7a8dde24adb8fd81480e4a9
Héctor Climente <hclimente@gmail.com>
2014-10-20 17:07:36 +0200

    Format changed

--- interface/export_to_MSAnalysis.py
+++ interface/export_to_MSAnalysis.py
@@ -9,7 +9,8 @@ class Export2MSAnalysis:
 		with open(options.Options().qout+"msInput.txt","w") as MS_INPUT:
 			for gene,info,switch in utils.iterate_switches_ScoreWise(gn_network):
 				if switch.nIsoform and switch.tIsoform:
-					MS_INPUT.write("{0}\t{1}\t{2}\t".format(gene,switch.nTx,switch.tTx))
+					MS_INPUT.write("{0}_{1}_{2}_{3}\t".format(gene,info["symbol"],
+															switch.nTx,switch.tTx))
 					MS_INPUT.write("{0}\t{1}\t".format(switch.nIsoform.seq,switch.tIsoform.seq))
 					MS_INPUT.write("{0}\n".format(",".join(switch.patients)))
 
