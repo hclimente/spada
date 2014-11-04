42b713363d1b2a39bfb1e475a81acacfffd9187d
Héctor Climente <hclimente@gmail.com>
2014-11-04 15:56:42 +0100

    Small fixes.

--- scripts/tcgaLauncherGenCluster.sh
+++ scripts/tcgaLauncherGenCluster.sh
@@ -9,7 +9,7 @@ function getSpecificDrivers {
 	subtissue=`python -c "dSubtissue = {\"bladder\": [\"blca\",\"blca-epithelial\",\"blca-mesenchymal\"],\"colon\": [\"coad\",\"coad-hypermutated\",\"coad-hypomutated\"],\"head_neck\": [\"hnsc\"],\"\": [\"brca\",\"brca-basal\",\"brca-her2\",\"brca-luminala\",\"brca-luminalb\",\"kich\",\"kirc\",\"kirp\",\"lihc\",\"luad\",\"lusc\",\"lusc-basal\",\"lusc-classical\",\"lusc-primitive\",\"lusc-secretory\",\"prad\",\"thca\",\"tcga\"]};print [ x for x in dSubtissue if '$cancerType' in dSubtissue[x] ][0]"`
 	histology=`python -c "dHistology = {\"carcinoma\": [\"blca\",\"blca-epithelial\",\"blca-mesenchymal\",\"brca\",\"brca-basal\",\"brca-her2\",\"brca-luminala\",\"brca-luminalb\",\"hnsc\",\"kich\",\"kirc\",\"kirp\",\"lihc\",\"thca\"],\"\": [\"coad\",\"coad-hypermutated\",\"coad-hypomutated\",\"luad\",\"lusc\",\"lusc-basal\",\"lusc-classical\",\"lusc-primitive\",\"lusc-secretory\",\"prad\",\"tcga\"]};print [ x for x in dHistology if '$cancerType' in dHistology[x] ][0]"`
 	subhistology=`python -c "dSubhistology = {\"\": [\"blca\",\"brca\",\"thca\",\"blca-epithelial\",\"blca-mesenchymal\",\"tcga\"],\"HER-positive_carcinoma\": [\"brca-her2\"],\"luminal_A_carcinoma\": [\"brca-luminala\"],\"luminal_B_carcinoma\": [\"brca-luminalb\"],\"basal_(triple-negative)_carcinoma\": [\"brca-basal\"],\"adenocarcinoma\": [\"coad\",\"coad-hypermutated\",\"coad-hypomutated\",\"luad\",\"prad\"],\"chromophobe_renal_cell_carcinoma\": [\"kich\"],\"clear_cell_renal_cell_carcinoma\": [\"kirc\"],\"papillary_renal_cell_carcinoma\": [\"kirp\"],\"hepatocellular_carcinoma\": [\"lihc\"],\"squamous_cell_carcinoma\": [\"hnsc\",\"lusc\",\"lusc-basal\",\"lusc-classical\",\"lusc-primitive\",\"lusc-secretory\"]};print [ x for x in dSubhistology if '$cancerType' in dSubhistology[x] ][0]"`
-	awk '$27 == "primary"' Data/CosmicCompleteExport_v70_100814.tsv >kk.tmp
+	awk '$27 == "primary"' /genomics/users/hector/SmartAS/Data/CosmicCompleteExport_v70_100814.tsv >kk.tmp
 	if [[ $tissue -ne "" ]]
 		then
 		awk -v k=$tissue '$8==k' kk.tmp >kk1.tmp
@@ -49,8 +49,8 @@ function launchQ {
 	echo '# SmartAS import data' >>$tag.sh
 	echo '#$ -q normal' >>$tag.sh
 	echo '#$ -cwd' >>$tag.sh
-	echo "#$ -e /genomics/users/hector/SmartAS/esmartas_$tag.1.txt" >>$tag.sh
-	echo "#$ -o /genomics/users/hector/SmartAS/osmartas_$tag.1.txt" >>$tag.sh
+	echo "#$ -e /data/users/hector/esmartas_$tag.1.txt" >>$tag.sh
+	echo "#$ -o /data/users/hector/osmartas_$tag.1.txt" >>$tag.sh
 
 	echo "/data/users/hector/Pipeline/SmartAS.py -f $tag.cfg" >>$tag.sh
 
@@ -60,8 +60,8 @@ function launchQ {
 	echo '# SmartAS getCandidates' >>$tag.2.sh
 	echo '#$ -q normal' >>$tag.2.sh
 	echo '#$ -cwd' >>$tag.2.sh
-	echo "#$ -e /genomics/users/hector/SmartAS/esmartas_$tag.2.txt" >>$tag.2.sh
-	echo "#$ -o /genomics/users/hector/SmartAS/osmartas_$tag.2.txt" >>$tag.2.sh
+	echo "#$ -e /data/users/hector/esmartas_$tag.2.txt" >>$tag.2.sh
+	echo "#$ -o /data/users/hector/osmartas_$tag.2.txt" >>$tag.2.sh
 
 	echo "/data/users/hector/Pipeline/SmartAS.py -f $tag.cfg" >>$tag.2.sh
 
@@ -83,7 +83,7 @@ do
 	echo minimum-expression=-1.0 >>$fullTag.cfg
 	echo tag=$fullTag >>$fullTag.cfg
 	echo specific-drivers=Data/"$fullTag"Drivers.txt >>$fullTag.cfg
-	echo working-directory=/genomics/users/hector/SmartAS >>$fullTag.cfg
+	echo working-directory=/data/users/hector >>$fullTag.cfg
 
 	launchQ $fullTag &
 
@@ -96,7 +96,7 @@ do
 	echo specific-drivers=Data/TCGA/specificDrivers/"$cancerTag"Drivers.txt >>u_$fullTag.cfg
 	echo unpaired-replicates=Yes >>u_$fullTag.cfg
 	echo specific-drivers=Data/"$fullTag"Drivers.txt >>u_$fullTag.cfg
-	echo working-directory=/genomics/users/hector/SmartAS >>u_$fullTag.cfg
+	echo working-directory=/data/users/hector >>u_$fullTag.cfg
 	
 	launchQ u_$fullTag &
 
