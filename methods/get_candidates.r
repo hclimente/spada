413668920cb2e817d80503ed555b954347493fb2
Héctor Climente <hclimente@gmail.com>
2014-11-11 18:24:40 +0100

    Paired and unpaired samples are imported and analyzed altogether.

--- methods/get_candidates.r
+++ methods/get_candidates.r
@@ -7,11 +7,7 @@ args <- commandArgs(trailingOnly = TRUE)
 minExpression <- as.numeric(args[1])
 load(paste0(args[2], "RWorkspaces/1_ExploreData.RData"))
 
-patientSet <- inputData$Replicates
-if (length(inputData$unpairedReplicates) > 0) {
-  patientSet <- inputData$unpairedReplicates
-}
-
+patientSet <- c(inputData$Replicates,inputData$unpairedReplicates)
 numOfReplicates <- length(patientSet)
 
 logger <- getLogger(name="get_candidates", level=10) #Level debug
@@ -19,19 +15,33 @@ logger <- getLogger(name="get_candidates", level=10) #Level debug
 addHandler(writeToConsole, logger="get_candidates", level='INFO')
 addHandler(writeToFile, logger="get_candidates", file=paste0(out, "rSmartAS.log"), level='DEBUG')
 
-binomialTest <- function(x){
+getRobustZscore <- function(x){
 
-  successes <- as.numeric(x[4])
-  a <- binom.test(successes,numOfReplicates,p = 0.05,alternative = "greater",conf.level = 0.95)
-  return(unlist(a["p.value"]))
+  s      <- as.numeric(x[2])
+  median <- as.numeric(x[3])
+  mad    <- as.numeric(x[4])
+  mead   <- as.numeric(x[5])
 
+  if(is.na(median)){
+    return(NA)
+  } else if (!is.na(mad)){
+    if(mad != 0){
+      z <- (s - median)/(1.486*mad)
+    } else {
+      z <- (s - median)/(1.253314*mead)
+    } 
+  } else if (!is.na(mead) & mead != 0) {
+    z <- (s - median)/(1.253314*mead)
+  } else {
+    return(NA)
+  }
+  pvalue = pnorm(z)
+
+  return(pvalue)
 }
 
 candidates <- list()
 replicateExpressed  <- list()
-candidateMask <- list()
-candidateMask[["N"]] <- list()
-candidateMask[["T"]] <- list()
 
 loginfo("Searching isoform switches in %d patients.", numOfReplicates, logger="get_candidates")
 
@@ -40,54 +50,64 @@ counter <- 1
 
 for (replicate in patientSet){
   
-  candidates[[replicate]] <- data.frame(Gene=as.character(), Switch=as.numeric(), maxdPSI=as.character(), mindPSI=as.character())
-  
   #Filter by deltaPSI and expression, based on the FPR
-  psiThreshold <- abs(intraReplicate[[replicate]]$deltaPSI) > interReplicate[["N"]]$FPR_5
-  psiThreshold[is.na(psiThreshold)] <- FALSE
+  data <- merge(intraReplicate[[replicate]][,c("Transcript", "deltaPSI")],interReplicate[["N"]][,c("Transcript","Median_dPSI","MAD_dPSI","MeAD_dPSI")])
+
+  intraReplicate[[replicate]]$p <- apply(data,1,getRobustZscore)
+  intraReplicate[[replicate]]$padj <- p.adjust(intraReplicate[[replicate]]$p, method="fdr")
+  significant <- intraReplicate[[replicate]]$padj < 0.05 | intraReplicate[[replicate]]$padj > 0.95 
+  significant[is.na(significant)] <- FALSE
   
   norExpression <- log(intraReplicate[[replicate]]$TPM_N) > minExpression
   tumExpression <- log(intraReplicate[[replicate]]$TPM_T) > minExpression
   
-  replicateCandidates <- vector('list', length(unique(intraReplicate[[replicate]]$Gene[psiThreshold]))) 
+  replicateCandidates <- vector('list', length(unique(intraReplicate[[replicate]]$Gene[significant]))) 
   
-  for (aCandidate in unique(intraReplicate[[replicate]]$Gene[psiThreshold])){ 
+  for (aCandidate in unique(intraReplicate[[replicate]]$Gene[significant])){ 
     
-    thisGeneData <- intraReplicate[[replicate]]$Gene == aCandidate
+    thisGeneMask <- intraReplicate[[replicate]]$Gene == aCandidate
+        
+    norTranscriptMask <- intraReplicate[[replicate]]$deltaPSI < 0 & significant & norExpression & thisGeneMask
+    tumTranscriptMask <- intraReplicate[[replicate]]$deltaPSI > 0 & significant & tumExpression & thisGeneMask
     
-    norTranscripts <- intraReplicate[[replicate]]$deltaPSI > 0 & psiThreshold & norExpression & thisGeneData
-    tumTranscripts <- intraReplicate[[replicate]]$deltaPSI < 0 & psiThreshold & tumExpression & thisGeneData
-    
-    if(length(intraReplicate[[replicate]]$deltaPSI[norTranscripts]) == 0 || length(intraReplicate[[replicate]]$deltaPSI[tumTranscripts]) == 0){
+    if(sum(norTranscriptMask,na.rm=T) == 0 || sum(tumTranscriptMask,na.rm=T) == 0){
       next
     }
+    
+    norTranscripts <- intraReplicate[[replicate]][norTranscriptMask,]
+    tumTranscripts <- intraReplicate[[replicate]][tumTranscriptMask,]
     
     #Calculate max difference between the transcripts and choose the candidates
-    #     deltaPSI = PSI_ref - PSI_alt
+    #     deltaPSI = PSI_alt - PSI_ref
     # MaxDeltaPsiCond: predominant transcript in the normal sample
     # MinDeltaPsiCond: predominant transcript in the tumor sample
-    maxDeltaPsi <- max(intraReplicate[[replicate]]$deltaPSI[thisGeneData & norTranscripts], na.rm=T)
-    norCandidate <- intraReplicate[[replicate]]$deltaPSI == maxDeltaPsi
-    
-    minDeltaPsi <- min(intraReplicate[[replicate]]$deltaPSI[thisGeneData & tumTranscripts], na.rm=T)
-    tumCandidate <- intraReplicate[[replicate]]$deltaPSI == minDeltaPsi
-
-    Switch <- maxDeltaPsi - minDeltaPsi
-    
-    if (Switch < 0.2){
-      next
+    maxP <- norTranscripts$padj == max(norTranscripts$padj, na.rm=T)
+    if (sum(maxP) > 1){
+      maxTPM <- norTranscripts$TPM_N == max(norTranscripts$TPM_N, na.rm=T)
+      norCandidate <- norTranscripts$Transcript[maxP & maxTPM]  
+    } else {
+      norCandidate <- norTranscripts$Transcript[maxP]
     }
     
-    replicateCandidates[[aCandidate]] <- data.frame(Gene=aCandidate,
-                                                    maxdPSI=intraReplicate[[replicate]]$Transcript[thisGeneData & norCandidate], 
-                                                    mindPSI=intraReplicate[[replicate]]$Transcript[thisGeneData & tumCandidate],
-                                                    Switch=Switch)
+    minP <- tumTranscripts$padj == max(tumTranscripts$padj, na.rm=T)
+    if (sum(minP) > 1){
+      minTPM <- tumTranscripts$TPM_T == max(tumTranscripts$TPM_T, na.rm=T)
+      tumCandidate <- tumTranscripts$Transcript[minP & minTPM]
+    } else {
+      tumCandidate <- tumTranscripts$Transcript[minP]
+    }
+    
+    if (sum(!is.na(norCandidate)) == 0 || sum(!is.na(tumCandidate)) == 0){
+      next
+    }
+  
+    Switch <- norTranscripts$deltaPSI[norTranscripts$Transcript==norCandidate] - tumTranscripts$deltaPSI[tumTranscripts$Transcript==tumCandidate]
+
+    replicateCandidates[[aCandidate]] <- data.frame(Gene=aCandidate,maxdPSI=norCandidate, 
+                                                    mindPSI=tumCandidate,Switch=Switch)
   }
   
   candidates[[replicate]] <- do.call('rbind', replicateCandidates)
-  
-  candidateMask[["N"]][[replicate]] <- interReplicate[["T"]]$Transcript %in% candidates[[replicate]]$maxdPSI
-  candidateMask[["T"]][[replicate]] <- interReplicate[["T"]]$Transcript %in% candidates[[replicate]]$mindPSI
   
   #Expressed genes: transcripts whose expression are above the expression threshold in any of the conditions
   replicateExpressed[[replicate]] <- data.frame(Transcript=intraReplicate[[replicate]]$Transcript[norExpression | tumExpression],
@@ -103,24 +123,20 @@ close(candidatesPB)
 allExpressedTranscripts <- unique(do.call('rbind', replicateExpressed))
 allExpressedTranscripts <- allExpressedTranscripts[with(allExpressedTranscripts, order(Transcript)), ]
 
-candidateMask[["N"]] <- do.call('cbind', candidateMask[["N"]])
-candidateMask[["T"]] <- do.call('cbind', candidateMask[["T"]])
-
 for (i in patientSet){candidates[[i]]$Origin = i}
 
 candidateList <- do.call("rbind", candidates)
 candidateList <- ddply(candidateList,.(Gene,maxdPSI,mindPSI), summarise, Replicated=length(Gene), Patients=paste(Origin, collapse = ",") )
 candidateList <- candidateList[with(candidateList, order(-Replicated)), ]
-candidateList$pval <- apply(candidateList,1,binomialTest)
 
 write.table(candidateList, file=paste0(out, "candidateList.tsv"), sep="\t", row.names=F, col.names=F, quote=F)
 write.table(allExpressedTranscripts, paste0(out, "expressedGenes.lst"), sep="\t", row.names=F, col.names=F, quote=F)
 
-cols <- c( paste0("TPM_",patientSet), paste0("PSI_",patientSet), paste0("tTPM_",patientSet), "FPR_4MAD" )
-write.table(interReplicate[["N"]][,!(colnames(interReplicate$N) %in% cols)], file=paste0(out, "expression_normal.tsv"), sep="\t", row.names=F, quote=F)
-write.table(interReplicate[["T"]][,!(colnames(interReplicate$T) %in% cols)], file=paste0(out, "expression_tumor.tsv"), sep="\t", row.names=F, quote=F)
+cols <- c("Gene","Transcript","Median_PSI","MAD_PSI","MeAD_PSI","Median_TPM","MAD_TPM","MeAD_TPM","Median_tTPM","MAD_tTPM","MeAD_tTPM")
+write.table(interReplicate[["N"]][,cols], file=paste0(out, "expression_normal.tsv"), sep="\t", row.names=F, quote=F)
+write.table(interReplicate[["T"]][,cols], file=paste0(out, "expression_tumor.tsv"), sep="\t", row.names=F, quote=F)
 
-save(isoformExpression, intraReplicate, interReplicate, candidates, candidateList, candidateMask, inputData, allExpressedTranscripts, wd, out, file=paste0(out, "RWorkspaces/2_GetCandidates.RData"))
+save(isoformExpression, intraReplicate, interReplicate, candidates, candidateList, inputData, allExpressedTranscripts, wd, out, file=paste0(out, "RWorkspaces/2_GetCandidates.RData"))
 
 #Plot heatmap
 suppressMessages(library(gplots)) #Avoid the annoying message
@@ -153,4 +169,4 @@ heatmap.2(as.matrix(fig), trace="none", scale="none", col=myPalette, na.col="gre
           
 graphics.off()
 
-save(isoformExpression, intraReplicate, interReplicate, candidates, candidateList, candidateMask, inputData, allExpressedTranscripts, wd, out, fig, file=paste0(out, "RWorkspaces/2_GetCandidates.RData"))
+save(isoformExpression, intraReplicate, interReplicate, candidates, candidateList, inputData, allExpressedTranscripts, wd, out, fig, file=paste0(out, "RWorkspaces/2_GetCandidates.RData"))
