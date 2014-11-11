3a40284041d0ec12475aa414af0cd3e81d216299
Héctor Climente <hclimente@gmail.com>
2014-11-11 18:31:54 +0100

    Changed to work with string format for initial step. Added function to iterate only relevant switches.

--- libs/utils.py
+++ libs/utils.py
@@ -45,7 +45,7 @@ def iterate_switches_ScoreWise(gene_network,only_first=False):
 	"""Iterate through the isoform switches of a gene network, and
 		generate a list of (gene,geneInformation,isoformSwitch).
 		Only return those switches with an overlap between the CDS 
-		of the transcripts, and with no differential expression.
+		of the transcripts.
 
 		only_first(bool): if True, only the first switch (the most 
 			common) will be returned for each gene.
@@ -57,7 +57,31 @@ def iterate_switches_ScoreWise(gene_network,only_first=False):
 	
 	for gene,info in sortedNodes:
 		if not info["isoformSwitches"]: continue
-		elif abs(info["diffExpression_logFC"]) > 0.5 or info["diffExpression_p"] < 0.05: continue
+		#elif abs(info["diffExpression_logFC"]) > 0.5 or info["diffExpression_p"] < 0.05: continue
+
+		if only_first:
+			yield gene,info,info["isoformSwitches"][0]
+		else:
+			for switch in info["isoformSwitches"]:
+				yield gene,info,switch
+
+def iterate_relevantSwitches_ScoreWise(gene_network,only_first=False):
+	"""Iterate through the isoform switches of a gene network, and
+		generate a list of (gene,geneInformation,isoformSwitch).
+		Only return those switches with an overlap between the CDS 
+		of the transcripts and that have different features.
+
+		only_first(bool): if True, only the first switch (the most 
+			common) will be returned for each gene.
+	"""
+
+	sortedNodes = sorted(gene_network.nodes(data=True), 
+						 key=lambda (a, dct): dct['score'], 
+						 reverse=True)
+	
+	for gene,info in sortedNodes:
+		if not info["isoformSwitches"]: continue
+		#elif abs(info["diffExpression_logFC"]) > 0.5 or info["diffExpression_p"] < 0.05: continue
 
 		if only_first:
 			if info["isoformSwitches"][0].is_relevant:
@@ -73,15 +97,15 @@ def setEnvironment():
 
 	logger.info("Preparing the environment.")
 
-	if not os.path.exists(".old/" + o.out):
-		cmd("mkdir","-p",".old/" + o.out)
-	if not os.path.exists(".old2/" + o.out):
-		cmd("mkdir","-p",".old2/" + o.out)
+	if not os.path.exists(".testOld/" + o.out):
+		cmd("mkdir","-p",".testOld/" + o.out)
+	if not os.path.exists(".testOld2/" + o.out):
+		cmd("mkdir","-p",".testOld2/" + o.out)
 
-	cmd("rm -r .old2/" + o.out )
-	cmd("mv", ".old/" + o.out, ".old2/" + o.out )
-	cmd("mv", o.qout, ".old/" + o.out )
-	cmd("mkdir -p", "Results/" + o.out + "RWorkspaces")
+	cmd("rm -r .testOld2/" + o.out )
+	cmd("mv", ".testOld/" + o.out, ".testOld2/" + o.out )
+	cmd("mv", o.qout, ".testOld/" + o.out )
+	cmd("mkdir -p", "testResults/" + o.out + "RWorkspaces")
 	cmd("mkdir", o.qout + "DataExploration")
 	cmd("mkdir -p", o.qout + "iLoops/" + o.iLoopsVersion)
 	cmd("mkdir", o.qout + "GUILD_experimental")
@@ -90,58 +114,46 @@ def setEnvironment():
 	cmd("mkdir", o.qout + "neighborhood_analysis")
 	cmd("mkdir", o.qout + "result_summary")
 
-	if not o.external:
-		if o.initialStep <= 1:
-			
-			#Set R workspace
-			r("wd <- \"{0}\"".format(options.Options().wd))
-			r("out <- \"{0}\"".format(options.Options().qout))
-			r("inputData <- list()")	
-			r('inputData[["Conditions"]] <- c("N", "T")')
-			reps = ",".join(set([ "\"" + x + "\"" for x in options.Options().replicates ]))
-			r('inputData[["Replicates"]] <- c({0})'.format (reps))
-			ureps = ",".join(set([ "\"" + x + "\"" for x in options.Options().unpairedReplicates ]))
-			r('inputData[["unpairedReplicates"]] <- c({0})'.format (ureps))
-			r('save.image("' + options.Options().qout + 'RWorkspaces/0_InitialEnvironment.RData")')
-
-		if o.initialStep >= 1:
-
-			cmd("cp -r", ".old/" + o.out + "DataExploration", o.qout)
-			cmd("cp", ".old/" + o.out + "RWorkspaces/1_ExploreData.RData", o.qout + "/RWorkspaces")
-			cmd("cp -r", ".old/{0}/iLoops".format(o.out), o.qout)
-
-		if o.initialStep > 2:
-			cmd("cp", ".old/" + o.out + "RWorkspaces/2_GetCandidates.RData", o.qout + "/RWorkspaces")
-			cmd("cp", ".old/" + o.out + "candidateList.tsv", ".old/" + o.out + "candidateList_v2.tsv",".old/" + o.out + "expressedGenes.lst", o.qout)
-			cmd("cp", ".old/" + o.out + "candidates_normal.gtf", ".old/" + o.out + "candidates_tumor.gtf", o.qout)
-			cmd("cp", ".old/" + o.out + "geneNetwork*.pkl", ".old/" + o.out + "txNetwork*.pkl", o.qout)
-			cmd("cp", ".old/" + o.out + "expression_normal.tsv", ".old/" + o.out + "expression_tumor.tsv", o.qout)
-			cmd("cp", ".old/" + o.out + "msInput.txt", o.qout)
-			
-	else:
+	if o.initialStep == "get-switches":
 		
-		if o.initialStep == 2:
-			oriOut = "_".join(o.out.split("_")[:-1])
-			cmd("cp", "Results/" + oriOut + "/RWorkspaces/1_ExploreData.RData", "Results/" + o.out + "RWorkspaces")
-			cmd("Pipeline/scripts/InputUnpaired.r", oriOut, o.unpairedReplicates, "Data/Input/" + o.out + "")
-		if o.initialStep == 3:
-			cmd("cp", o.external + ".tsv" , "Results/" + o.out + "candidateList.tsv")
-			cmd("cp", o.external + "_expressedGenes.lst", "Results/" + o.out + "expressedGenes.lst")
+		#Set R workspace
+		r("wd <- \"{0}\"".format(options.Options().wd))
+		r("out <- \"{0}\"".format(options.Options().qout))
+		r("inputData <- list()")	
+		r('inputData[["Conditions"]] <- c("N", "T")')
+		reps = ",".join(set([ "\"" + x + "\"" for x in options.Options().replicates ]))
+		r('inputData[["Replicates"]] <- c({0})'.format (reps))
+		ureps = ",".join(set([ "\"" + x + "\"" for x in options.Options().unpairedReplicates ]))
+		r('inputData[["unpairedReplicates"]] <- c({0})'.format (ureps))
+		r('save.image("' + options.Options().qout + 'RWorkspaces/0_InitialEnvironment.RData")')
 
-	if o.initialStep > 3:
-		cmd("cp", ".old/" + o.out + "candidatesGaudi.lst", o.qout)
-	if o.initialStep > 4:
-		cmd("cp -r", ".old/{0}/GUILD_experimental".format(o.out), o.qout)
-		cmd("cp", ".old/{0}/geneSubnetwork.pkl".format(o.out), o.qout)
-	if o.initialStep > 5:
-		cmd("cp -r", ".old/{0}/neighborhood_analysis".format(o.out), o.qout)
-	if o.initialStep > 6:
-		cmd("cp -r", ".old/{0}/GUILD_enriched".format(o.out), o.qout)
-		cmd("cp -r", ".old/{0}/iLoops/{1}".format(o.out, o.iLoopsVersion), o.qout)
-	if o.initialStep > 7:
-		cmd("cp -r", ".old/{0}/structural_analysis".format(options.Options().out), o.qout)
-	if o.initialStep > 8:
-		cmd("cp -r", ".old/{0}/result_summary".format(o.out), o.qout)
+	if o.initialStep not in ["import-data","get-switches"]:
+		cmd("cp -r", ".testOld/" + o.out + "DataExploration", o.qout)
+		cmd("cp", ".testOld/" + o.out + "RWorkspaces/0_InitialEnvironment.RData", o.qout + "/RWorkspaces")
+		cmd("cp", ".testOld/" + o.out + "RWorkspaces/1_ExploreData.RData", o.qout + "/RWorkspaces")
+		cmd("cp", ".testOld/" + o.out + "RWorkspaces/2_GetCandidates.RData", o.qout + "/RWorkspaces")
+		cmd("cp", ".testOld/" + o.out + "RWorkspaces/3_ClusteringFilter.RData", o.qout + "/RWorkspaces")
+		
+		cmd("cp", ".testOld/" + o.out + "expression_normal.tsv", ".testOld/" + o.out + "expression_tumor.tsv", o.qout)
+		cmd("cp", ".testOld/" + o.out + "candidateList.tsv", ".testOld/" + o.out + "candidateList_v2.tsv",".testOld/" + o.out + "candidateList_v3.tsv",".testOld/" + o.out + "expressedGenes.lst", o.qout)
+		cmd("cp", ".testOld/" + o.out + "candidates_normal.gtf", ".testOld/" + o.out + "candidates_tumor.gtf", o.qout)
+		cmd("cp", ".testOld/" + o.out + "geneNetwork*.pkl", ".testOld/" + o.out + "txNetwork*.pkl", o.qout)
+		cmd("cp", ".testOld/" + o.out + "msInput.txt", o.qout)
+			
+	if o.initialStep != "launch-iloops" and o.initialStep not in ["import-data","get-switches"]:
+		cmd("cp", ".testOld/" + o.out + "candidatesGaudi.lst", o.qout)
+	if o.initialStep != "neighborhood-analysis" and o.initialStep not in ["import-data","get-switches"]:
+		cmd("cp -r", ".testOld/{0}/GUILD_experimental".format(o.out), o.qout)
+		cmd("cp", ".testOld/{0}/geneSubnetwork.pkl".format(o.out), o.qout)
+	if o.initialStep != "experimental-network-analysis" and o.initialStep not in ["import-data","get-switches"]:
+		cmd("cp -r", ".testOld/{0}/neighborhood_analysis".format(o.out), o.qout)
+	if o.initialStep != "predicted-network-analysis" and o.initialStep not in ["import-data","get-switches"]:
+		cmd("cp -r", ".testOld/{0}/GUILD_enriched".format(o.out), o.qout)
+		cmd("cp -r", ".testOld/{0}/iLoops/{1}".format(o.out, o.iLoopsVersion), o.qout)
+	if o.initialStep != "get-relevant-switches" and o.initialStep not in ["import-data","get-switches"]:
+		cmd("cp -r", ".testOld/{0}/structural_analysis".format(options.Options().out), o.qout)
+	if o.initialStep != "summary" and o.initialStep not in ["import-data","get-switches"]:
+		cmd("cp -r", ".testOld/{0}/result_summary".format(o.out), o.qout)
 
 def pickUniqPatterns(tx_network, gn_network):
 
