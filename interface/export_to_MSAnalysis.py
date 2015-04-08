d661dbdd9e87c3f40c8702339d96aa9a4fe6fdfe
Héctor Climente <hclimente@gmail.com>
2015-04-08 17:33:51 +0200

    add information about the speficic part (length and if its at the beginning or the end)

--- interface/export_to_MSAnalysis.py
+++ interface/export_to_MSAnalysis.py
@@ -10,16 +10,24 @@ class Export2MSAnalysis:
 			for gene,info,switchDict,switch in gn_network.iterate_switches_ScoreWise(tx_network,removeNoise=False,partialCreation=False):
 				if switch.nIsoform and switch.tIsoform:
 
-					nIso = [ len(x) for x in switch.nIsoform.getSegments('isoform-specific') ]
-					nIso = sum(nIso)
-					tIso = [ len(x) for x in switch.tIsoform.getSegments('isoform-specific') ]
-					tIso = sum(tIso)
+					nIso = switch.nIsoform.getSegments('isoform-specific')
+					tIso = switch.tIsoform.getSegments('isoform-specific')
+
+					nSp = []
+					tSp = []
+
+					[nSp.extend(x) for x in nIso]
+					[tSp.extend(x) for x in tIso]
+
+					skipping = sum([ (switch.nIsoform._structure[0]==x or switch.nIsoform._structure[-1]==x ) for x in nSp ])
+					skippingTag = "S" if skipping else "NS"
 
 					MS_INPUT.write("{0}_{1}_{2}_{3}\t".format(gene,info["symbol"],
 															switch.nTx,switch.tTx))
 					MS_INPUT.write("{0}\t{1}\t".format(switch.nIsoform.seq,switch.tIsoform.seq))
 					MS_INPUT.write("{0}\t".format(",".join(switch.patients)))
-					MS_INPUT.write("{0}\t{1}\n".format(nIso,tIso))
+					MS_INPUT.write("{0}\t{1}\t".format(len(nSp),len(tSp)))
+					MS_INPUT.write("{0}\n".format(skippingTag))
 
 if __name__ == '__main__':
 	pass
