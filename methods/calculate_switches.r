301267b2a92a85eaa9fbd0ec7c564fe82b48478c
Héctor Climente <hclimente@gmail.com>
2016-02-19 15:08:26 +0100

    corrected expression calculation using logs added test to ensure a homogeneous split across paired and unpaired samples

--- methods/calculate_switches.r
+++ methods/calculate_switches.r
@@ -51,11 +51,11 @@ genes <- unlist(strsplit(row.names(psi),","))[c(T,F)]
 transcripts <- unlist(strsplit(row.names(psi),","))[c(F,T)]
 psi$Gene <- genes
 
-## get patients
-pats.n <- grep("^.{4}N$",colnames(psi), value=TRUE)
-pats.t <- grep("^.{4}T$",colnames(psi), value=TRUE)
-pats.t.nt <- gsub("N$","T",pats.n)
-pats.t.t <- setdiff(pats.t,pats.t.nt)
+## get sample names
+normal <- grep("^.{4}N$",colnames(psi), value=TRUE)
+tumor <- grep("^.{4}T$",colnames(psi), value=TRUE)
+tumor.paired <- gsub("N$","T",normal)
+tumor.unpaired <- setdiff(tumor,tumor.paired)
 
 ## read isoform expression
 xpr.nt <- read.table("/projects_rg/TCGA/pipeline/run11/luad_iso_tpm_paired-filtered.txt", check.names=FALSE)
@@ -69,38 +69,45 @@ xpr.gene.nt <- read.table("/projects_rg/TCGA/pipeline/run11/luad_gene_tpm_paired
 xpr.gene.t <- read.table("/projects_rg/TCGA/pipeline/run11/luad_gene_tpm_tumor-filtered.txt", check.names=FALSE)
 xpr.gene <- cbind(xpr.gene.nt,xpr.gene.t)
 
-rm(xpr.gene.nt,xpr.gene.t)
+### transform to log
+### Some cases with very high variability between normal samples AND orders of
+### magnitude above tumor samples where passing this test using raw difference.
+### Logarithm differences should alleviate this problem
+### Example: ADAMTS8|11095, Normal:uc001qgg.3, Tumor:uc001qgf.2, Sample: A46PT
+logxpr.gene <- log2(xpr.gene)
+
+rm(xpr.gene,xpr.gene.nt,xpr.gene.t)
 
 # Calculate switches
 ## Gene expression
-xpr.gene.diff <- data.frame(matrix(nrow=nrow(xpr.gene),ncol=length(pats.t)))
-colnames(xpr.gene.diff) <- pats.t
-xpr.gene.diff[,pats.t.nt] <- abs(xpr.gene[,pats.t.nt] - xpr.gene[,pats.n])
-xpr.gene.diff[,pats.t.t] <- abs(xpr.gene[,pats.t.t] - apply(xpr.gene[,pats.n],1,median))
+logxpr.gene.diff <- data.frame(matrix(nrow=nrow(logxpr.gene),ncol=length(tumor)))
+colnames(logxpr.gene.diff) <- tumor
+logxpr.gene.diff[,tumor.paired] <- abs(logxpr.gene[,tumor.paired] - logxpr.gene[,normal])
+logxpr.gene.diff[,tumor.unpaired] <- abs(logxpr.gene[,tumor.unpaired] - apply(logxpr.gene[,normal],1,median))
 
 ### get p
-xpr.gene.ecdf <- apply(xpr.gene[,pats.n],1,getEmpiricalDistribution,0.1)
-xpr.gene.diff.p <- mapply(do.call, xpr.gene.ecdf, apply(xpr.gene.diff,1,list))
-xpr.gene.diff.p <- lapply(xpr.gene.diff.p, function(x) 1 - x)
-xpr.gene.diff.padj <- lapply(xpr.gene.diff.p,p.adjust)
-xpr.gene.diff.padj <- do.call("rbind",xpr.gene.diff.padj)
+logxpr.gene.ecdf <- apply(logxpr.gene[,normal],1,getEmpiricalDistribution,0.1)
+logxpr.gene.diff.p <- mapply(do.call, logxpr.gene.ecdf, apply(logxpr.gene.diff,1,list))
+logxpr.gene.diff.p <- lapply(logxpr.gene.diff.p, function(x) 1 - x)
+logxpr.gene.diff.padj <- lapply(logxpr.gene.diff.p,p.adjust)
+logxpr.gene.diff.padj <- do.call("rbind",logxpr.gene.diff.padj)
 
 ### prepare df
-xpr.gene.diff.padj <- as.data.frame(xpr.gene.diff.padj)
-colnames(xpr.gene.diff.padj) <- pats.t
-xpr.gene.diff.padj$Gene <- rownames(xpr.gene.diff.padj)
+logxpr.gene.diff.padj <- as.data.frame(logxpr.gene.diff.padj)
+colnames(logxpr.gene.diff.padj) <- tumor
+logxpr.gene.diff.padj$Gene <- rownames(logxpr.gene.diff.padj)
 
-rm(xpr.gene.diff,xpr.gene.ecdf,xpr.gene.diff.p)
+rm(logxpr.gene.diff,logxpr.gene.ecdf,logxpr.gene.diff.p)
 
 ## PSI
 ### calculate differences
-psi.diff <- data.frame(matrix(nrow=nrow(psi),ncol=length(pats.t)))
-colnames(psi.diff) <- pats.t
-psi.diff[,pats.t.nt] <- abs(psi[,pats.t.nt] - psi[,pats.n])
-psi.diff[,pats.t.t] <- abs(psi[,pats.t.t] - apply(psi[,pats.n],1,median))
+psi.diff <- data.frame(matrix(nrow=nrow(psi),ncol=length(tumor)))
+colnames(psi.diff) <- tumor
+psi.diff[,tumor.paired] <- abs(psi[,tumor.paired] - psi[,normal])
+psi.diff[,tumor.unpaired] <- abs(psi[,tumor.unpaired] - apply(psi[,normal],1,median))
 
 ### get p
-psi.ecdf <- apply(psi[,pats.n],1,getEmpiricalDistribution,0)
+psi.ecdf <- apply(psi[,normal],1,getEmpiricalDistribution,0)
 psi.diff.p <- mapply(do.call, psi.ecdf, apply(psi.diff,1,list))
 psi.diff.p <- lapply(psi.diff.p, function(x) 1 - x)
 psi.diff.padj <- lapply(psi.diff.p,p.adjust)
@@ -108,29 +115,29 @@ psi.diff.padj <- do.call("rbind",psi.diff.padj)
 
 ### prepare df
 psi.diff.padj <- as.data.frame(psi.diff.padj)
-colnames(psi.diff.padj) <- pats.t
+colnames(psi.diff.padj) <- tumor
 psi.diff.padj$Gene <- genes
 psi.diff.padj$Transcript <- transcripts
 
 rm(psi.diff,psi.ecdf,psi.diff.p)
 
 ## Sign
-psi.sign <- data.frame(matrix(nrow=nrow(psi),ncol=length(pats.t)))
-colnames(psi.sign) <- pats.t
-psi.sign[,pats.t.nt][psi[,pats.t.nt] > psi[,pats.n]] <- "Tumor"
-psi.sign[,pats.t.t][psi[,pats.t.t] > apply(psi[,pats.n],1,median)] <- "Tumor"
-psi.sign[,pats.t.nt][psi[,pats.t.nt] < psi[,pats.n]] <- "Normal"
-psi.sign[,pats.t.t][psi[,pats.t.t] < apply(psi[,pats.n],1,median)] <- "Normal"
+psi.sign <- data.frame(matrix(nrow=nrow(psi),ncol=length(tumor)))
+colnames(psi.sign) <- tumor
+psi.sign[,tumor.paired][psi[,tumor.paired] > psi[,normal]] <- "Tumor"
+psi.sign[,tumor.unpaired][psi[,tumor.unpaired] > apply(psi[,normal],1,median)] <- "Tumor"
+psi.sign[,tumor.paired][psi[,tumor.paired] < psi[,normal]] <- "Normal"
+psi.sign[,tumor.unpaired][psi[,tumor.unpaired] < apply(psi[,normal],1,median)] <- "Normal"
 
 ## Order
 ### Normal
-psi.order.n <- data.frame(medianPsi=apply(psi[,pats.n],1,median))
+psi.order.n <- data.frame(medianPsi=apply(psi[,normal],1,median))
 psi.order.n$Gene <- unlist(strsplit(rownames(psi),","))[c(T,F)]
 psi.order.n <- ddply(psi.order.n,.(Gene), summarise, orderNormal=order(-medianPsi))
 rownames(psi.order.n) <- rownames(psi)
 
 ### Tumor
-x <- by(psi[,pats.t], psi$Gene, function(y){
+x <- by(psi[,tumor], psi$Gene, function(y){
   apply(y[,colnames(y)!="Gene"],2,function(z){
     v <- rep(F,length(z))
     v[which.max(z)] <- T
@@ -138,7 +145,7 @@ x <- by(psi[,pats.t], psi$Gene, function(y){
   })
 })
 psi.order.t <- do.call("rbind",x)
-colnames(psi.order.t) <- pats.t
+colnames(psi.order.t) <- tumor
 
 # Define switches
 ## Conditions
@@ -146,14 +153,14 @@ colnames(psi.order.t) <- pats.t
 topTumor <- psi.sign=="Tumor" & psi.order.t
 topNormal <- psi.sign=="Normal" & psi.order.n$orderNormal==1
 ### significant deltaPSI
-bigChange <- psi.diff.padj[,pats.t] < 0.05
+bigChange <- psi.diff.padj[,tumor] < 0.05
 ### non-significant change in gene expression
-x <- merge(psi.diff.padj,xpr.gene.diff.padj,by="Gene",suffix=c(".psi.diff",".xpr.diff"))
-noExpressionChange <- x[,paste0(pats.t,".xpr.diff")] > 0.05
+x <- merge(psi.diff.padj,logxpr.gene.diff.padj,by="Gene",suffix=c(".psi.diff",".xpr.diff"))
+noExpressionChange <- x[,paste0(tumor,".xpr.diff")] > 0.05
 ### transcripts are expressed
-expressedTumor <- xpr[,pats.t] > 0.1
-medianN <- apply(xpr[,pats.n],1,median)
-expressedNormal <- cbind(xpr[,pats.n], replicate(length(pats.t.t),medianN)) > 0.1
+expressedTumor <- xpr[,tumor] > 0.1
+medianN <- apply(xpr[,normal],1,median)
+expressedNormal <- cbind(xpr[,normal], replicate(length(tumor.unpaired),medianN)) > 0.1
 
 ## Filter transcripts
 elegibleTxs <- psi.sign
@@ -176,7 +183,7 @@ switches <- by(elegibleTxs,elegibleTxs$Gene, function(x){
   })
   
   z <- data.frame(do.call("rbind",z))
-  z$Patient <- rownames(z)
+  z$Sample <- rownames(z)
   validCases <- rowSums(is.na(z[,c("Normal","Tumor")])) < 2
   z <- z[validCases,]
   if (nrow(z)){
@@ -188,17 +195,35 @@ switches <- by(elegibleTxs,elegibleTxs$Gene, function(x){
     NA
   })
 
-switches.formatted <- list()
+switches.df <- list()
 for (g in names(switches)){
   if (class(switches[[g]])=="data.frame"){
     switches[[g]]$Gene=g
-    switches.formatted[[g]] <- switches[[g]]
+    switches.df[[g]] <- switches[[g]]
   }
 }
 
-switches.formatted <- do.call("rbind",switches.formatted)
-switches.formatted <- switches.formatted[,c("Gene","Normal","Tumor","Patient")]
+switches.df <- do.call("rbind",switches.df)
+switches.df <- switches.df[,c("Gene","Normal","Tumor","Sample")]
 
-write.table(switches.formatted, file=outfile, sep="\t", row.names=F, col.names=F, quote=F)
+# we expect the switches to be distributed among paired and
+# unpaired samples, those that do not comply with it are
+# likely to be caused by a lack of representativeness of the median
+# when calculating the deltaPSI
+switches.df.byPat <- ddply(switches.df,.(Gene,Normal,Tumor),summarise,
+                                  Paired=sum(Sample %in% tumor.paired), Unpaired=sum(Sample %in% tumor.unpaired) )
 
-stromal.correlation <- read.table(paste0("/projects_rg/TCGA/pipeline/run11/",tumor,"_gene_gsea_full.txt"), check.names=FALSE, header=TRUE)
+p <- apply(switches.df.byPat[,c("Paired","Unpaired")],1,
+      function(x,p){
+        b <- binom.test(x[1],x[1]+x[2],p,"less")
+        b$p.value
+      },length(tumor.paired)/length(tumor.unpaired))
+
+# remove those with an uncorrected p < 0.25
+switches.df.byPat.filt <- subset(switches.df.byPat, p >= 0.25, select=c("Gene","Normal","Tumor"))
+
+switches.df.filt <- merge(switches.df.byPat.filt,switches.df)
+
+write.table(switches.df.filt, file=outfile, sep="\t", row.names=F, col.names=F, quote=F)
+
+#stromal.correlation <- read.table(paste0("/projects_rg/TCGA/pipeline/run11/",tumor,"_gene_gsea_full.txt"), check.names=FALSE, header=TRUE)
