0c0b256fece4b949711f181a07d474d8fde5d262
Héctor Climente <hclimente@gmail.com>
2016-02-24 15:36:05 +0100

    removed soappy mentions

--- interface/interpro_analysis.py
+++ interface/interpro_analysis.py
@@ -1,76 +1,20 @@
 from libs import options
 from libs import utils
 
-import base64, os, SOAPpy, time
+import os, time
 
 class InterproAnalysis:
-    def __init__(self,mode="offline"):
-        self._mode = mode
-        if self._mode=="online":
-
-            SOAPpy.Client.SOAPUserAgent = self.SOAPUserAgent
-
-            # Create the service interface
-            self._server = SOAPpy.WSDL.Proxy('http://www.ebi.ac.uk/Tools/services/soap/iprscan5?wsdl')
-
-            # Fix message namespace (not set from the WSDL).
-            for method in self._server.methods:
-                if self._server.methods[method].namespace == None:
-                    self._server.methods[method].namespace = 'http://soap.jdispatcher.ebi.ac.uk'
-
-            # Configure HTTP proxy from OS environment (e.g. http_proxy="http://proxy.example.com:8080")
-            if os.environ.has_key('http_proxy'):
-                http_proxy_conf = os.environ['http_proxy'].replace('http://', '')
-            elif os.environ.has_key('HTTP_PROXY'):
-                http_proxy_conf = os.environ['HTTP_PROXY'].replace('http://', '')
-            else:
-                http_proxy_conf = None
-            self._server.soapproxy.http_proxy = http_proxy_conf
+    def __init__(self):
+        pass
 
     def launchAnalysis(self,tx,seq):
-        out = "{0}Data/{1}/InterPro/{2}.tsv".format(options.Options().wd, options.Options().annotation, tx)
+        out = "{}data/{}/InterPro/{}.tsv".format(options.Options().wd, options.Options().annotation, tx)
 
-        # asume that if the file is not present, 
-        # its because no feature could be mapped
-        if os.path.isfile(out):
-            return out
-        else:
-            return None
-
-        if self._mode=="online":
-            params = {}
-            params['sequence']  = seq
-            params['goterms']   = 1
-            params['pathways']  = 1
-
-            jobId = self._server.run(email="hector.climente@upf.edu", title=tx, parameters=params)
-            self.checkStatus(jobId)
-
-            for resultType in self._server.getResultTypes(jobId=jobId)["type"]:
-                if resultType['identifier'] != "tsv": continue
-                
-                resultBase64 = self._server.getResult(jobId=jobId, type=resultType['identifier'])
-                result = base64.decodestring(resultBase64)
-
-                with open(out, 'w') as interPro_Out:
-                    interPro_Out.write(result)
-        else:
-
-            proc = utils.cmdOut("{0}Pipeline/libs/interpro/interproscan.sh".format(options.Options().wd),
+        if not os.path.isfile(out):
+            proc = utils.cmdOut("{}pipeline/libs/interpro/interproscan.sh".format(options.Options().wd),
                                 "--disable-precalc","--outfile",out,"--formats","TSV",
                                 "--input","{0}protein.fa".format(options.Options().qout))
         return out
-
-    def SOAPUserAgent(self):
-        return "SmartAS"
-
-    def checkStatus(self, jobId):
-        while True:
-            result = self._server.getStatus(jobId = jobId)
-            if result == 'RUNNING' or result == 'PENDING':
-                time.sleep(15)
-            else:
-                return True
 
     def readInterpro(self,interproOut,protein):
 
