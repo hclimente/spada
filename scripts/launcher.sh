2540e32361a32c85b755c4cb6dd90f9d7ee86c2f
Héctor Climente <hclimente@gmail.com>
2016-02-24 12:13:27 +0100

    finished rework using job arrays

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -1,49 +1,12 @@
 #!/bin/bash
+#$ -q normal 
+#$ -cwd 
+#$ -V 
+#$ -wd /data/users/hector/smartas/
 
-#Data/Input/TCGA_tags.txt
-fileList=$1
-action=$2
-models=$3
+source ~/.bashrc
+wd="/data/users/hector/smartas"
 
-function launchQ {
+tumor=`head -n$SGE_TASK_ID $wd/tcga_tags.txt | tail -n1`
 
-	source ~/.bashrc
-
-	tag=$2
-	echo '#!/bin/sh' >$tag.sh
-	echo "# SmartAS $tag" >>$tag.sh
-	echo '#$ -q normal' >>$tag.sh
-	echo '#$ -cwd' >>$tag.sh
-	echo "#$ -e esmartas_$tag.txt" >>$tag.sh
-	echo "#$ -o osmartas_$tag.txt" >>$tag.sh
-	echo "#$ -V" >>$tag.sh
-	echo "#$ -N $tag" >>$tag.sh
-
-	echo "Pipeline/SmartAS.py -f $tag.cfg" >>$tag.sh
-
-	qsub $tag.sh
-
-}
-
-while read fullTag
-do
-
-	echo $fullTag 
-	cfgFile="$fullTag"_$action.cfg
-
-	cancerTag=`echo $fullTag | cut -d'-' -f1`
-
-	echo initial-step=$action  >$cfgFile
-	echo tag=$fullTag >>$cfgFile
-	echo unpaired-replicates=Yes >>$cfgFile
-	echo working-directory=/data/users/hector >>$cfgFile
-	if [[ "$models" == "all-switches" ]]; then
-		echo $models >>$cfgFile
-	fi
-
-	rm esmartas_"$fullTag"_$action.txt osmartas_"$fullTag"_$action.txt
-
-	launchQ $fullTag "$fullTag"_$action
-	#Pipeline/SmartAS.py -f "$fullTag"_$action.cfg
-
-done < "$fileList"
+python $wd/pipeline/smartas.py -s $JOB_NAME  -t $tumor -wd $wd
