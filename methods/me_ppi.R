50462574f005ddd041f2a428a46b217fbc570ad5
Héctor Climente <hclimente@gmail.com>
2016-07-22 15:22:57 +0200

    just calculates the proportion of pannegative among the interactors

--- methods/me_ppi.R
+++ methods/me_ppi.R
@@ -1,35 +1,98 @@
-#!/usr/bin/env Rscript
-
-library(find.me)
-library(tidyr)
 source("~/smartas/pipeline/scripts/variablesAndFunctions.r")
+library(tidyr)
 
 args <- commandArgs(trailingOnly = TRUE)
 root <- args[1]
 wdp <- args[2]
-gene <- args[3]
 
-# root <- "~/smartas/"
-# wdp <- paste0(root,"analyses/pancancer/")
-# gene <- "TAF9"
+#############################
+##        SWITCHES         ##
+#############################
 
-load(file=paste0(wdp,"ppi/data.RData"))
+candidateInfo <- read_tsv(paste0(root,"notebook/data/pancancer/candidateList_full.tsv")) %>%
+  mutate(Coocurrent= ifelse(is.na(Coocurrent),FALSE,Coocurrent),
+         Candidate = as.logical(Recurrent) | as.logical(AffectingMutatedFeature) | as.logical(Coocurrent)) %>%
+  mutate(Candidate = revalue(as.character(Candidate),replace=c("TRUE"="Candidate","FALSE"="Non-candidate"))) %>%
+  select(GeneId,Symbol,Normal_transcript,Tumor_transcript,Recurrent,AffectingMutatedFeature,Coocurrent,Candidate)
 
-# add switches that affect the target
-affectedSwitches <- ppi %>%
-  filter(partnerSymbol==gene & What!="Kept") %>%
-  select(Tumor,Symbol.switch,Patients_affected,PatientNumber) %>%
-  set_colnames(c("Tumor","Symbol","Patients_affected","PatientNumber"))
+switches <- read_tsv(paste0(wdp,"candidateList_info.tumorSplit.tsv"))
+switches.agg <- read_tsv(paste0(wdp,"candidateList_info.agg.tsv"))
 
-# if present, add switches in the target gene
+#############################
+##         DRIVERS         ##
+#############################
+
+drivers <- read_tsv(paste0(root,"data/ucsc/intogen_cancer_drivers-2014.12b/Mutational_drivers_per_tumor_type.tsv"),comment="#") %>%
+  mutate(Tumor_type = ifelse(Tumor_type=="COREAD", "coad", Tumor_type),
+         Tumor_type = ifelse(Tumor_type=="HC", "lihc", Tumor_type),
+         Tumor_type = ifelse(Tumor_type=="RCCC", "kirc", Tumor_type),
+         Tumor_type = tolower(Tumor_type) ) %>%
+  set_colnames(c("Symbol","Tumor"))
+
+#############################
+##        MUTATIONS        ##
+#############################
+
+allPatients <- strsplit(switches.agg$Patients_affected,",") %>% unlist %>% unique
+
+mutations <- read_tsv(paste0(root,"notebook/data/mutations/wes_mutations.txt")) %>%
+  select(Tumor,Patient,Symbol) %>%
+  unique %>%
+  # filter out patients without RNAseq data available
+  filter(Patient %in% allPatients) %>%
+  mutate(Alteration2="MUT")
+
+#############################
+##       EXPRESSION        ##
+#############################
+
+proteome <- read_tsv(paste0(wdp,"mutations/proteome_information.txt"))
+
+#############################
+##      INTERACTIONS       ##
+#############################
+
+ppi.file <- paste0(root,"notebook/data/structural_analysis/Switched_interactions_consensus.txt")
+
+# get max number of columns (necessary for reading)
+no_col <- max(count.fields(ppi.file,sep = "\t"))
+no_col.ppi <- (no_col-6)/2
+ppi.cols <- paste(c("Origin","Interaction"), floor(seq(1,no_col.ppi,0.5)), sep="_")
+
+# read table
+ppi <- read.table(ppi.file,header=F,fill=T,col.names=1:no_col) %>%
+  set_colnames(c("GeneId","Symbol","Normal_transcript","Tumor_transcript","partnerId","partnerSymbol",ppi.cols)) %>%
+  # all Origin columns contail "DDI_match", so we can disregard them
+  select(-starts_with("Origin_")) %>%
+  # convert from wide to long table format
+  melt(id.vars = c("GeneId","Symbol","Normal_transcript","Tumor_transcript","partnerId","partnerSymbol"),
+       value.name = "Interaction") %>%
+  select(-variable) %>%
+  # remove cases with no interaction described
+  filter(Interaction!="") %>%
+  # split interaction information
+  separate(Interaction, into=c("What","Transcript","Pfams"), sep="-") %>%
+  # remove pfams columns (account for different domains for the same interaction)
+  select(-Pfams) %>%
+  # remove several instances of the same isoform
+  unique %>%
+  # annotate with switch info
+  merge(switches) %>%
+  merge(candidateInfo) %>%
+  # consider only the most abundant isoform as partner: one interaction per pair & only expressed genes
+  merge(proteome,by.x=c("Tumor","Transcript"), by.y=c("Cancer","Transcript"), suffixes = c(".switch",".partner"))
+
+#############################
+##   CREATE WIDE MATRIX    ##
+#############################
+
+# select the appropriate columns
 affectedSwitches <- switches %>%
-  filter(Symbol==gene & IsFunctional==1) %>%
-  select(Tumor,Symbol,Patients_affected,PatientNumber) %>%
-  rbind(affectedSwitches)
+  filter(IsFunctional==1) %>%
+  select(Tumor,Symbol,Patients_affected,PatientNumber)
 
-# if we do not have at least two genes, skip
-stopifnot(length(unique(affectedSwitches$Symbol)) >= 2)
-
+# create long format matrix for switches
+## split switch patients
 nocols <- max(affectedSwitches$PatientNumber)
 namescols <- paste("Patient", 1:nocols, sep="_")
 suppressWarnings( affectedSwitches.long <- affectedSwitches %>%
@@ -41,71 +104,77 @@ affectedSwitches.long <- affectedSwitches.long %>%
   select(-variable) %>%
   set_colnames(c("Symbol","Tumor","Patient")) %>%
   filter(!is.na(Patient)) %>%
+  # add "SPLICING" as tag
   mutate(Alteration="SPLICING")
 
+# create long matrix for mutations
 affectedMutations.long <- mutations %>%
-  filter(Symbol %in% affectedSwitches.long$Symbol & Symbol %in% drivers$geneHGNCsymbol)
+  merge(drivers)
 
+# merge both matrixes
 affected.long <- merge(affectedSwitches.long, affectedMutations.long,all=T) %>%
   mutate(Alteration=ifelse(is.na(Alteration),"",Alteration),
          Alteration2=ifelse(is.na(Alteration2),"",Alteration2)) %>%
   mutate(Alteration=paste(Alteration,Alteration2,sep=";"))
 
+# convert to wide format
 affected.wide <- affected.long %>%
   select(Symbol,Patient,Alteration) %>%
-  spread(Patient, Alteration)
+  spread(Patient, Alteration) %>%
+  set_rownames(.$Symbol) %>%
+  select(-Symbol) %>%
+  as.matrix
 
+## substitute NA's
 affected.wide[is.na(affected.wide)] <- ""
 
-rownames(affected.wide) <- affected.wide$Symbol
-affected.wide <- as.matrix(affected.wide)
-affected.wide <- affected.wide[, !colnames(affected.wide) %in% c("Tumor","Symbol")]
-affected.wide <- affected.wide[,colSums(affected.wide=="") < nrow(affected.wide)]
+write_tsv(affected.wide, paste0(wdp,"ppi/mutation_matrix.tsv"))
 
-# remove those genes with alterations that affect less than 5% of the patients
-minPatients <- floor(0.05 * ncol(affected.wide))
-affected.wide <- affected.wide[rowSums(affected.wide != "") > minPatients,]
-## remove new empty columns
-affected.wide <- affected.wide[,colSums(affected.wide != "") > 0]
+#############################
+##    CALCULATE OVERLAP    ##
+##     WITH MUTATIONS      ##
+#############################
 
-mutmat <- getSortedMatrix(affected.wide)$mutmat
-# add empty matrix by the side
-mutmat <- cbind(mutmat,matrix(0,nrow=nrow(mutmat), ncol=nPatients$total - nrow(mutmat)))
-sampling.pval <- me.test.permutateSamples(mutmat)
-fisher.pval <- me.test.fisher(mutmat)
+mutation.overlap <- list()
 
-if (min(sampling.pval,fisher.pval) < 0.05){
+for (gene in unique(drivers$Symbol)){
+  # add switches that affect the target
+  studiedGenes <- ppi %>%
+    # add interactors
+    filter(partnerSymbol==gene & What!="Kept") %>%
+    .$Symbol.switch %>%
+    as.character %>%
+    c(gene) %>%
+    unique %>%
+    # pick only those with a mutation or switch
+    intersect(rownames(affected.wide))
   
-  # get patients for tumor bar at top
-  patients <- affected.long %>% 
-    filter(Patient %in% colnames(affected.wide)) %>%
-    select(Tumor,Patient)
+  if(length(studiedGenes) < 2)
+    next
   
-  ngenes <- nrow(affected.wide)
+  this.affected.wide <- affected.wide[studiedGenes,]
   
-  plot.colors <- c(colorPalette, "amp" = "firebrick", "del" = "blue", "up" = NA, "down" = NA,
-                   "splicing" = "forestgreen", "germline" = "purple", "somatic" = "#36454F")
+  # remove those genes with alterations that affect less than 5% of the patients
+  ## remove empty columns
+  this.affected.wide <- this.affected.wide[,colSums(this.affected.wide != "") > 0]
+  minPatients <- floor(0.05 * ncol(this.affected.wide))
   
-  p <- oncoprint(affected.wide) + 
-    geom_tile(data=patients, aes(x=Patient,y=ngenes+1,fill=Tumor), height=0.3) +
-    scale_fill_manual(values = plot.colors) +
-    smartas_theme() +
-    theme(axis.text.x=element_blank())
+  if(sum(rowSums(this.affected.wide != "") > minPatients) < 2)
+    next
   
-  if (sampling.pval < 0.05){
-    sampling.pval <- format(sampling.pval,scientific=TRUE)
-    q <- p + labs(title=paste(gene,"p <= ",sampling.pval), x="")
-    ggsave(paste0(wdp,"ppi/resampling.",gene,".png"),q,  width=10, height=10)
-  }
+  ## remove new empty columns
+  this.affected.wide <- this.affected.wide[,colSums(this.affected.wide != "") > 0]
   
-  if (fisher.pval < 0.05){
-    fisher.pval <- format(fisher.pval,scientific=TRUE)
-    q <- p + labs(title=paste(gene,"p <= ",fisher.pval), x="")
-    ggsave(paste0(wdp,"ppi/fisher.",gene,".png"), q, width = 10, height = 10)
-  }
+  nonMutated <- affected.wide[, colnames(this.affected.wide)]
+  nonMutated[!grepl("MUT",nonMutated)] <- ""
+  mutatedPatients <- sum(colSums(nonMutated != "") > 0)/ncol(nonMutated)
+  
+  mutation.overlap[[gene]] <- c(length(studiedGenes), ncol(this.affected.wide), mutatedPatients)
 }
 
-result <- data.frame(Gene=gene,fisher.p=fisher.pval,sampling.p=sampling.pval,
-                     n.patients=sum(colSums(mutmat) > 0), n.genes=nrow(mutmat), 
-                     n.drivers=sum(rownames(mutmat) %in% drivers$geneHGNCsymbol))
-write_tsv(result,paste0(wdp,"ppi/",gene,".txt"))
+mutation.overlap <- do.call("rbind",mutation.overlap) %>%
+  as.data.frame %>%
+  set_colnames(c("num.genes","num.patients","prop.mutated")) %>%
+  mutate(., Driver=rownames(.))
+
+write_tsv(mutation.overlap, paste0(wdp,"ppi/me_ppi.tsv"))
--- methods/me_ppi.py
+++ methods/me_ppi.py
@@ -15,35 +15,10 @@ class MEPPI(method.Method):
 
 	def run(self):
 
-		self.logger.info("Preparing the environment variables.")
+		self.logger.info("Calculating proportion of pannegative.")
 
-		utils.cmd('Rscript', 'pipeline/methods/me_ppi.environment.R',
+		utils.cmd('Rscript', 'pipeline/methods/me_ppi.R',
 				  options.Options().wd, options.Options().qout)
-
-		self.logger.info("Launching jobs.")
-		i = 0
-		for symbol in utils.readTable("{}ppi/genes".format(options.Options().qout)):
-			symbol = symbol[0]
-			utils.geneclusterLaunch("{}-ppi".format(symbol), "~/anaconda/bin/Rscript",
-				'pipeline/methods/me_ppi.R', options.Options().wd, options.Options().qout, symbol)
-
-			i += 1
-
-			if i%1000 == 0:
-				time.sleep(200)
-
-		self.logger.info("Waiting for the all the threads to finish.")
-		time.sleep(1800)
-
-		files = glob.glob("{}ppi/*.txt".format(options.Options().qout))
-		outFile = "{}ppi/me_ppi.tsv".format(options.Options().qout)
-
-		with open(outFile,"w") as OUT:
-			OUT.write("Gene\tfisher.p\tsampling.p\tn.patients\tn.genes\tn.drivers\n")
-			for aFile in files:
-				with open(aFile) as IN:
-					IN.readline()
-					OUT.write(IN.read())
 
 if __name__ == '__main__':
 	pass
