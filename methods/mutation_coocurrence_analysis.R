e6146a42ce6a814465223182ca401c250cb5bdb9
Héctor Climente <hclimente@gmail.com>
2016-07-27 19:19:46 +0200

    maggitr'd
    print Tumor in the output

--- methods/mutation_coocurrence_analysis.R
+++ methods/mutation_coocurrence_analysis.R
@@ -1,4 +1,4 @@
-library(plyr)
+source("~/smartas/pipeline/scripts/variablesAndFunctions.r")
 
 args <- commandArgs(trailingOnly = TRUE)
 switches.in <- args[1]
@@ -10,67 +10,83 @@ out.candidates <- args[3]
 #out.candidates <- "/home/hector/smartas/analyses/luad/candidateList_mutationCoocurrence.tsv"
 
 # read WGS mutations
-wgs <- read.delim(wgs.in)
-colnames(wgs) <- c("Tumor","GeneId","Symbol","Patient","Position","Reference","Variant")
-wgs$Patient <- paste0(wgs$Patient,"T")
+wgs <- read_tsv(wgs.in) %>%
+	set_colnames(c("Tumor","GeneId","Symbol","Patient","Position","Reference","Variant")) %>%
+	mutate(Patient = paste0(Patient,"T"))
 
-wgs.patients <- unique(wgs[,c("GeneId","Symbol","Patient")])
-colnames(wgs.patients) <- c("GeneId","Symbol","MutPatient")
-wgs.patients$Patient <- wgs.patients$MutPatient
+wgs.patients <- unique(wgs[,c("GeneId","Symbol","Patient")]) %>%
+	set_colnames(c("GeneId","Symbol","MutPatient")) %>%
+	mutate(Patient = MutPatient)
 
 # get patients affected by any switch
 ## read switches
-switches.info <- read.delim(switches.in)
+switches.info <- read_tsv(switches.in)
 
 ## convert to same format as mutations
-switch.patients <- ddply(switches.info,.(GeneId,Symbol,Normal_transcript,Tumor_transcript),summarise,PatientAffected=unlist(strsplit(paste(Patients_affected,collapse=","),",")))
-colnames(switch.patients) <- c("GeneId","Symbol","Normal_transcript","Tumor_transcript","SwitchedPatient")
-switch.patients$Patient <- switch.patients$SwitchedPatient
+nocol <- switches.info$Patients_affected %>%
+  strsplit(",") %>%
+  lapply(length) %>%
+  unlist %>%
+  max
+
+switch.patients <- switches.info %>%
+  select(GeneId,Symbol,Normal_transcript,Tumor_transcript,Patients_affected) %>%
+  separate(Patients_affected, into=paste0("Patient_",1:nocol)) %>%
+  melt(measure.vars = paste0("Patient_",1:nocol)) %>%
+  select(-variable) %>%
+  set_colnames(c("GeneId","Symbol","Normal_transcript","Tumor_transcript","SwitchedPatient")) %>%
+  mutate(Patient = SwitchedPatient) %>%
+  filter(!is.na(SwitchedPatient))
 
 # get overlap between studied mechanisms
-wgs.patients <- subset(wgs.patients,MutPatient %in% switch.patients$SwitchedPatient)
-switch.patients <- subset(switch.patients,SwitchedPatient %in% wgs.patients$MutPatient)
+wgs.patients <- subset(wgs.patients, MutPatient %in% switch.patients$SwitchedPatient)
+switch.patients <- subset(switch.patients, SwitchedPatient %in% wgs.patients$MutPatient)
 
 # calculate unbalance
 ## get contingency table
 patients <- merge(wgs.patients,switch.patients,all=TRUE)
-co.tmp <- ddply(patients,.(GeneId,Symbol,Normal_transcript,Tumor_transcript),summarise,
-            MS=sum(!is.na(SwitchedPatient) & !is.na(MutPatient)),
-            M=sum(is.na(SwitchedPatient) & !is.na(MutPatient)),
-            S=sum(!is.na(SwitchedPatient) & is.na(MutPatient)))
+co.tmp <- patients %>%
+	group_by(GeneId,Symbol,Normal_transcript,Tumor_transcript) %>%
+	summarise(MS=sum(!is.na(SwitchedPatient) & !is.na(MutPatient)),
+		M=sum(is.na(SwitchedPatient) & !is.na(MutPatient)),
+		S=sum(!is.na(SwitchedPatient) & is.na(MutPatient)))
 
 ### get M cases (where no pair of transcripts can be inferred)
 co.s <- subset(co.tmp, !is.na(Normal_transcript) & !is.na(Tumor_transcript))
 co.m <- subset(co.tmp, is.na(Normal_transcript) & is.na(Tumor_transcript),select=c("GeneId","M"))
 
-co <- merge(co.m,co.s,by=c("GeneId"))
-co$M <- co$M.x
+co <- merge(co.m,co.s,by=c("GeneId")) %>%
+	mutate(M = M.x)
 
-discount <- ddply(patients,.(GeneId,Symbol),summarise,
-                  anySwitch=sum(!is.na(SwitchedPatient)))
+discount <- patients %>%
+	group_by(GeneId,Symbol) %>%
+	summarise(anySwitch = sum(!is.na(SwitchedPatient)))
 
 totalPatients <- length(unique(patients$Patient))
-co <- merge(co,discount,all.x=TRUE)
-co$N <- totalPatients - (co$MS+co$M+co$S) - (co$anySwitch - (co$MS+co$S))
+co <- merge(co,discount,all.x=TRUE) %>%
+	mutate(N = totalPatients - (MS+M+S) - (anySwitch - (MS + S)))
 
-co <- co[,c("GeneId","Symbol","Normal_transcript","Tumor_transcript","MS","M","S","N")]
+co <- co %>%
+  select(GeneId,Symbol,Normal_transcript,Tumor_transcript,MS,M,S,N)
 
 ## fisher test
-f <- apply(co[,c("MS","M","S","N")],1, function(x){
+co <- apply(co[,c("MS","M","S","N")],1, function(x){
   y <- x + 0.5  
   f <- fisher.test(x=matrix(x,nrow=2,ncol=2),alternative="greater")
   or <- y[1]*y[4]/(y[2]*y[3])
   
-  c(p.o=f$p.value,or=f$estimate,my.or=or)})
-
-f.df <- as.data.frame(t(f))
-colnames(f.df) <- c("p.o","OR","eOR")
-
-co <- cbind(co,f.df)
+  c(p.o=f$p.value,or=f$estimate,my.or=or)}) %>%
+	t %>%
+	as.data.frame %>%
+	set_colnames(c("p.o","OR","eOR")) %>%
+  cbind(co,.)
 
 # complete information and save
-co.info <- merge(co,switches.info[,c("GeneId","Symbol","Normal_transcript","Tumor_transcript","Annotation","DriverAnnotation","Driver","Druggable")],all.y=TRUE)
-co.info <- co.info[order(co.info$p.o),]
-co.info <- co.info[,c("GeneId","Symbol","Normal_transcript","Tumor_transcript","MS","M","S","N","p.o")]
+co.info <- switches.info %>%
+	select(GeneId,Symbol,Normal_transcript,Tumor_transcript,Annotation,DriverAnnotation,Driver,Druggable) %>%
+	merge(co,all.x=TRUE) %>%
+	arrange(p.o) %>%
+  mutate(Tumor=unique(wgs$Tumor)) %>%
+	select(Tumor,GeneId,Symbol,Normal_transcript,Tumor_transcript,MS,M,S,N,p.o)
 
 write.table(co.info, out.candidates, sep="\t", row.names=F, quote=F)
