343438711ad343202fedf819b2e0ff7ff0924762
Héctor Climente <hclimente@gmail.com>
2016-08-03 18:17:29 +0200

    maggitr'd

--- methods/mutual_exclusion_analysis.R
+++ methods/mutual_exclusion_analysis.R
@@ -1,20 +1,24 @@
+source("~/smartas/pipeline/scripts/variablesAndFunctions.r")
+
 args <- commandArgs(trailingOnly = TRUE)
 wes.in <- args[1]
 wgs.in <- args[2]
 out.file <- args[3]
 
-wes <- read.delim(wes.in)
-wes <- wes[,c("Tumor","GeneId","Symbol","Normal_transcript","Tumor_transcript","MS","M","N","p")]
-wes$p.me <- wes$p
-wgs <- read.delim(wgs.in)
+wes <- read_tsv(wes.in) %>%
+  select(Tumor,GeneId,Symbol,Normal_transcript,Tumor_transcript,MS,M,N,p) %>%
+  mutate( p.me = p)
+wgs <- read_tsv(wgs.in)
 
-meScore <- merge(wgs,wes,all.x=TRUE,by=c("GeneId","Symbol","Normal_transcript","Tumor_transcript"))
-
-meScore$score <- -log10(meScore$p.me) + log10(meScore$p.o)
-meScore <- meScore[order(-meScore$score),]
+meScore <- merge(wgs, wes, all.x=TRUE,
+                 by=c("Tumor","GeneId","Symbol","Normal_transcript","Tumor_transcript")) %>%
+  mutate(score = -log10(p.me) + log10(p.o)) %>%
+  arrange(score)
 
 top <- quantile(meScore$score,0.99,na.rm=T)
-meScore$candidate <- 0
-meScore$candidate[meScore$score > top] <- 1
 
-write.table(meScore[,c("GeneId","Symbol","Normal_transcript","Tumor_transcript","candidate")], out.file, sep="\t", row.names=F, quote=F)
+meScore <- mutate(meScore, candidate = ifelse( meScore$score > top, 1, 0))
+
+meScore %>%
+  select(Tumor,GeneId,Symbol,Normal_transcript,Tumor_transcript,candidate) %>%
+  write_tsv(out.file)
