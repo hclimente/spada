54ffa5bff4814a66eb3eb896b673a89031e12a81
Héctor Climente <hclimente@gmail.com>
2016-08-03 18:15:27 +0200

    create both pancancer and split candidateList_full

--- scripts/create_macrotable.R
+++ scripts/create_macrotable.R
@@ -1,23 +1,40 @@
 #!/usr/bin/env Rscript
 
 source("~/smartas/pipeline/scripts/variablesAndFunctions.r")
-
 wd <- "~/smartas/analyses/pancancer/"
+
+#############################
+##        AGGREGATED       ##
+#############################
 
 switches <- read_tsv(paste0(wd,"candidateList_info.agg.tsv"))
 candidates <- read_tsv("~/smartas/notebook/data/switches/driverEvidence.tsv") %>%
                          group_by(Normal_transcript,Tumor_transcript) %>%
                          summarise(Recurrence = max(Recurrence), DriverME = max(DriverME),
                                    Affects_mutated_feature = max(Affects_mutated_feature),
-                                   PPI = max(PPI), Pannegative = max(Pannegative)) %>%
-                         as.data.frame
+                                   PPI = max(PPI), Pannegative = max(Pannegative))
 wes <- read_tsv(paste0(wd,"mutations/gene_functional_mutations_all_switches.txt")) %>%
 	set_colnames(c("GeneId","Symbol","Normal_transcript","Tumor_transcript","MS.pam","M.pam","S.pam","N.pam","p.pam.me"))
 wgs <- read_tsv(paste0(wd,"mutations/gene_wgs_mutations_all_switches.txt")) %>%
 	set_colnames(c("GeneId","Symbol","Normal_transcript","Tumor_transcript","MS.mut","M.mut","S.mut","N.mut","p.mut.o"))
 
-x <- merge(switches,candidates,all.x=T) %>%
+merge(switches,candidates,all.x=T) %>%
 	merge(wes) %>%
-	merge(wgs,all.x=T)
+	merge(wgs,all.x=T) %>%
+  write_tsv(paste0(wd,"candidateList_full.tsv"))
 
-write_tsv(x,paste0(wd,"candidateList_full.tsv"))
+#############################
+##       TUMOR-SPLIT       ##
+#############################
+
+switches <- read_tsv(paste0(wd,"candidateList_info.tumorSplit.tsv"))
+candidates <- read_tsv("~/smartas/notebook/data/switches/driverEvidence.tsv")
+wes <- read_tsv("~/smartas/notebook/data/mutations/gene_functional_mutations_all_switches.txt") %>%
+  set_colnames(c("Tumor","GeneId","Symbol","Normal_transcript","Tumor_transcript","MS.pam","M.pam","S.pam","N.pam","p.pam.me","OR.pam","Switched","PAM"))
+wgs <- read_tsv("~/smartas/notebook/data/mutations/gene_wgs_mutations_all_switches.txt") %>%
+  set_colnames(c("Tumor","GeneId","Symbol","Normal_transcript","Tumor_transcript","MS.mut","M.mut","S.mut","N.mut","p.mut.o"))
+
+merge(switches,candidates,all.x=T) %>%
+  merge(wes) %>%
+  merge(wgs,all.x=T) %>%
+  write_tsv(paste0(wd,"candidateList_full.tumorSplit.tsv"))
