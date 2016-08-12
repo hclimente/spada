1be0f271a4ead13f853c5f0d46b04c97f9e5e832
Héctor Climente <hclimente@gmail.com>
2016-08-12 18:28:11 +0200

    if there is an origin column, discard those switches where origin!=Tumor

--- methods/recurrence_analysis.R
+++ methods/recurrence_analysis.R
@@ -1,22 +1,29 @@
+source("~/smartas/pipeline/scripts/variablesAndFunctions.r")
+
 args <- commandArgs(trailingOnly = TRUE)
 switches.file <- args[1]
 out.file <- args[2]
 
 # read switches
-switches <- read.delim(switches.file)
-
-## calculate patient number
-switches$Patient_number <- unlist(lapply(strsplit(as.character(switches$Patients_affected),","),length))
+switches <- read_tsv(switches.file) %>%
+  ## calculate patient number
+  mutate(Patient_number = Patients_affected %>% strsplit(",") %>% lapply(length) %>% unlist)
+         
+if ("Origin" %in% colnames(switches)){
+  switches <- switches %>%
+    filter(Origin == "Tumor")
+}
 
 # calculate expected frequency of a switch
-patientNumber <- length(unique(unlist(strsplit(as.character(switches$Patients_affected),","))))
-geneNumber <- length(unique(switches$GeneId))
-p <- sum(switches$Patient_number)/(geneNumber*patientNumber)
+patientNumber <- switches$Patients_affected %>% strsplit(",") %>% unlist %>% unique %>% length
+geneNumber <- switches$GeneId %>% unique %>% length
+f.exp <- sum(switches$Patient_number)/(geneNumber*patientNumber)
 
 # binomial test
-tests <- lapply(switches$Patient_number, binom.test, patientNumber, p, "greater")
-switches$p.recurrence <- unlist(lapply(tests,function(x){x$p.value}))
-switches$padj.recurrence <- p.adjust(switches$p.recurrence)
-
-# save results
-write.table(switches[,c("GeneId","Symbol","Normal_transcript","Tumor_transcript","p.recurrence","padj.recurrence")], out.file, sep="\t", row.names=F, quote=F)
+tests <- lapply(switches$Patient_number, binom.test, patientNumber, f.exp, "greater")
+switches %>%
+  mutate(p.recurrence = unlist(lapply(tests,function(x){x$p.value})),
+         padj.recurrence = p.adjust(p.recurrence)) %>%
+  # save results
+  select(GeneId,Symbol,Normal_transcript,Tumor_transcript,p.recurrence,padj.recurrence) %>%
+  write_tsv(out.file)
