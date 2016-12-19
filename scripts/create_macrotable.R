c2fd3036f625a8865e140b4399c9eedcfa2ace06
Hector Climente <hclimente@gmail.com>
2016-12-19 15:37:02 +0100

    separated switch origin from macrotable creation

--- scripts/create_macrotable.R
+++ scripts/create_macrotable.R
@@ -5,44 +5,8 @@ wd <- "~/smartas/analyses/pancancer/"
 nb <- "~/smartas/notebook/data/"
 library(tidyr)
 
-#############################
-##      STROMA/IMMUNE      ##
-#############################
-
-# switches with high correlation with:
-## stromal content
-stromaCorr <- lapply(cancerTypes, function(tumor){
-  filename <- paste0(nb,"eporta/estimate/", tumor, "_ouptut_r_stroma.txt")
-  read.delim(filename) %>%
-    mutate(., Switch=row.names(.), Tumor=tumor, SwitchCorrelation="Stroma") %>%
-    filter(FDR_linear < 0.05 | FDR_wilcox < 0.05) %>%
-    separate(Switch,into=c("Normal_transcript","Tumor_transcript"), sep="-")
-}) %>% do.call("rbind", .)
-
-## immune content
-immuneCorr <- lapply(cancerTypes, function(tumor){
-  filename <- paste0(nb,"eporta/estimate/", tumor, "_ouptut_r_immune.txt")
-  read.delim(filename) %>%
-    mutate(., Switch=row.names(.), Tumor=tumor, SwitchCorrelation="Immune") %>%
-    filter(FDR_linear < 0.05 | FDR_wilcox < 0.05) %>%
-    separate(Switch,into=c("Normal_transcript","Tumor_transcript"), sep="-")
-}) %>% do.call("rbind", .)
-
-contaminantSwitches <- rbind(stromaCorr[,c("Normal_transcript","Tumor_transcript","SwitchCorrelation")],
-                             immuneCorr[,c("Normal_transcript","Tumor_transcript","SwitchCorrelation")])
-
-# cell-lineage specific genes
-cellSpecificGenes <- paste0(nb,"eporta/estimate/geneset_ESTIMATE.txt") %>%
-  read_tsv(col_names = FALSE) %>% 
-  t %>%
-  as.data.frame %>%
-  set_colnames(c("StromalSignature","ImmuneSignature")) %>%
-  .[3:nrow(.),]
-
-stromaGenes <- data.frame(Symbol=cellSpecificGenes$StromalSignature, Genetype="Stroma")
-immuneGenes <- data.frame(Symbol=cellSpecificGenes$ImmuneSignature, Genetype="Immune")
-
-contaminantGenes <- rbind(stromaGenes,immuneGenes)
+# read stroma/immune information
+origin <- read_tsv(paste0(wd,"candidateList.origin.tsv"))
 
 #############################
 ##        AGGREGATED       ##
@@ -70,19 +34,7 @@ switches <- merge(switches,candidates,all.x=T) %>%
          Candidate = ifelse(IsFunctional == 1, Candidate, 0))
 
 switches %>%
-  left_join(contaminantSwitches) %>%
-  left_join(contaminantGenes) %>%
-  select(Normal_transcript, Tumor_transcript, Genetype,SwitchCorrelation) %>%
-  mutate(Genetype = ifelse(is.na(Genetype), "Tumor", as.character(Genetype)),
-         SwitchCorrelation = ifelse(is.na(SwitchCorrelation), "Tumor", SwitchCorrelation),
-         # cases where the switch and the gene analysis do not agree are "Uncertain"
-         Origin = ifelse(Genetype==SwitchCorrelation, SwitchCorrelation, "Uncertain"),
-         Origin = ifelse(Origin=="Uncertain" & SwitchCorrelation=="Tumor", Genetype, Origin),
-         Origin = ifelse(Origin=="Uncertain" & Genetype=="Tumor", SwitchCorrelation, Origin)) %>%
-  group_by(Normal_transcript,Tumor_transcript) %>%
-  summarise(Origin = paste0(sort(unique(Origin)), collapse="&")) %>%
-  mutate(Origin = ifelse(grepl("&Uncertain",Origin), "Uncertain", Origin)) %>%
-  merge(switches) %>%
+  merge(origin) %>%
   # rearrange columns
   select(GeneId,Symbol,Normal_transcript,Tumor_transcript,Normal_protein:IsFunctional,Origin,
          Driver:Pannegative, Candidate, MS.pam:p.mut.o) %>%
@@ -109,20 +61,8 @@ switches.split <- merge(switches.split,candidates,all.x=T) %>%
   merge(candidateInfo)
 
 switches.split %>%
-  left_join(contaminantSwitches) %>%
-  left_join(contaminantGenes) %>%
-  select(Normal_transcript, Tumor_transcript, Genetype,SwitchCorrelation) %>%
-  mutate(Genetype = ifelse(is.na(Genetype), "Tumor", as.character(Genetype)),
-         SwitchCorrelation = ifelse(is.na(SwitchCorrelation), "Tumor", SwitchCorrelation),
-         # cases where the switch and the gene analysis do not agree are "Uncertain"
-         Origin = ifelse(Genetype==SwitchCorrelation, SwitchCorrelation, "Uncertain"),
-         Origin = ifelse(Origin=="Uncertain" & SwitchCorrelation=="Tumor", Genetype, Origin),
-         Origin = ifelse(Origin=="Uncertain" & Genetype=="Tumor", SwitchCorrelation, Origin)) %>%
-  group_by(Normal_transcript,Tumor_transcript) %>%
-  summarise(Origin = paste0(sort(unique(Origin)), collapse="&")) %>%
-  mutate(Origin = ifelse(grepl("&Uncertain",Origin), "Uncertain", Origin)) %>%
-  merge(switches.split) %>%
+  merge(origin) %>%
   # rearrange columns
   select(Tumor,GeneId,Symbol,Normal_transcript,Tumor_transcript,Normal_protein:IsFunctional,
          Reliable,Origin,Driver:Pannegative, Candidate, MS.pam:p.mut.o) %>%
-  write_tsv(paste0(wd,"candidateList_full.tumorSplit.tsv")) 
+  write_tsv(paste0(wd,"candidateList_full.tumorSplit.tsv"))
