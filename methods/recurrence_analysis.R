ce5d269d970bea47917b080fcbb85f9e2b6f864e
Hector Climente <hclimente@gmail.com>
2017-01-05 18:19:05 +0100

    give information on all the switches

--- methods/recurrence_analysis.R
+++ methods/recurrence_analysis.R
@@ -7,8 +7,9 @@ out.file <- args[3]
 
 # read switches
 origin <- read_tsv(switches.origin)
+switches <- read_tsv(switches.file)
 
-switches <- read_tsv(switches.file) %>%
+frequencies <- switches %>%
   merge(origin) %>%
   filter(Origin == "Tumor") %>%
   ## calculate patient number
@@ -20,11 +21,15 @@ geneNumber <- switches$GeneId %>% unique %>% length
 f.exp <- sum(switches$Patient_number)/(geneNumber*patientNumber)
 
 # binomial test
-tests <- lapply(switches$Patient_number, binom.test, patientNumber, f.exp)
-switches %>%
+tests <- lapply(frequencies$Patient_number, binom.test, patientNumber, f.exp)
+frequencies <- frequencies %>%
   mutate(p.recurrence = unlist(lapply(tests,function(x){x$p.value})),
          padj.recurrence = p.adjust(p.recurrence),
-         what = ifelse(switches$Patient_number/patientNumber > f.exp, "greater", "less")) %>%
+         what = ifelse(switches$Patient_number/patientNumber > f.exp, "greater", "less"))
+
+# complete with unused switches
+frequencies %>%
+  merge(switches, all.y = T) %>%
   # save results
   select(GeneId,Symbol,Normal_transcript,Tumor_transcript,p.recurrence,padj.recurrence,what) %>%
   write_tsv(out.file)
