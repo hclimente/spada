65c946e5e3a4e3336f605781f60f73480ab6695c
Hector Climente <hclimente@gmail.com>
2017-03-17 18:01:09 +0100

    added driverness surrogates measurement

--- methods/annotate_switches.py
+++ methods/annotate_switches.py
@@ -3,6 +3,7 @@ from libs import utils
 from methods import method
 
 import os
+import networkx as nx
 
 class AnnotateSwitches(method.Method):
 	def __init__(self,gn_network,tx_network):
@@ -12,6 +13,7 @@ class AnnotateSwitches(method.Method):
 
 	def run(self):
 
+		# read AS driver information
 		self.logger.info("Reading pancancer recurrence.")
 		recurrence = self.readRecurrence()
 		self.logger.info("Reading affection to mutated features.")
@@ -21,11 +23,20 @@ class AnnotateSwitches(method.Method):
 		self.logger.info("Reading pannegative switches and mutual exclusion with drivers.")
 		panneg = self.readPannegative()
 
+		# calculate AS driverness surrogates
+		self.logger.info("Calculating centrality.")
+		centrality = self.getCentrality()
+		self.logger.info("Calculating mutual exclusion.")
+		me = self.getME()
+		self.logger.info("Calculating distance to closest driver.")
+		ccd = self.getClosestDriverDistance()
+
 		self.logger.info("Annotating tumor-type switches.")
 		with open("{}candidateList_driverEvidence.tsv".format(options.Options().qout),"w") as OUT:
 			OUT.write("Tumor\tGeneId\tSymbol\tNormal_transcript\t")
 			OUT.write("Tumor_transcript\tRecurrence\t")
-			OUT.write("Affects_mutated_feature\tPPI\tPannegative\n")
+			OUT.write("Affects_mutated_feature\tPPI\tPannegative\t")
+			OUT.write("Centrality\tME\tCCD\n")
 
 			for gene,info,switchDict,thisSwitch in self._gene_network.iterate_switches_byPatientNumber(
 				self._transcript_network,partialCreation=True, removeNoise=False):
@@ -41,7 +52,14 @@ class AnnotateSwitches(method.Method):
 
 				OUT.write("{}\t{}\t{}\t{}\t{}\t".format(options.Options().tag,gene,symbol,nTx,tTx))
 				OUT.write("{}\t{}\t".format(recurrence[swt],mutAffectation[swt]))
-				OUT.write("{}\t{}\n".format(ppi[swt],panneg[swt]))
+				OUT.write("{}\t{}\t".format(ppi[swt],panneg[swt]))
+
+				centrality.setdefault(gene,0)
+				me.setdefault(swt,0)
+				ccd.setdefault(swt,0)
+
+				OUT.write("{}\t{}\t".format(centrality[gene],me[swt]))
+				OUT.write("{}\n".format(ccd[swt]))
 
 	def readProteome(self):
 		proteome = {}
@@ -134,6 +152,47 @@ class AnnotateSwitches(method.Method):
 				panneg[switch] = 0
 
 		return(panneg)
+
+	def getCentrality(self):
+		centrality = nx.degree_centrality(self._gene_network._net)
+		return(centrality)
+
+	def getME(self):
+		me = {}
+
+		for i in range(3,11):
+			infile = "{}mutations/pannegative_mutual_exclusion.top_{}_drivers.txt".format(options.Options().qout,i)
+
+			if not os.path.isfile(infile):
+				break
+			else:
+				for line in utils.readTable(infile):
+					switch = "{}_{}".format(line[3],line[4])
+
+					p = float(line[9])
+					me.setdefault(switch,0)
+
+					if p < 0.05 and me[switch] > 0:
+						J = float(line[5])/(float(line[5]) + float(line[6]) + float(line[7]))
+						me[switch] = 1 - J
+
+		return(me)
+
+	def getClosestDriverDistance(self):
+		cdd = {}
+
+		for gene,info,switchDict,thisSwitch in self._gene_network.iterate_switches_byPatientNumber(
+			self._transcript_network,partialCreation=True, removeNoise=False):
+
+			nTx = switchDict["nIso"]
+			tTx = switchDict["tIso"]
+			swt = "{}_{}".format(nTx,tTx)
+
+			for visiting_node in nx.bfs_tree(self._gene_network._net, gene):
+	    		if self._gene_network._net.node[visiting_node].get('driver', True):
+	        		cdd[swt] = nx(self._gene_network._net, gene, visiting_node)
+
+		return(cdd)
 
 if __name__ == '__main__':
 	pass
