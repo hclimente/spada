e730124933c813b409f0dcba4f02f6f495ee3b2f
hclimente <hclimente@gmail.com>
2017-10-09 17:36:00 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+  name='spada',
+  packages=['spada'],
+  version='1.0',
+  description='Find splicing-led, functional changes of the proteome. ',
+  author='Héctor Climente-González',
+  author_email='eduardo.eyras@upf.edu',
+  license='MIT',
+  url='https://github.com/hclimente/spada',
+  download_url='https://github.com/hclimente/spada/archive/v1.0.tar.gz',
+  keywords=['alternative', 'splicing', 'analysis', 'transcriptomics', 'networks'],
+  classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5'],
+  install_requires=['networkx>=1.11',
+                    'pandas>=0.20.3',
+                    'biopython>=1.69']
+)
