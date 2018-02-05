44f0748893660941cb9169dbc05f0128462c0cb3
hclimente <hclimente@gmail.com>
2018-02-05 12:13:24 +0100

    README and setup extended

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
+# SPADA
+
 [![Build Status](https://travis-ci.org/hclimente/spada.svg?branch=master)](https://travis-ci.org/hclimente/spada)
 [![codecov](https://codecov.io/gh/hclimente/spada/branch/master/graph/badge.svg)](https://codecov.io/gh/hclimente/spada)
 
-SPADA (Splicing-led Protein Alterations Discovered Agilely) is a pipeline oriented to finding interesting isoform switches between two conditions. An analysis of the TCGA dataset using this pipeline was published in [Climente-González *et al.* (2017)](http://www.cell.com/cell-reports/abstract/S2211-1247(17)31104-X). For information about how to run SPADA on your dataset, please read the [documentation](docs/index.md).
+**WARNING:** *SPADA is still a work-in-progress. Take this README as a statement of intent.*
+
+SPADA (Splicing-led Protein Alterations Discovered Agilely) is a tool to study the functional impact of alternative splicing changes between two conditions. The alterations have to be represented as isoform switches i.e. when one condition is best represented by one isoform, and the second condition by another one. Then, SPADA is able to predict which protein features are changing between both isoforms, and their impact on the protein-protein interaction network. A SPADA-based analysis of isoform switches found in the TCGA dataset was published in [Climente-González *et al.* (2017)](http://www.cell.com/cell-reports/abstract/S2211-1247(17)31104-X).
+
+To start using SPADA, simply install it with
+
+``` python
+pip install spada
+```
+
+and run it in a toy dataset with
+
+``` python
+spada.py function --annotation gencode --switches spada.example
+```
+
+For information about how to run SPADA on your dataset, please read the [documentation](docs/index.md).
--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-  name='spada',
-  packages= {'spada': 'spada'},
-  version='1.9',
-  description='Find splicing-led, functional changes of the proteome. ',
-  author='Héctor Climente-González',
-  author_email='eduardo.eyras@upf.edu',
-  license='MIT',
-  url='https://github.com/hclimente/spada',
-  package_data={'spada': ['data/*.pkl']},
-  download_url='https://github.com/hclimente/spada/archive/v1.9.1.tar.gz',
-  keywords=['alternative', 'splicing', 'analysis', 'transcriptomics', 'networks'],
-  classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.5'],
-  install_requires=['networkx>=1.11',
-                    'pandas>=0.20.3',
-                    'numpy>=1.13.1']
+	name = 'spada',
+	packages = {'spada': 'spada'},
+	package_dir = {'spada': 'spada'},
+	package_data = {'spada': ['data/*.pkl']},
+	version = '1.9.1b1',
+	description = 'Find splicing-led, functional changes of the proteome. ',
+	author = 'Héctor Climente-González',
+	author_email = 'eduardo.eyras@upf.edu',
+	license = 'MIT',
+	url = 'https://github.com/hclimente/spada',
+	download_url = 'https://github.com/hclimente/spada/archive/v1.9.1.tar.gz',
+	keywords = ['alternative', 'splicing', 'analysis', 'transcriptomics', 'networks'],
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Topic :: Scientific/Engineering :: Bio-Informatics',
+		'Intended Audience :: Science/Research',
+		'Intended Audience :: Healthcare Industry',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: POSIX :: Linux',
+		'Operating System :: MacOS',
+		'Operating System :: Microsoft :: Windows',
+		'Programming Language :: Python :: 3 :: Only'],
+	install_requires = [
+		'networkx >= 1.11',
+		'pandas >= 0.20.3',
+		'numpy >= 1.13.1']
 )
