087e7c2df06cadf6254174d15882302fdd60ee0f
hclimente <hclimente@gmail.com>
2018-02-24 03:07:59 +0100

    finished first functioning version

--- scripts/calculate_switches.r
+++ scripts/calculate_switches.r
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 # get the empirical distribution of the differences between a vector
 #   x numerical vector
 #   minV minimum value to consider that measure
@@ -30,114 +32,129 @@ getEmpiricalDistribution <- function(x,minV){
   }
 }
 
-readExpression <- function(nTxFile, tTxFile, tx2geneFile) {
-  
-  tpmRead <- function(file) {
-    read_tsv(file) %>% gather(key = sample, value = expr, -1)
+readTxExpression <- function(nTxFile, tTxFile, tx2geneFile) {
+
+  tpmRead <- function(f) {
+    read_tsv(f, col_types = cols(transcript = "c", .default = "d")) %>%
+	gather(key = sample, value = expr, -1)
   }
-  
-  tx2gene <- read_tsv(tx2geneFile)
-  
+
+  tx2gene <- read_csv(tx2geneFile, col_types = 'cc')
+
   left_join(tpmRead(tTxFile) %>% rename(tpmTumor = expr),
-            tpmRead(nTxFile) %>% rename(tpmNormal = expr)) %>% 
-    left_join(tx2gene) %>% 
+            tpmRead(nTxFile) %>% rename(tpmNormal = expr),
+		    by = c("transcript", "sample")) %>%
+    left_join(tx2gene, by = "transcript") %>%
     select(gene, transcript, sample, tpmTumor, tpmNormal) %>%
     arrange(sample, gene, transcript)
-  
+
 }
 
 calculateDPSI <- function(expression) {
-  
-  dpsi <- expression %>% 
-    group_by(gene, sample) %>% 
+
+  dpsi <- expression %>%
+    group_by(gene, sample) %>%
     mutate(psiNormal = tpmNormal/sum(tpmNormal),
            psiTumor = tpmTumor/sum(tpmTumor)) %>%
     ungroup
-  
+
   normalMedianPsi <- dpsi %>%
     group_by(transcript) %>%
     summarize(psiNormalMedian = median(psiNormal, na.rm = T))
-  
-  left_join(dpsi, normalMedianPsi) %>%
+
+  left_join(dpsi, normalMedianPsi, by = "transcript") %>%
     mutate(psiNormal = ifelse(is.na(psiNormal), psiNormalMedian, psiNormal),
-           deltaPsi = psiNormal - psiTumor) %>%
+           dPSI = psiNormal - psiTumor) %>%
     select(-psiNormalMedian)
-  
+
 }
 
 scoreDPSI <- function(dpsi) {
-  
-  pDPSI <- by(dpsi, dpsi$transcript, function(x) { getEmpiricalDistribution(x$deltaPsi, 0) })
-  
+
+  pdPSI <- by(dpsi, dpsi$transcript, function(x) { getEmpiricalDistribution(x$dPSI, 0) })
+
   dpsi %>%
     group_by(transcript) %>%
-    mutate(pDPSI = pDPSI[[unique(transcript)]](deltaPsi)) %>%
+    mutate(pdPSI = pdPSI[[unique(transcript)]](dPSI)) %>%
     ungroup
-  
+
 }
 
 calculateDE <- function(normalGeneExpressionFile, tumorGeneExpressionFile) {
-  
-  tumorGeneExpression <- read.table(tumorGeneExpressionFile, check.names=FALSE)
-  tumorSamples <- colnames(tumorGeneExpression)
-  normalGeneExpression <- read.table(normalGeneExpressionFile, check.names=FALSE)
-  normalSamples <- colnames(normalGeneExpression)
-  
-  geneExpression <- cbind(tumorGeneExpression, normalGeneExpression)
-  y <- DGEList(counts = geneExpression)
-  y <- calcNormFactors(y)
-  normGeneExpression <- cpm(y, normalized.lib.sizes=TRUE)
-  
-  logNormGeneExpression <- log2(normGeneExpression)
-  logNormGeneExpression[logNormGeneExpression == -Inf] <- NA
-  
-  tibble(gene = rownames(logNormGeneExpression),
-         pDE = apply(logNormGeneExpression, 1, function(x) {
-           wilcox.test(x[normalSamples], x[tumorSamples])$p.value
-         }))
-  
+
+	# read expression files
+	normalGeneExpression <- read_tsv(normalGeneExpressionFile,
+									 col_types = cols(gene = 'c', .default = 'i'))
+	normalSamples <- colnames(normalGeneExpression)
+ 	tumorGeneExpression <- read_tsv(tumorGeneExpressionFile,
+	  							    col_types = cols(gene = 'c', .default = 'i'))
+	tumorSamples <- colnames(tumorGeneExpression)
+
+	# generate expression matrix
+	geneExpression <- left_join(tumorGeneExpression, normalGeneExpression,
+	  						  by = 'gene')
+	genes <- geneExpression$gene
+	geneExpression <- select(geneExpression, -gene) %>% as.matrix
+	rownames(geneExpression) <- genes
+
+	# normalize expression and calculate log2
+	y <- DGEList(counts = geneExpression)
+	y <- calcNormFactors(y)
+
+	logNormGeneExpression <- cpm(y, normalized.lib.sizes=TRUE) %>%
+		log2
+	logNormGeneExpression[logNormGeneExpression == -Inf] <- NA
+
+	# calculate differential expression
+	tibble(gene = rownames(logNormGeneExpression),
+		   pDE = apply(logNormGeneExpression, 1, function(x) {
+			   wilcox.test(x[normalSamples], x[tumorSamples])$p.value
+		   }))
+
 }
 
 calculateSwitches <- function(txInfo) {
-  
+
   txInfo %>%
     # remove differentially expressed genes
-    filter(dGE > 0.01)
+    filter(pDE > 0.01) %>%
     # remove low deltaPSI
-    filter(pDPSI < 0.01 & deltaPsi > 0.05) %>%
+    filter(pdPSI < 0.01 & dPSI > 0.05) %>%
     # remove lowly expressed transcript
-    filter((deltaPsi > 0 & tpmTumor > 0.1) | (deltaPsi < 0 & tpmNormal > 0.1)) %>%
+    filter((dPSI > 0 & tpmTumor > 0.1) | (dPSI < 0 & tpmNormal > 0.1)) %>%
     group_by(gene, sample) %>%
     # remove unelligible genes
-    filter(n() > 1 & any(deltaPsi > 0) & any(deltaPsi < 0)) %>%
-    summarise(normal = transcript[which.min(deltaPsi)],
-              tumor = transcript[which.max(deltaPsi)]) %>%
+    filter(n() > 1 & any(dPSI > 0) & any(dPSI < 0)) %>%
+    summarise(normal = transcript[which.min(dPSI)],
+              tumor = transcript[which.max(dPSI)]) %>%
     ungroup %>%
     group_by(gene, normal, tumor) %>%
     summarise(samples = paste(samples, collapse = ','))
-  
+
 }
 
 library(tidyverse)
 library(edgeR)
 
-setwd("~/projects/spada/spada/tests/data")
-nTxFile <- "expression"
-tTxFile <- "expression"
-tx2geneFile <- "tx2gene"
-outfile <- args[4]
+# cd /Users/hclimente/projects/spada/spada/tests/data/calculate_switches
+# /Users/hclimente/projects/spada/scripts/calculate_switches.R tx_normal tx_tumor gn_normal gn_tumor tx2gene switches.tsv
+args <- commandArgs(trailingOnly = TRUE)
+nTxFile <- args[1]
+tTxFile <- args[2]
+nGnFile <- args[3]
+tGnFile <- args[4]
+tx2geneFile <- args[5]
+outfile <- args[6]
 
-# args <- commandArgs(trailingOnly = TRUE)
-# nTxFile <- args[1]
-# tTxFile <- args[2]
-# tx2geneFile <- args[3]
-# outfile <- args[4]
+print("Differential transcript-expression")
+txDPSI <- readTxExpression(nTxFile, tTxFile, tx2geneFile) %>%
+	calculateDPSI %>%
+	scoreDPSI
 
-txExpression <- readExpression(nTxFile, tTxFile, tx2geneFile)
+print("Calculate differential gene-expression")
 gnDiffExpr <- calculateDE(nGnFile, tGnFile)
-txDPSI <- calculateDPSI(txExpression)
-txDPSI <- scoreDPSI(txDPSI)
 
-txInfo <- left_join(txDPSI, gnDiffExpr)
-
-calculateSwitches(txInfo)
+print("Compute switches")
+left_join(txDPSI, gnDiffExpr, by = "gene") %>%
+	calculateSwitches %>%
+	write_tsv(outfile)
