8983a7cdb8237bae821f57f2ca9a546d07f5f751
hclimente <hclimente@gmail.com>
2018-07-12 10:51:16 +0900

    fixed tests

--- spada/tests/unit_tests/methods/test_summary.py
+++ spada/tests/unit_tests/methods/test_summary.py
@@ -10,12 +10,12 @@ dataPath = os.path.dirname(scriptPath) + "/../../data/"
 def test_init():
 
 	s = summary.Summary(dataPath + 'annotation.pklz')
-						
+
 
 def test_run():
 
 	s = summary.Summary(dataPath + 'annotation.pklz')
-						
+
 	s.run(dataPath + "expression", dataPath + "expression_case")
 
 	os.remove('proteome_features.tsv')
@@ -28,19 +28,24 @@ def test_run():
 def test_proteomeStatistics():
 
 	s = summary.Summary(dataPath + 'annotation.pklz')
-						
+
 	s.proteomeStatistics(dataPath + "expression", dataPath + "expression_case")
 	proteome = [ x for x in io.readTable("proteome_features.tsv") ]
 
-	assert len(proteome) == 4 # ENSG00.5 isnot complete, as we don't have info for JKLM-.3 and DE_FG_HI.2
+	assert len(proteome) == 16
+	txs = [ x['Transcript'] for x in proteome if x['Feature_type'] == 'Transcript' ]
+	assert len(txs) == len(set(txs))
+
+	# ENSG00.5 is not complete, as we don't have info for JKLM-.3 and DE_FG_HI.2
+	assert not [ x for x in proteome if x['GeneId'] == 'ENSG00.5' ]
 
 	assert len([ x for x in proteome if x['Feature_type'] == 'Prosite' ]) == 3
 	assert len([ x for x in proteome if x['Feature_type'] == 'Pfam' ]) == 1
 
 	# ENST08.1 goes first in the file, and both have same median
-	assert len([ x for x in proteome if x['Transcript'] == 'ENST08.1' ]) == 2
+	assert len([ x for x in proteome if x['Transcript'] == 'ENST08.1' and x['Feature_type'] != 'Transcript' ]) == 2
 	assert len([ x for x in proteome if x['Transcript'] == 'ENST09.1' ]) == 0
 
-	assert len([ x for x in proteome if x['Transcript'] == 'ENST18.3' ]) == 1
+	assert len([ x for x in proteome if x['Transcript'] == 'ENST18.3' and x['Feature_type'] != 'Transcript' ]) == 1
 
 	os.remove('proteome_features.tsv')
