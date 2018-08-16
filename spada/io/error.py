bebf3744f2c54a1a9668193ddfd4b8c12670058e
hclimente <hclimente@gmail.com>
2018-08-16 16:27:30 +0900

    revert reversion

--- spada/io/error.py
+++ spada/io/error.py
@@ -2,8 +2,9 @@ import logging
 
 class SpadaError(Exception):
 	def __init__(self, value):
-		self.logger = logging.getLogger('spada error')
+		self.logger = logging.getLogger(__name__)
 		self.value = value
+		self.logger.exception(value) 
 	def __str__(self):
 		self.logger.exception(value)
 		return repr(self.value)
