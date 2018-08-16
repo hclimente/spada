f50df6592dbcff9d15aaf7886128902c936bee36
hclimente <hclimente@gmail.com>
2018-08-16 16:21:00 +0900

    bugfix spada error not interacting properly with logging

--- spada/io/error.py
+++ spada/io/error.py
@@ -4,6 +4,6 @@ class SpadaError(Exception):
 	def __init__(self, value):
 		self.logger = logging.getLogger('spada error')
 		self.value = value
-	def __str__(self):
 		self.logger.exception(value)
+	def __str__(self):
 		return repr(self.value)
